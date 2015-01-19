8a28bee9918a4312e8feba38babe1659ad1c2545
Colm MacCarthaigh <colmmacc@amazon.com>
2015-01-19 08:57:48 -0800

    Add support for english language error strings

--- error/s2n_errno.c
+++ error/s2n_errno.c
@@ -11,11 +11,68 @@ __thread const char *s2n_debug_str;
 struct s2n_error_translation
 {
     int errno_value;
-    char * str;
+    const char * str;
 };
 
-struct s2n_error_translation EN[] = { { 0 }
-};
+struct s2n_error_translation EN[] = { 
+    { S2N_ERR_KEY_INIT, "error initializing encryption key" },
+    { S2N_ERR_ENCRYPT, "error encrypting data" },
+    { S2N_ERR_DECRYPT, "error decrypting data" },
+    { S2N_ERR_MEMSET,  "error calling memset" },
+    { S2N_ERR_MEMCPY,  "error calling memcpy" },
+    { S2N_ERR_REALLOC, "error calling realloc" },
+    { S2N_ERR_MLOCK,   "error calling mlock" },
+    { S2N_ERR_FSTAT,   "error calling fstat" },
+    { S2N_ERR_OPEN,    "error calling open" },
+    { S2N_ERR_MMAP,    "error calling mmap" },
+    { S2N_ERR_NULL,    "NULL pointer encountered" },
+    { S2N_ERR_CLOSED,  "connection is closed" },
+    { S2N_ERR_SAFETY,  "a safety check failed" },
+    { S2N_ERR_NOT_INITIALIZED,  "s2n not initialized" },
+    { S2N_ERR_RANDOM_UNITIALIZED, "s2n enctropy not initialized" },
+    { S2N_ERR_OPEN_RANDOM, "error opening urandom" },
+    { S2N_ERR_RESIZE_STATIC_STUFFER, "cannot resize a static stuffer" },
+    { S2N_ERR_RESIZE_TAINTED_STUFFER, "cannot resize a tainted stuffer" },
+    { S2N_ERR_STUFFER_OUT_OF_DATA, "stuffer is out of data" },
+    { S2N_ERR_STUFFER_IS_FULL, "stuffer is full" },
+    { S2N_ERR_INVALID_BASE64, "invalid base64 encountered" },
+    { S2N_ERR_INVALID_PEM, "invalid PEM encountered" },
+    { S2N_ERR_DH_COPYING_PARAMETERS, "error copying diffie-helman parameters" },
+    { S2N_ERR_DH_COPYING_PUBLIC_KEY, "error copying diffie-helman public key" },
+    { S2N_ERR_DH_GENERATING_PARAMETERS, "error generating diffie-helman parameters" },
+    { S2N_ERR_DH_PARAMS_CREATE, "error creating diffie-helman parameters" },
+    { S2N_ERR_DH_SERIAZING, "error serializing diffie-helman parameters" },
+    { S2N_ERR_DH_SHARED_SECRET, "error computing diffie-helman shared secret" },
+    { S2N_ERR_DH_WRITING_PUBLIC_KEY, "error writing diffie-helman public key" },
+    { S2N_ERR_DH_FAILED_SIGNING, "error signing diffie helman values" },
+    { S2N_ERR_INVALID_PKCS3, "invalid PKCS3 encountered" },
+    { S2N_ERR_HASH_DIGEST_FAILED, "failed to create hash digest" },
+    { S2N_ERR_HASH_INIT_FAILED, "error initializing hash" },
+    { S2N_ERR_HASH_INVALID_ALGORITHM, "invalid hash algorithm" },
+    { S2N_ERR_HASH_UPDATE_FAILED, "error updating hash" },
+    { S2N_ERR_HMAC_INVALID_ALGORITHM, "invalid HMAC algorithm" },
+    { S2N_ERR_SIZE_MISMATCH, "size mismatch" },
+    { S2N_ERR_DECODE_CERTIFICATE, "error decoding certificate" },
+    { S2N_ERR_DECODE_PRIVATE_KEY, "error decoding private key" },
+    { S2N_ERR_KEY_MISMATCH, "public and private key do not match" },
+    { S2N_ERR_NOMEM, "no memory" },
+    { S2N_ERR_SIGN, "error signing data"  },
+    { S2N_ERR_VERIFY_SIGNATURE, "error verifying signature" },
+    { S2N_ERR_ALERT_PRESENT, "TLS alert is already pending" },
+    { S2N_ERR_ALERT, "TLS alert received" },
+    { S2N_ERR_CBC_VERIFY, "Failed CBC verification" },
+    { S2N_ERR_CIPHER_NOT_SUPPORTED, "Cipher is not supported" },
+    { S2N_ERR_BAD_MESSAGE, "Bad message encountered" },
+    { S2N_ERR_INVALID_SIGNATURE_ALGORITHM, "Invalid signature algorithm" },
+    { S2N_ERR_INVALID_KEY_EXCHANGE_ALGORITHM, "Invaid key exchange algorithm" },
+    { S2N_ERR_NO_CERTIFICATE_IN_PEM, "No certificate in PEM" },
+    { S2N_ERR_NO_ALERT, "No Alert present" },
+    { S2N_ERR_CLIENT_MODE, "operation not allowed in client mode" },
+    { S2N_ERR_SERVER_NAME_TOO_LONG, "server name is too long" },
+    { S2N_ERR_INSECURE_CLIENT, "client connections not allowed" },
+    { S2N_ERR_HANDSHAKE_STATE, "Invalid handshake state encountered" },
+    { S2N_ERR_FALLBACK_DETECTED, "TLS fallback detected" },
+}; 
 
 const char *s2n_strerror(int error, const char *lang)
 {
@@ -28,6 +85,12 @@ const char *s2n_strerror(int error, const char *lang)
 
     if (strcasecmp(lang, "EN")) {
         return no_such_language;
+    }
+
+    for (int i = 0; i < (sizeof(EN) / sizeof(struct s2n_error_translation)); i++) {
+        if (EN[i].errno_value == error) {
+            return EN[i].str;
+        }
     }
 
     return no_such_error;
