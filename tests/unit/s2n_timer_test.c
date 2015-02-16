bdcde1bb93fafc97319f11d6dfad758857660d4a
Colm MacCárthaigh <colmmacc@amazon.com>
2015-02-16 07:31:56 +0000

    Add boundary case tests for timespec timer
    This change adds explicit boundary case testing around the timespec
    implementation of the s2n_timer functions.

--- tests/unit/s2n_timer_test.c
+++ tests/unit/s2n_timer_test.c
@@ -12,8 +12,6 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
-#include <unistd.h>
-
 #include "s2n_test.h"
 
 #include "utils/s2n_timer.h"
@@ -25,6 +23,7 @@ int main(int argc, char **argv)
 
     BEGIN_TEST();
 
+    /* First: Perform some tests using the real clock */
     EXPECT_SUCCESS(s2n_timer_start(&timer));
     EXPECT_SUCCESS(s2n_timer_reset(&timer, &nanoseconds));
     EXPECT_TRUE(nanoseconds < 1000000000);
@@ -32,6 +31,56 @@ int main(int argc, char **argv)
     EXPECT_SUCCESS(s2n_timer_reset(&timer, &nanoseconds));
     EXPECT_TRUE(nanoseconds > 1000000000);
     EXPECT_TRUE(nanoseconds < 2000000000);
+
+#if !defined(__APPLE__) || !defined(__MACH__)
+    /* Next: perform some tests around timespec boundaries */
+
+    /* Pretend that there were 999,999,999 nanoseconds elapsed in the
+     * previously measured instant. Keep reseting the timer until
+     * the second progresses from that instant, and there are also
+     * less than 999,999,999 nanoseconds elapsed.
+     *
+     * This sets up a situation in which the tv_sec field causes time
+     * to move "forwards", and tv_nsec causes it to move backwards.
+     * e.g.
+     *
+     * previous_time = 10
+     *
+     * timer.time.tv_sec = 11
+     * timer.time.tv_nsec = 123456789;
+     *
+     * delta will be:
+     *   (11 - 10) * 1000000000
+     * + (123456789 - 999999999)
+     *
+     * = 123456790 (same as 1 + 123456789)
+     */
+    time_t previous_time = timer.time.tv_sec;
+    do {
+	previous_time = timer.time.tv_sec;
+        timer.time.tv_nsec = 999999999;
+
+        EXPECT_SUCCESS(s2n_timer_reset(&timer, &nanoseconds));
+    }
+    while(previous_time != (timer.time.tv_sec - 1) || timer.time.tv_nsec == 999999999);
+
+    EXPECT_TRUE(nanoseconds < 1000000000);
+    EXPECT_TRUE(nanoseconds == 1 + timer.time.tv_nsec);
+
+    /* Now we perform the oppossite test: make sure that the previous value for
+     * nsec is smaller than the later one */
+    do {
+	previous_time = timer.time.tv_sec;
+        timer.time.tv_nsec = 0;
+
+        EXPECT_SUCCESS(s2n_timer_reset(&timer, &nanoseconds));
+    }
+    while(previous_time != (timer.time.tv_sec - 1) || timer.time.tv_nsec == 0);
+
+    EXPECT_TRUE(nanoseconds > 1000000000);
+    EXPECT_TRUE(nanoseconds < 2000000000);
+    EXPECT_TRUE(nanoseconds == 1000000000 + timer.time.tv_nsec);
+#endif
 
     END_TEST();
 }
