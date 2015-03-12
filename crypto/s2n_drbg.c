1e5221b461ac7fd1e9bbc488f5f1c0b239553002
Colm MacCarthaigh <colmmacc@amazon.com>
2015-03-12 10:37:37 -0700

    Implementation of NIST SP800-90A CTR_DRBG
    This change makes our DRBG conformant with NIST SP800-90A and now
    matches the NIST test vectors.

--- crypto/s2n_drbg.c
+++ crypto/s2n_drbg.c
@@ -13,6 +13,7 @@
  * permissions and limitations under the License.
  */
 
+#include <openssl/aes.h>
 #include <openssl/evp.h>
 
 #include "crypto/s2n_sequence.h"
@@ -22,6 +23,16 @@
 #include "utils/s2n_random.h"
 #include "utils/s2n_blob.h"
 
+static int s2n_drbg_block_encrypt(uint8_t key[16], uint8_t in[16], uint8_t out[16])
+{
+    AES_KEY ctx;
+
+    AES_set_encrypt_key(key, 128, &ctx);
+    AES_encrypt(in, out, &ctx);
+
+    return 0;
+}
+
 static int s2n_drbg_bits(struct s2n_drbg *drbg, struct s2n_blob *out)
 {
     int block_aligned_size = out->size - (out->size % 16);
@@ -29,14 +40,7 @@ static int s2n_drbg_bits(struct s2n_drbg *drbg, struct s2n_blob *out)
     /* Per NIST SP800-90A 10.2.1.2: */
     for (int i = 0; i < block_aligned_size; i += 16) {
         GUARD(s2n_increment_sequence_number(&drbg->value));
-
-        int len = 16;
-        if (EVP_EncryptUpdate(&drbg->evp_cipher_ctx, out->data + i, &len, drbg->value.data, drbg->value.size) == 0) {
-            S2N_ERROR(S2N_ERR_DRBG);
-        }
-        if (len != 16) {
-            S2N_ERROR(S2N_ERR_DRBG);
-        }
+        GUARD(s2n_drbg_block_encrypt(drbg->key, drbg->v, out->data + i));
     }
 
     if (out->size <= block_aligned_size) {
@@ -44,10 +48,8 @@ static int s2n_drbg_bits(struct s2n_drbg *drbg, struct s2n_blob *out)
     }
 
     uint8_t spare_block[16];
-    struct s2n_blob spare = {.data = spare_block, .size = 16};
-
-    /* Recurse to get the spare block */
-    GUARD(s2n_drbg_bits(drbg, &spare));
+    GUARD(s2n_increment_sequence_number(&drbg->value));
+    GUARD(s2n_drbg_block_encrypt(drbg->key, drbg->v, spare_block));
 
     memcpy_check(out->data + block_aligned_size, spare_block, out->size - block_aligned_size);
 
@@ -61,21 +63,15 @@ static int s2n_drbg_update(struct s2n_drbg *drbg, struct s2n_blob *provided_data
 
     eq_check(provided_data->size, sizeof(temp));
 
-    /* Per NIST SP800-90A 10.2.1.2: */
     GUARD(s2n_drbg_bits(drbg, &temp_blob));
 
     /* XOR in the provided data */
-    for (int i = 0; i < provided_data->size && i < 32; i++) {
+    for (int i = 0; i < provided_data->size; i++) {
         temp[i] ^= provided_data->data[i];
     }
 
-    /* Update the key we use */
-    if (EVP_EncryptInit(&drbg->evp_cipher_ctx, EVP_aes_128_ecb(), temp, NULL) == 0) {
-        S2N_ERROR(S2N_ERR_DRBG);
-    }
-
-    /* Update the value we use */
-    memcpy_check(drbg->value.data, temp + 16, drbg->value.size);
+    memcpy_check(drbg->key, temp, 16);
+    memcpy_check(drbg->v, temp + 16, 16);
 
     return 0;
 }
@@ -106,18 +102,14 @@ int s2n_drbg_seed(struct s2n_drbg *drbg, struct s2n_blob *personalization_string
 
 int s2n_drbg_instantiate(struct s2n_drbg *drbg, struct s2n_blob *personalization_string)
 {
-    EVP_CIPHER_CTX_init(&drbg->evp_cipher_ctx);
-    EVP_CIPHER_CTX_set_padding(&drbg->evp_cipher_ctx, EVP_CIPH_NO_PADDING);
+    drbg->value.size = sizeof(drbg->v);
+    drbg->value.data = drbg->v;
 
     /* Start off with zerod data, per 10.2.1.3.1 item 4 */
-    drbg->value.size = sizeof(drbg->value_data);
-    drbg->value.data = drbg->value_data;
-    memset_check(drbg->value.data, 0, drbg->value.size);
+    memset_check(drbg->v, 0, sizeof(drbg->v));
 
     /* Start off with zerod key, per 10.2.1.3.1 item 5 */
-    if (EVP_EncryptInit(&drbg->evp_cipher_ctx, EVP_aes_128_ctr(), drbg->value.data, NULL) == 0) {
-        S2N_ERROR(S2N_ERR_DRBG);
-    }
+    memset_check(drbg->key, 0, sizeof(drbg->key));
 
     /* Seed / update the DRBG */
     GUARD(s2n_drbg_seed(drbg, personalization_string));
@@ -127,6 +119,8 @@ int s2n_drbg_instantiate(struct s2n_drbg *drbg, struct s2n_blob *personalization
 
 int s2n_drbg_generate(struct s2n_drbg *drbg, struct s2n_blob *blob)
 {
+    uint8_t all_zeros[32] = { 0 };
+    struct s2n_blob zeros = {.data = all_zeros, .size = sizeof(all_zeros) };
     if (blob->size > S2N_DRBG_GENERATE_LIMIT) {
         S2N_ERROR(S2N_ERR_DRBG_REQUEST_SIZE);
     }
@@ -137,6 +131,7 @@ int s2n_drbg_generate(struct s2n_drbg *drbg, struct s2n_blob *blob)
     }
 
     GUARD(s2n_drbg_bits(drbg, blob));
+    GUARD(s2n_drbg_update(drbg, &zeros));
 
     return 0;
 }
