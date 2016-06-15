bd10f692007c0db7eb2fbfb5c6f8212a7ba0672c
Steven Collison <soco@amazon.com>
2016-06-14 19:04:03 -0700

    Remove unused s2n_queue_reader_close_alert

--- tls/s2n_alerts.h
+++ tls/s2n_alerts.h
@@ -21,5 +21,4 @@
 
 extern int s2n_process_alert_fragment(struct s2n_connection *conn);
 extern int s2n_queue_writer_close_alert(struct s2n_connection *conn);
-extern int s2n_queue_reader_close_alert(struct s2n_connection *conn);
 extern int s2n_queue_reader_unsupported_protocol_version_alert(struct s2n_connection *conn);
