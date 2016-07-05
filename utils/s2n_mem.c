791966e7f9ac3e173fcf5b3401628fbf5510a924
Ravinder Shankesi <rsh@amazon.com>
2016-07-05 10:33:23 -0700

    Lock and unlock mlocked memory correctly.
    Currently we set a flag on s2n_blob to indicate that a blob is mlocked.
    However this isn't reliable as we free up blobs later by initializing them
    with the size/location independently (see s2n_config_free for e.g,).
    Removing the flag and making it dependent on use_mlock flag (initialized during
    s2n_init).  Found via warnings from valgrind.

--- utils/s2n_mem.c
+++ utils/s2n_mem.c
@@ -42,7 +42,6 @@ int s2n_alloc(struct s2n_blob *b, uint32_t size)
     b->data = NULL;
     b->size = 0;
     b->allocated = 0;
-    b->mlocked = 0;
     GUARD(s2n_realloc(b, size));
     return 0;
 }
@@ -88,15 +87,16 @@ int s2n_realloc(struct s2n_blob *b, uint32_t size)
         GUARD(s2n_free(b));
         S2N_ERROR(S2N_ERR_MLOCK);
     }
-    b->mlocked = 1;
 
     return 0;
 }
 
+#include<stdio.h>
+
 int s2n_free(struct s2n_blob *b)
 {
     int munlock_rc = 0;
-    if (b->mlocked) {
+    if (use_mlock && b->data) {
        munlock_rc = munlock(b->data, b->size);
     }
 
@@ -108,7 +108,6 @@ int s2n_free(struct s2n_blob *b)
     if (munlock_rc < 0) {
         S2N_ERROR(S2N_ERR_MUNLOCK);
     }
-    b->mlocked = 0;
 
     return 0;
 }
