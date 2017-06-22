b06d31f6efe33f15cee726562c483db1e04b54b0
Aaron Tomb <atomb@galois.com>
2017-06-22 08:07:50 -0700

    Fetch SAW using HTTPS URL

--- .travis/install_saw.sh
+++ .travis/install_saw.sh
@@ -18,7 +18,7 @@ mkdir -p $DOWNLOAD_DIR
 cd $DOWNLOAD_DIR
 
 #download saw binaries
-curl http://saw.galois.com/builds/nightly/saw-0.2-2017-06-22-Ubuntu14.04-64.tar.gz > saw.tar.gz;
+curl https://saw.galois.com/builds/nightly/saw-0.2-2017-06-22-Ubuntu14.04-64.tar.gz > saw.tar.gz;
 
 mkdir -p saw && tar -xzf saw.tar.gz --strip-components=1 -C saw
 mkdir -p $INSTALL_DIR && mv saw/* $INSTALL_DIR
