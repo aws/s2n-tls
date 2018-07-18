3fe41a22d8d2bfb0b855df7294791b19fecd2582
fatrat <fatrat1117@gmail.com>
2018-07-18 15:55:03 -0700

    fix fresh Ubuntu Install scripts
    fix fresh Ubuntu Install scripts

--- .travis/install_ubuntu_dependencies.sh
+++ .travis/install_ubuntu_dependencies.sh
@@ -16,15 +16,21 @@
 set -ex
 
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get update
 
-DEPENDENCIES="indent kwstyle"
+DEPENDENCIES="unzip make indent kwstyle python3.6 libssl-dev"
 
 sudo apt-get install -y ${DEPENDENCIES}
 
 if [[ "$GCC6_REQUIRED" == "true" ]]; then
     sudo apt-get -y install gcc-6;
 fi
+
+#Installing pip with get-pip.py
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
+rm get-pip.py
 
 # Download and Install prlimit for memlock
 if [[ ! -d "$PRLIMIT_INSTALL_DIR" ]] && [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
