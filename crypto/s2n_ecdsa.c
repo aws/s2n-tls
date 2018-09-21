b87f2c926294af08e155cddf7fbcc6a52665df23
Daniel Schwartz-Narbonne <dsn@amazon.com>
2018-09-21 15:33:17 -0400

    remove both remaining uses of GUARD_GOTO

--- crypto/s2n_ecdsa.c
+++ crypto/s2n_ecdsa.c
@@ -84,40 +84,29 @@ static int s2n_ecdsa_keys_match(const struct s2n_pkey *pub, const struct s2n_pke
 {
     uint8_t input[16];
     struct s2n_blob random_input = {0};
-    struct s2n_blob signature = { 0 };
-    struct s2n_hash_state state_in = { 0 }, state_out = { 0 };
+    DEFER_CLEANUP(struct s2n_blob signature = { 0 }, s2n_free);
+    DEFER_CLEANUP(struct s2n_hash_state state_in = { 0 }, s2n_hash_free);
+    DEFER_CLEANUP(struct s2n_hash_state state_out = { 0 }, s2n_hash_free);
 
     random_input.data = input;
     random_input.size = sizeof(input);
-    GUARD_GOTO(s2n_get_public_random_data(&random_input), failed);
+    GUARD(s2n_get_public_random_data(&random_input));
 
     /* s2n_hash_new only allocates memory when using high-level EVP hashes, currently restricted to FIPS mode. */
-    GUARD_GOTO(s2n_hash_new(&state_in), failed);
-    GUARD_GOTO(s2n_hash_new(&state_out), failed);
+    GUARD(s2n_hash_new(&state_in));
+    GUARD(s2n_hash_new(&state_out));
 
-    GUARD_GOTO(s2n_hash_init(&state_in, S2N_HASH_SHA1), failed);
-    GUARD_GOTO(s2n_hash_init(&state_out, S2N_HASH_SHA1), failed);
-    GUARD_GOTO(s2n_hash_update(&state_in, input, sizeof(input)), failed);
-    GUARD_GOTO(s2n_hash_update(&state_out, input, sizeof(input)), failed);
+    GUARD(s2n_hash_init(&state_in, S2N_HASH_SHA1));
+    GUARD(s2n_hash_init(&state_out, S2N_HASH_SHA1));
+    GUARD(s2n_hash_update(&state_in, input, sizeof(input)));
+    GUARD(s2n_hash_update(&state_out, input, sizeof(input)));
 
-    GUARD_GOTO(s2n_alloc(&signature, s2n_ecdsa_der_signature_size(priv)), failed);
+    GUARD(s2n_alloc(&signature, s2n_ecdsa_der_signature_size(priv)));
 
-    GUARD_GOTO(s2n_ecdsa_sign(priv, &state_in, &signature), failed);
-    GUARD_GOTO(s2n_ecdsa_verify(pub, &state_out, &signature), failed);
+    GUARD(s2n_ecdsa_sign(priv, &state_in, &signature));
+    GUARD(s2n_ecdsa_verify(pub, &state_out, &signature));
 
-    int rc = 0;
-    goto cleanup;
-
-    //cppcheck-suppress unusedLabel
-failed:
-    rc = -1;
-
-cleanup:
-    s2n_hash_free(&state_in);
-    s2n_hash_free(&state_out);
-    s2n_free(&signature);
-
-    return rc;
+    return 0;
 }
 
 static int s2n_ecdsa_key_free(struct s2n_pkey *pkey)
