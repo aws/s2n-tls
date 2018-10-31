6f722937acc318f95a52f31784bf698f91ad5ea0
fwsGonzo <fwsgonzo@hotmail.com>
2018-10-31 19:05:01 +0100

    connection: Add dummy functions for serialization

---
+++ tls/s2n_connection_serialize.c
@@ -0,0 +1,11 @@
+#include "s2n_connection.h"
+
+size_t s2n_conn_serialize_to(struct s2n_connection* conn, void* addr)
+{
+  return 0;
+}
+
+struct s2n_connection* s2n_conn_serialize_from(void* addr)
+{
+  return 0;
+}
