39ee3e090e950a0f0b57ae5d671862e59a7e50dc
Steven Collison <steven@raycoll.com>
2019-02-06 18:34:32 -0800

    test: Remove TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256 from wire_ciphers_only_ecdsa

--- tests/unit/s2n_cipher_suite_match_test.c
+++ tests/unit/s2n_cipher_suite_match_test.c
@@ -181,7 +181,6 @@ int main(int argc, char **argv)
             TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
             TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
             TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
-            TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
         };
         const uint8_t cipher_count_only_ecdsa = sizeof(wire_ciphers_only_ecdsa) / S2N_TLS_CIPHER_SUITE_LEN;
 
