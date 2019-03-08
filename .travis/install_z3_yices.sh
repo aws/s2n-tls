353fe2dd118525453b90783989120e662fb8dd17
Aaron Tomb <atomb@galois.com>
2019-03-08 10:49:10 -0800

    Install Yices with apt-get on Travis

--- .travis/install_z3_yices.sh
+++ .travis/install_z3_yices.sh
@@ -30,13 +30,14 @@ INSTALL_DIR=$2
 mkdir -p "$DOWNLOAD_DIR"
 cd "$DOWNLOAD_DIR"
 
+sudo add-apt-repository ppa:sri-csl/formal-methods
+sudo apt-get update
+sudo apt-get install yices2
+
 #download z3 and yices
 curl --retry 3 https://s3-us-west-2.amazonaws.com/s2n-public-test-dependencies/z3-2017-04-04-Ubuntu14.04-64 --output z3
-curl --retry 3 https://s3-us-west-2.amazonaws.com/s2n-public-test-dependencies/yices_smt2-linux-static-2017-06-21 --output yices-smt2
 sudo chmod +x z3
-sudo chmod +x yices-smt2
 mkdir -p "$INSTALL_DIR"/bin
 mv z3 "$INSTALL_DIR"/bin
-mv yices-smt2 "$INSTALL_DIR"/bin
 "$INSTALL_DIR"/bin/z3 --version
-"$INSTALL_DIR"/bin/yices-smt2 --version
+yices-smt2 --version
