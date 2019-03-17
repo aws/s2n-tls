cb471eaaf4a302eed2aabc61d4e5cba494d69899
Daniil Bondarev <xonatius@gmail.com>
2019-03-17 05:12:41 -0700

    Handle nextUpdate absence when validating OCSP staple
    NextUpdate might not be present on OCSP, if OCSP endpoint is willing to
    provide fresh information every time[1]. Previously this resulted in null
    pointer dereference, with this change we will allow OCSP to be valid for
    1 hour since thisUpdate time.
    
    While in theory absence of NextUpdate means that OCSP staple should be
    as fresh as possible, in practice nginx[2] caches such OCSPs for an hour,
    hence we're matching this validation behavior here.
    
    [1] https://tools.ietf.org/html/rfc6960#section-4.2.2.1
    [2] https://github.com/nginx/nginx/blob/9197a3c/src/event/ngx_event_openssl_stapling.c#L746

--- tests/unit/s2n_x509_validator_test.c
+++ tests/unit/s2n_x509_validator_test.c
@@ -27,6 +27,11 @@ static int fetch_invalid_before_ocsp_timestamp(void *data, uint64_t *timestamp) 
     return 0;
 }
 
+static int fetch_not_expired_ocsp_timestamp(void *data, uint64_t *timestamp) {
+    *timestamp = 1552824239000000000;
+    return 0;
+}
+
 
 static uint32_t write_pem_file_to_stuffer_as_chain(struct s2n_stuffer *chain_out_stuffer, const char *pem_data) {
     struct s2n_stuffer chain_in_stuffer, cert_stuffer;
@@ -772,6 +777,56 @@ int main(int argc, char **argv) {
         EXPECT_EQUAL(S2N_CERT_OK, s2n_x509_validator_validate_cert_stapled_ocsp_response(&validator, connection,
                                                                                           s2n_stuffer_raw_read(&ocsp_data_stuffer, ocsp_data_len),
                                                                                           ocsp_data_len));
+        s2n_stuffer_free(&ocsp_data_stuffer);
+        s2n_connection_free(connection);
+        s2n_pkey_free(&public_key_out);
+        s2n_x509_validator_wipe(&validator);
+        s2n_x509_trust_store_wipe(&trust_store);
+    }
+
+    /* Test valid OCSP date range wihtout nextUpdate field */
+    {
+        struct s2n_x509_trust_store trust_store;
+        s2n_x509_trust_store_init_empty(&trust_store);
+        EXPECT_SUCCESS(s2n_x509_trust_store_from_ca_file(&trust_store, S2N_OCSP_CA_CERT, NULL));
+
+        struct s2n_x509_validator validator;
+        s2n_x509_validator_init(&validator, &trust_store, 1);
+
+        struct s2n_connection *connection = s2n_connection_new(S2N_CLIENT);
+        EXPECT_NOT_NULL(connection);
+
+        struct host_verify_data verify_data = { .callback_invoked = 0, .found_name = 0, .name = NULL };
+        EXPECT_SUCCESS(s2n_connection_set_verify_host_callback(connection, verify_host_accept_everything, &verify_data));
+
+        uint8_t cert_chain_pem[S2N_MAX_TEST_PEM_SIZE];
+        EXPECT_SUCCESS(s2n_read_test_pem(S2N_OCSP_SERVER_CERT, (char *) cert_chain_pem, S2N_MAX_TEST_PEM_SIZE));
+        struct s2n_stuffer chain_stuffer;
+        uint32_t chain_len = write_pem_file_to_stuffer_as_chain(&chain_stuffer, (const char *) cert_chain_pem);
+        EXPECT_TRUE(chain_len > 0);
+        uint8_t *chain_data = s2n_stuffer_raw_read(&chain_stuffer, (uint32_t) chain_len);
+
+        struct s2n_pkey public_key_out;
+        EXPECT_SUCCESS(s2n_pkey_zero_init(&public_key_out));
+        s2n_cert_type cert_type;
+        EXPECT_EQUAL(S2N_CERT_OK,
+                     s2n_x509_validator_validate_cert_chain(&validator, connection, chain_data, chain_len, &cert_type, &public_key_out));
+        s2n_stuffer_free(&chain_stuffer);
+
+        struct s2n_stuffer ocsp_data_stuffer;
+        EXPECT_SUCCESS(read_file(&ocsp_data_stuffer, S2N_OCSP_RESPONSE_NO_NEXT_UPDATE_DER, S2N_MAX_TEST_PEM_SIZE));
+        uint32_t ocsp_data_len = s2n_stuffer_data_available(&ocsp_data_stuffer);
+        EXPECT_TRUE(ocsp_data_len > 0);
+
+        s2n_clock_time_nanoseconds old_clock = connection->config->wall_clock;
+        s2n_config_set_wall_clock(connection->config, fetch_not_expired_ocsp_timestamp, NULL);
+
+        EXPECT_EQUAL(S2N_CERT_OK, s2n_x509_validator_validate_cert_stapled_ocsp_response(&validator, connection,
+                                                                                          s2n_stuffer_raw_read(&ocsp_data_stuffer, ocsp_data_len),
+                                                                                          ocsp_data_len));
+        EXPECT_EQUAL(1, verify_data.callback_invoked);
+        s2n_config_set_wall_clock(connection->config, old_clock, NULL);
+
         s2n_stuffer_free(&ocsp_data_stuffer);
         s2n_connection_free(connection);
         s2n_pkey_free(&public_key_out);
