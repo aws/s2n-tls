988222fe74a72dd96d24ffaef88374df4fff12e3
Steven Collison <steven@raycoll.com>
2019-05-09 13:44:04 -0700

    Add missing include in s2n_certificate.c
    string.h is needed for strnlen.

--- crypto/s2n_certificate.c
+++ crypto/s2n_certificate.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
@@ -16,6 +16,7 @@
 #include <s2n.h>
 #include <openssl/x509v3.h>
 #include <openssl/pem.h>
+#include <string.h>
 #include <strings.h>
 
 #include "crypto/s2n_certificate.h"
