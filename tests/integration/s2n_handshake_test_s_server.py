087e46ef7d9c4eae0e917ecd4b7d2b29c2d5eaca
Hao <fatrat1117@gmail.com>
2019-05-13 13:59:34 -0700

    Fix corked IO server test

--- tests/integration/s2n_handshake_test_s_server.py
+++ tests/integration/s2n_handshake_test_s_server.py
@@ -321,19 +321,11 @@ def elliptic_curve_test(host, port):
                     failed = 1
     return failed
 
-def str2bool(v):
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
-        return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
-        return False
-    else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
-
 def main():
     parser = argparse.ArgumentParser(description='Runs TLS server integration tests against Openssl s_server using s2nc')
     parser.add_argument('host', help='The host for s2nc to connect to')
     parser.add_argument('port', type=int, help='The port for s_server to bind to')
-    parser.add_argument('use_corked_io', type=str2bool, help='Turn corked IO on/off')
+    parser.add_argument('--use_corked_io', action='store_true', help='Turn corked IO on/off')
     parser.add_argument('--libcrypto', default='openssl-1.1.1', choices=['openssl-1.0.2', 'openssl-1.0.2-fips', 'openssl-1.1.1', 'libressl'],
             help="""The Libcrypto that s2n was built with. s2n supports different cipher suites depending on
                     libcrypto version. Defaults to openssl-1.1.1.""")
