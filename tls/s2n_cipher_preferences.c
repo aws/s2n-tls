c5d16a4cb1bf162bff158dc5648a0de92676db19
David Wilkie <dwilkie@gmail.com>
2019-05-14 09:28:14 +0700

    Remove weak RSA ciphers

--- tls/s2n_cipher_preferences.c
+++ tls/s2n_cipher_preferences.c
@@ -551,8 +551,6 @@ struct s2n_cipher_suite *cipher_suites_elb_security_policy_tls_1_2_2019_05[] = {
     &s2n_ecdhe_rsa_with_aes_128_gcm_sha256,
     &s2n_ecdhe_ecdsa_with_aes_256_gcm_sha384,
     &s2n_ecdhe_rsa_with_aes_256_gcm_sha384,
-    &s2n_rsa_with_aes_128_gcm_sha256,
-    &s2n_rsa_with_aes_256_gcm_sha384,
 };
 
 const struct s2n_cipher_preferences elb_security_policy_tls_1_2_2019_05 = {
