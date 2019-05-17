ae1a459f202cde2cb719b062c83ac7d062aebf1f
Hao <fatrat1117@gmail.com>
2019-05-16 17:02:09 -0700

    Reformat macros

--- bin/common.h
+++ bin/common.h
@@ -1,17 +1,17 @@
 #pragma once
 
-#define GUARD_EXIT( x , msg )   \
-  do {						    \
-    if (( x ) < 0) {		    \
-        print_s2n_error(msg);	\
-        exit(1);                \
-    }							\
+#define GUARD_EXIT(x, msg)  \
+  do {                      \
+    if ((x) < 0) {          \
+      print_s2n_error(msg); \
+      exit(1);              \
+    }                       \
   } while (0)
 
-#define GUARD_RETURN( x , msg ) \
-  do {						    \
-    if (( x ) < 0) {		    \
-        print_s2n_error(msg);	\
-        return -1;              \
-    }							\
+#define GUARD_RETURN(x, msg) \
+  do {                       \
+    if ((x) < 0) {           \
+      print_s2n_error(msg);  \
+      return -1;             \
+    }                        \
   } while (0)
