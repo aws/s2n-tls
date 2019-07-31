6cc098375fde8cb234592561ae5474426de6878a
Justin Boswell <boswej@amazon.com>
2019-07-31 11:08:25 -0700

    Exported config needs threads too

--- cmake/s2n-config.cmake
+++ cmake/s2n-config.cmake
@@ -1,5 +1,7 @@
 include(CMakeFindDependencyMacro)
 
 find_dependency(LibCrypto)
+set(THREADS_PREFER_PTHREAD_FLAG ON)
+find_package(Threads REQUIRED)
 
 include(${CMAKE_CURRENT_LIST_DIR}/@CMAKE_PROJECT_NAME@-targets.cmake)
