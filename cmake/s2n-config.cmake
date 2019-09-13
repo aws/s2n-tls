4675f78e3e7ecc573639e5a6e4ba8f60759e6df5
Jonathan M. Henson <henso@amazon.com>
2019-09-13 16:33:39 -0700

    Fixed mixed static and shared lib builds in the same build closure output.

--- cmake/s2n-config.cmake
+++ cmake/s2n-config.cmake
@@ -7,4 +7,9 @@ endif()
 
 find_dependency(LibCrypto)
 
-include(${CMAKE_CURRENT_LIST_DIR}/@CMAKE_PROJECT_NAME@-targets.cmake)
+if (BUILD_SHARED_LIBS)
+    include(${CMAKE_CURRENT_LIST_DIR}/shared/@CMAKE_PROJECT_NAME@-targets.cmake)
+else()
+    include(${CMAKE_CURRENT_LIST_DIR}/static/@CMAKE_PROJECT_NAME@-targets.cmake)
+endif()
+
