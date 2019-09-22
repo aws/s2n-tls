28fce0707a3c9387f3dc40199e33085fd9be9c1e
Lindsay Stewart <slindsay@amazon.com>
2019-09-22 16:59:14 -0700

    Consolidate cipher change spec message handlers
    Both client and server send the same ccs messages. They also
    perform some common parsing on recv, which is actually the only
    parsing done on ccs messages in tls1.3.

--- tests/s2n_test.h
+++ tests/s2n_test.h
@@ -90,6 +90,8 @@
 #define EXPECT_SUCCESS( function_call )  EXPECT_NOT_EQUAL( (function_call) ,  -1 )
 
 #define EXPECT_BYTEARRAY_EQUAL( p1, p2, l ) EXPECT_EQUAL( memcmp( (p1), (p2), (l) ), 0 )
+#define EXPECT_BYTEARRAY_NOT_EQUAL( p1, p2, l ) EXPECT_NOT_EQUAL( memcmp( (p1), (p2), (l) ), 0 )
+
 #define EXPECT_STRING_EQUAL( p1, p2 ) EXPECT_EQUAL( strcmp( (p1), (p2) ), 0 )
 
 #define S2N_TEST_ENTER_FIPS_MODE()    { if (FIPS_mode_set(1) == 0) { \
