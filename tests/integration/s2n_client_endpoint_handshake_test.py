9ee1775eb7b0ecc0f11d1b223b49c90540e92ed1
bbutch <bbutch@amazon.com>
2019-11-14 10:16:37 -0800

    Update client handshake integ test to connect to KMS using PQ-hybrid ciphers

--- tests/integration/s2n_client_endpoint_handshake_test.py
+++ tests/integration/s2n_client_endpoint_handshake_test.py
@@ -21,16 +21,23 @@ import subprocess
 import itertools
 from s2n_test_constants import *
 
+
+# If a cipher_preference_version is specified, we will use it while attempting the handshake;
+# otherwise, s2n will use the default. If an expected_cipher is specified, the test will pass
+# if and only if the handshake is negotiated with that cipher; otherwise, the test will pass
+# if the handshake is negotiated with any cipher.
 well_known_endpoints = [
-    "amazon.com",
-    "facebook.com",
-    "google.com",
-    "netflix.com",
-    "s3.amazonaws.com",
-    "twitter.com",
-    "wikipedia.org",
-    "yahoo.com",
-    ]
+    {"endpoint": "amazon.com"},
+    {"endpoint": "facebook.com"},
+    {"endpoint": "google.com"},
+    {"endpoint": "netflix.com"},
+    {"endpoint": "s3.amazonaws.com"},
+    {"endpoint": "twitter.com"},
+    {"endpoint": "wikipedia.org"},
+    {"endpoint": "yahoo.com"},
+    {"endpoint": "kms.us-east-1.amazonaws.com", "cipher_preference_version": "KMS-PQ-TLS-1-0-2019-06", "expected_cipher": "ECDHE-BIKE-RSA-AES256-GCM-SHA384"},
+    {"endpoint": "kms.us-east-1.amazonaws.com", "cipher_preference_version": "PQ-SIKE-TEST-TLS-1-0-2019-11", "expected_cipher": "ECDHE-SIKE-RSA-AES256-GCM-SHA384"}
+]
 
 def print_result(result_prefix, return_code):
     print(result_prefix, end="")
@@ -45,18 +52,19 @@ def print_result(result_prefix, return_code):
         else:
             print("FAILED")
 
-def try_client_handshake(endpoint, use_corked_io):
-    s2nc_cmd = ["../../bin/s2nc", "-f",  "./trust-store/ca-bundle.crt", "-a", "http/1.1", str(endpoint)]
-    if use_corked_io:
-        s2nc_cmd.append("-C")
-
+def try_client_handshake(endpoint, arguments, expected_cipher):
+    s2nc_cmd = ["../../bin/s2nc", "-f", "./trust-store/ca-bundle.crt", "-a", "http/1.1"] + arguments + [str(endpoint)]
     currentDir = os.path.dirname(os.path.realpath(__file__))
     s2nc = subprocess.Popen(s2nc_cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, cwd=currentDir)
 
     found = 0
+    expected_output = "Cipher negotiated: "
+    if expected_cipher:
+        expected_output += expected_cipher
+
     for line in range(0, 10):
-        output = s2nc.stdout.readline().decode("utf-8")
-        if "Cipher negotiated" in str(output):
+        output = str(s2nc.stdout.readline().decode("utf-8"))
+        if expected_output in output:
             found = 1
             break
 
@@ -69,23 +77,31 @@ def try_client_handshake(endpoint, use_corked_io):
     return 0
 
 def well_known_endpoints_test(use_corked_io):
+    arguments = []
     msg = "\n\tTesting s2n Client with Well Known Endpoints:"
     if use_corked_io:
         msg = "\n\tTesting s2n Client with Well Known Endpoints using Corked IO:"
+        arguments += ["-C"]
     print(msg)
 
     maxRetries = 5
     failed = 0
-    for endpoint in well_known_endpoints:
+    for endpoint_config in well_known_endpoints:
+        endpoint = endpoint_config["endpoint"]
+        expected_cipher = endpoint_config.get("expected_cipher")
+
+        if "cipher_preference_version" in endpoint_config:
+            arguments += ["-c", endpoint_config["cipher_preference_version"]]
+
         # Retry handshake in case there are any problems going over the internet
         for i in range(1, maxRetries):
-            ret = try_client_handshake(endpoint, use_corked_io)
+            ret = try_client_handshake(endpoint, arguments, expected_cipher)
             if ret is 0:
                 break
             else:
                 time.sleep(i)
-        
-        print_result("Endpoint:  %-40s... " % endpoint, ret)
+
+        print_result("Endpoint: %-35sExpected Cipher: %-40s... " % (endpoint, expected_cipher if expected_cipher else "Any"), ret)
         if ret != 0:
             failed += 1
 
