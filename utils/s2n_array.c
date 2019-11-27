a522431bb96ee748d60f966c24261c0df33cafc4
Daniel Schwartz-Narbonne <dsn@amazon.com>
2019-11-27 15:40:12 -0500

    change how we initilize the array.  See if that makes KWStyle happy

--- utils/s2n_array.c
+++ utils/s2n_array.c
@@ -40,12 +40,9 @@ struct s2n_array *s2n_array_new(size_t element_size)
     struct s2n_blob mem = {0};
     GUARD_PTR(s2n_alloc(&mem, sizeof(struct s2n_array)));
 
+    struct s2n_array initilizer = {.mem = {0}, .num_of_elements = 0, .capacity = 0, .element_size = element_size};
     struct s2n_array *array = (void *) mem.data;
-    array->mem = (struct s2n_blob) {0};	
-    array->num_of_elements = 0;
-    array->capacity = 0;
-    array->element_size = element_size;
-
+    *array = initilizer;
     GUARD_PTR(s2n_array_enlarge(array, S2N_INITIAL_ARRAY_SIZE));
 
     return array;
