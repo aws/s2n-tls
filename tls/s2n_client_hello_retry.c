a11dc35735ea212e009b9039b1f9568fecbc6ef8
Ravi Singh <ravsi@amazon.com>
2019-12-11 16:39:03 -0800

    verify if server_hello is actually a hello_retry request or not

--- tls/s2n_client_hello_retry.c
+++ tls/s2n_client_hello_retry.c
@@ -12,8 +12,24 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
+#include <stdbool.h>
 
 #include "error/s2n_errno.h"
+#include "utils/s2n_blob.h"
+#include "tls/s2n_tls.h"
+#include "utils/s2n_safety.h"
+
+/* from RFC: https://tools.ietf.org/html/rfc8446#section-4.1.3*/
+const uint8_t hello_retry_req_random[] = {
+    0xCF ,0x21 ,0xAD ,0x74 ,0xE5 ,0x9A ,0x61 ,0x11 ,0xBE ,0x1D ,0x8C ,0x02 ,0x1E ,0x65 ,0xB8 ,0x91,
+    0xC2 ,0xA2 ,0x11 ,0x16 ,0x7A ,0xBB ,0x8C ,0x5E ,0x07 ,0x9E ,0x09 ,0xE2 ,0xC8 ,0xA8 ,0x33 ,0x9C
+};
+
+inline bool s2n_is_hello_retry_req(struct s2n_connection *conn)
+{
+    return s2n_constant_time_equals(hello_retry_req_random, conn->secure.server_random, S2N_TLS_RANDOM_DATA_LEN);
+}
+
 
 int s2n_client_hello_retry_send(struct s2n_connection *conn)
 {
