50472edce0c12b99ca5c105da28c796fe9959ddb
Madeleine <madelea@amazon.com>
2020-01-17 14:12:30 -0800

    Expanded unit test cases

--- tests/unit/s2n_tls13_parse_record_type_test.c
+++ tests/unit/s2n_tls13_parse_record_type_test.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
@@ -25,16 +25,18 @@
 
 int main(int argc, char **argv)
 {
-    uint16_t plaintext = 0xdaf3;
-    struct s2n_stuffer plaintext_stuffer = {0};
-    uint8_t record_type;
     BEGIN_TEST();
+
+    uint8_t record_type;
 
    /* In tls13 the true record type is inserted in the last byte of the encrypted payload. This
     * test creates a fake unencrypted payload and checks that the helper function
     * s2n_parse_record_type() correctly parses the type.
     */
     {
+        uint16_t plaintext = 0xdaf3;
+        struct s2n_stuffer plaintext_stuffer = {0};
+
         EXPECT_SUCCESS(s2n_stuffer_alloc(&plaintext_stuffer, sizeof(plaintext)));
         EXPECT_SUCCESS(s2n_stuffer_write_uint16(&plaintext_stuffer, plaintext));
         EXPECT_SUCCESS(s2n_parse_record_type(&plaintext_stuffer, &record_type));
@@ -45,7 +47,29 @@ int main(int argc, char **argv)
         EXPECT_SUCCESS(s2n_stuffer_free(&plaintext_stuffer));
     }
 
-    END_TEST();
+    /* Test for failure when stuffer is completely empty */
+    {
+        struct s2n_stuffer empty_stuffer = {0};
 
+        EXPECT_SUCCESS(s2n_stuffer_alloc(&empty_stuffer, 0));
+        EXPECT_FAILURE(s2n_parse_record_type(&empty_stuffer, &record_type));
+    }
+
+    /* Test for case where there is a record type in the stuffer but no content */
+    {
+        uint16_t plaintext = 0xf3;
+        struct s2n_stuffer plaintext_stuffer = {0};
+
+        EXPECT_SUCCESS(s2n_stuffer_alloc(&plaintext_stuffer, sizeof(plaintext)));
+        EXPECT_SUCCESS(s2n_stuffer_write_uint16(&plaintext_stuffer, plaintext));
+        EXPECT_SUCCESS(s2n_parse_record_type(&plaintext_stuffer, &record_type));
+        EXPECT_EQUAL(record_type, 0xf3);
+        EXPECT_EQUAL(s2n_stuffer_data_available(&plaintext_stuffer), 1);
+
+        /* Clean up */
+        EXPECT_SUCCESS(s2n_stuffer_free(&plaintext_stuffer));
+    }
+    
+    END_TEST();
 }
 
