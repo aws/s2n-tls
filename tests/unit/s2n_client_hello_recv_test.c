2e431ffb8a33ba9a94efc12b7ec7221d38f760d7
Madeleine <madelea@amazon.com>
2020-02-10 12:04:07 -0800

    Fixing unit test setup

--- tests/unit/s2n_client_hello_recv_test.c
+++ tests/unit/s2n_client_hello_recv_test.c
@@ -38,20 +38,35 @@ int main(int argc, char **argv)
     struct s2n_connection *client_conn;
     struct s2n_stuffer *hello_stuffer;
     struct s2n_config *config;
+    struct s2n_config *tls13_config;
     struct s2n_cert_chain_and_key *chain_and_key;
+    struct s2n_cert_chain_and_key *tls13_chain_and_key;
     char *cert_chain;
+    char *tls13_cert_chain;
     char *private_key;
+    char *tls13_private_key;
 
     BEGIN_TEST();
 
     EXPECT_NOT_NULL(cert_chain = malloc(S2N_MAX_TEST_PEM_SIZE));
     EXPECT_NOT_NULL(private_key = malloc(S2N_MAX_TEST_PEM_SIZE));
     EXPECT_NOT_NULL(config = s2n_config_new());
+    EXPECT_NOT_NULL(chain_and_key = s2n_cert_chain_and_key_new());
+   
+    EXPECT_NOT_NULL(tls13_cert_chain = malloc(S2N_MAX_TEST_PEM_SIZE));
+    EXPECT_NOT_NULL(tls13_private_key = malloc(S2N_MAX_TEST_PEM_SIZE));
+    EXPECT_NOT_NULL(tls13_config = s2n_config_new());
+    EXPECT_NOT_NULL(tls13_chain_and_key = s2n_cert_chain_and_key_new());
+
     EXPECT_SUCCESS(s2n_read_test_pem(S2N_DEFAULT_TEST_CERT_CHAIN, cert_chain, S2N_MAX_TEST_PEM_SIZE));
     EXPECT_SUCCESS(s2n_read_test_pem(S2N_DEFAULT_TEST_PRIVATE_KEY, private_key, S2N_MAX_TEST_PEM_SIZE));
-    EXPECT_NOT_NULL(chain_and_key = s2n_cert_chain_and_key_new());
     EXPECT_SUCCESS(s2n_cert_chain_and_key_load_pem(chain_and_key, cert_chain, private_key));
     EXPECT_SUCCESS(s2n_config_add_cert_chain_and_key_to_store(config, chain_and_key));
+
+    EXPECT_SUCCESS(s2n_read_test_pem(S2N_ECDSA_P384_PKCS1_CERT_CHAIN, tls13_cert_chain, S2N_MAX_TEST_PEM_SIZE));
+    EXPECT_SUCCESS(s2n_read_test_pem(S2N_ECDSA_P384_PKCS1_KEY, tls13_private_key, S2N_MAX_TEST_PEM_SIZE));
+    EXPECT_SUCCESS(s2n_cert_chain_and_key_load_pem(tls13_chain_and_key, tls13_cert_chain, tls13_private_key));
+    EXPECT_SUCCESS(s2n_config_add_cert_chain_and_key_to_store(tls13_config, tls13_chain_and_key));
 
     /* These tests verify the logic behind the setting of these three connection fields: 
     server_protocol_version, client_protocol_version, and actual_protocol version. */ 
@@ -110,7 +125,7 @@ int main(int argc, char **argv)
         
         EXPECT_SUCCESS(s2n_stuffer_write(&server_conn->handshake.io, &client_conn->handshake.io.blob));
         EXPECT_SUCCESS(s2n_client_hello_recv(server_conn)); 
-        /* Check to make sure actual protocol version is set to tls13*/
+      
         EXPECT_SUCCESS(server_conn->server_protocol_version = S2N_TLS13);
         EXPECT_SUCCESS(server_conn->actual_protocol_version = S2N_TLS12);
         EXPECT_SUCCESS(server_conn->client_protocol_version = S2N_TLS12);
@@ -124,8 +139,11 @@ int main(int argc, char **argv)
         EXPECT_SUCCESS(s2n_enable_tls13());
         EXPECT_NOT_NULL(server_conn = s2n_connection_new(S2N_SERVER));
         EXPECT_NOT_NULL(client_conn = s2n_connection_new(S2N_CLIENT));
-        EXPECT_SUCCESS(s2n_connection_set_config(server_conn, config));
+        EXPECT_SUCCESS(s2n_connection_set_config(server_conn, tls13_config));
         EXPECT_SUCCESS(s2n_connection_set_config(client_conn, config));
+
+        EXPECT_SUCCESS(s2n_connection_set_cipher_preferences(client_conn, "default_tls13"));
+        EXPECT_SUCCESS(s2n_connection_set_cipher_preferences(server_conn, "default_tls13"));
 
         hello_stuffer = &client_conn->handshake.io;
 
@@ -140,7 +158,7 @@ int main(int argc, char **argv)
         
         EXPECT_SUCCESS(s2n_stuffer_write(&server_conn->handshake.io, &hello_stuffer->blob));
         EXPECT_SUCCESS(s2n_client_hello_recv(server_conn)); 
-        /* Check to make sure actual protocol version is set to tls13 */
+
         EXPECT_SUCCESS(server_conn->server_protocol_version = S2N_TLS13);
         EXPECT_SUCCESS(server_conn->actual_protocol_version = S2N_TLS13);
         EXPECT_SUCCESS(server_conn->client_protocol_version = S2N_TLS13);
@@ -154,14 +172,17 @@ int main(int argc, char **argv)
         EXPECT_SUCCESS(s2n_enable_tls13());
         EXPECT_NOT_NULL(server_conn = s2n_connection_new(S2N_SERVER));
         EXPECT_NOT_NULL(client_conn = s2n_connection_new(S2N_CLIENT));
-        EXPECT_SUCCESS(s2n_connection_set_config(server_conn, config));
+        EXPECT_SUCCESS(s2n_connection_set_config(server_conn, tls13_config));
         EXPECT_SUCCESS(s2n_connection_set_config(client_conn, config));
+
+        EXPECT_SUCCESS(s2n_connection_set_cipher_preferences(client_conn, "default_tls13"));
+        EXPECT_SUCCESS(s2n_connection_set_cipher_preferences(server_conn, "default_tls13"));
 
         EXPECT_SUCCESS(s2n_client_hello_send(client_conn)); 
         
         EXPECT_SUCCESS(s2n_stuffer_write(&server_conn->handshake.io, &client_conn->handshake.io.blob));
         EXPECT_SUCCESS(s2n_client_hello_recv(server_conn)); 
-        /* Check to make sure actual protocol version is set to tls13*/
+       
         EXPECT_SUCCESS(server_conn->server_protocol_version = S2N_TLS13);
         EXPECT_SUCCESS(server_conn->actual_protocol_version = S2N_TLS13);
         EXPECT_SUCCESS(server_conn->client_protocol_version = S2N_TLS13);
@@ -177,6 +198,8 @@ int main(int argc, char **argv)
         EXPECT_NOT_NULL(client_conn = s2n_connection_new(S2N_CLIENT));
         EXPECT_SUCCESS(s2n_connection_set_config(server_conn, config));
         EXPECT_SUCCESS(s2n_connection_set_config(client_conn, config));
+
+        EXPECT_SUCCESS(s2n_connection_set_cipher_preferences(server_conn, "default_tls13"));
         client_conn->client_protocol_version = S2N_TLS12;
         hello_stuffer = &client_conn->handshake.io;
 
@@ -191,7 +214,7 @@ int main(int argc, char **argv)
         
         EXPECT_SUCCESS(s2n_stuffer_write(&server_conn->handshake.io, &hello_stuffer->blob));
         EXPECT_SUCCESS(s2n_client_hello_recv(server_conn)); 
-        /* Check to make sure actual protocol version is set to tls13*/
+    
         EXPECT_SUCCESS(server_conn->server_protocol_version = S2N_TLS13);
         EXPECT_SUCCESS(server_conn->actual_protocol_version = S2N_TLS12);
         EXPECT_SUCCESS(server_conn->client_protocol_version = S2N_TLS12);
@@ -201,9 +224,13 @@ int main(int argc, char **argv)
     } 
 
     s2n_config_free(config);
+    s2n_config_free(tls13_config);
     s2n_cert_chain_and_key_free(chain_and_key);
     free(cert_chain);
     free(private_key);
+    s2n_cert_chain_and_key_free(tls13_chain_and_key);
+    free(tls13_cert_chain);
+    free(tls13_private_key);
     EXPECT_SUCCESS(s2n_disable_tls13());
     END_TEST();
     return 0;
