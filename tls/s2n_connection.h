efad87508785eb5aadad367faaadff4f63b368fa
Hao <fatrat1117@gmail.com>
2020-02-17 15:34:42 -0800

    Do not try write on a half closed connection

--- tls/s2n_connection.h
+++ tls/s2n_connection.h
@@ -284,6 +284,9 @@ struct s2n_connection {
 
     /* Cookie extension data */
     struct s2n_stuffer cookie_stuffer;
+
+    /* If write fd is broken */
+    unsigned write_fd_broken : 1;
 };
 
 int s2n_connection_is_managed_corked(const struct s2n_connection *s2n_connection);
