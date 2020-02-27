628218ebabe3a12c965c62d11e2e1ee1d317b2e5
Madeleine <madelea@amazon.com>
2020-02-27 09:33:38 -0800

    Added more tests

---
+++ tests/unit/s2n_choose_supported_group_test.c
@@ -0,0 +1,77 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+#include "s2n_test.h"
+#include "testlib/s2n_testlib.h"
+#include "tls/s2n_tls.h"
+#include "tls/s2n_tls13.h"
+#include "tls/extensions/s2n_client_supported_groups.h"
+
+int main(int argc, char **argv) 
+{
+    struct s2n_connection *server_conn;
+
+    BEGIN_TEST();
+    /* This test checks the logic in the function s2n_choose_supported_group, which should select the highest
+     * supported group or, if none are available, select NULL.
+     */
+    {
+        /* If the list of mutually supported groups is empty, chosen curve should be set to null */
+        EXPECT_NOT_NULL(server_conn = s2n_connection_new(S2N_SERVER));
+        EXPECT_NULL(server_conn->secure.server_ecc_evp_params.negotiated_curve);
+        for (int i = 0; i < S2N_ECC_EVP_SUPPORTED_CURVES_COUNT; i++) {
+            EXPECT_NULL(server_conn->secure.mutually_supported_groups[i].negotiated_curve);
+        }
+        EXPECT_SUCCESS(s2n_choose_supported_group(&server_conn->secure.server_ecc_evp_params, server_conn->secure.mutually_supported_groups));
+
+        EXPECT_NULL(server_conn->secure.server_ecc_evp_params.negotiated_curve);
+        EXPECT_SUCCESS(s2n_connection_free(server_conn));
+    }
+
+    {
+        /* If the list of mutually supported groups has one match, chosen curve should be set to that
+         * match.
+         */
+        EXPECT_NOT_NULL(server_conn = s2n_connection_new(S2N_SERVER));
+        EXPECT_NULL(server_conn->secure.server_ecc_evp_params.negotiated_curve);
+        server_conn->secure.mutually_supported_groups[1].negotiated_curve = s2n_ecc_evp_supported_curves_list[1];
+
+        EXPECT_SUCCESS(s2n_choose_supported_group(&server_conn->secure.server_ecc_evp_params, server_conn->secure.mutually_supported_groups));
+
+        EXPECT_EQUAL(server_conn->secure.server_ecc_evp_params.negotiated_curve, s2n_ecc_evp_supported_curves_list[1]);
+        EXPECT_SUCCESS(s2n_connection_free(server_conn));
+
+    }
+
+    {
+        /* If the list of mutually supported groups has several matches, chosen curve should be set to the first
+         * match.
+         */
+        EXPECT_NOT_NULL(server_conn = s2n_connection_new(S2N_SERVER));
+        EXPECT_NULL(server_conn->secure.server_ecc_evp_params.negotiated_curve);
+        for (int i = 0; i < S2N_ECC_EVP_SUPPORTED_CURVES_COUNT; i++) {
+            server_conn->secure.mutually_supported_groups[i].negotiated_curve = s2n_ecc_evp_supported_curves_list[i];
+        }
+
+        EXPECT_SUCCESS(s2n_choose_supported_group(&server_conn->secure.server_ecc_evp_params, server_conn->secure.mutually_supported_groups));
+
+        EXPECT_EQUAL(server_conn->secure.server_ecc_evp_params.negotiated_curve, s2n_ecc_evp_supported_curves_list[0]);
+        EXPECT_SUCCESS(s2n_connection_free(server_conn));
+
+    }
+
+    END_TEST();
+    return 0;
+
+}
---
+++ tests/unit/s2n_parse_client_supported_groups_list_test.c
@@ -0,0 +1,95 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+#include "s2n_test.h"
+#include "testlib/s2n_testlib.h"
+#include "tls/s2n_tls.h"
+#include "tls/s2n_tls13.h"
+#include "utils/s2n_blob.h"
+#include "tls/extensions/s2n_client_supported_groups.h"
+
+int main(int argc, char **argv)
+{
+    struct s2n_connection *server_conn;
+    struct s2n_blob iana_ids;
+    struct s2n_stuffer out;
+
+    BEGIN_TEST();
+    
+    EXPECT_SUCCESS(s2n_enable_tls13());
+
+    /* These tests check how the server parses the supported groups sent by the client */
+    {
+        /* If the client sent a supported group that the server also supports, mutually_supported_groups
+         * should contain the sent group.
+         */
+        uint8_t data[2];
+        EXPECT_SUCCESS(s2n_blob_init(&iana_ids, data, sizeof(data)));
+        EXPECT_SUCCESS(s2n_stuffer_init(&out, &iana_ids));
+        EXPECT_SUCCESS(s2n_stuffer_write_uint16(&out, s2n_ecc_evp_supported_curves_list[0]->iana_id));
+        EXPECT_NOT_NULL(server_conn = s2n_connection_new(S2N_SERVER));
+        for (int i = 0; i < S2N_ECC_EVP_SUPPORTED_CURVES_COUNT; i++) {
+            EXPECT_NULL(server_conn->secure.mutually_supported_groups[i].negotiated_curve);
+        }
+        
+        EXPECT_SUCCESS(s2n_parse_client_supported_groups_list(&iana_ids, server_conn->secure.mutually_supported_groups));
+        
+        EXPECT_EQUAL(server_conn->secure.mutually_supported_groups[0].negotiated_curve, s2n_ecc_evp_supported_curves_list[0]);
+        EXPECT_NULL(server_conn->secure.mutually_supported_groups[1].negotiated_curve);
+        
+        EXPECT_SUCCESS(s2n_connection_free(server_conn)); 
+    }
+
+    { 
+        /* If the client sent no supported groups at all, mutually_supported_groups should contain
+        * NULL values and no supported group should be chosen.
+        */
+        uint8_t data[2];
+        EXPECT_SUCCESS(s2n_blob_init(&iana_ids, data, sizeof(data)));
+        EXPECT_NOT_NULL(server_conn = s2n_connection_new(S2N_SERVER));
+        
+        EXPECT_SUCCESS(s2n_parse_client_supported_groups_list(&iana_ids, server_conn->secure.mutually_supported_groups));
+        
+        for (int i = 0; i < S2N_ECC_EVP_SUPPORTED_CURVES_COUNT; i++) {
+            EXPECT_NULL(server_conn->secure.mutually_supported_groups[i].negotiated_curve);
+        }
+        EXPECT_SUCCESS(s2n_connection_free(server_conn)); 
+    }
+
+    {
+        /* If the client has sent one mutually supported group and several groups the server does not support,
+        * mutually_supported_groups should contain only the group that the server supports.
+        */
+
+        uint8_t data[6];
+        EXPECT_SUCCESS(s2n_blob_init(&iana_ids, data, sizeof(data)));
+        EXPECT_SUCCESS(s2n_stuffer_init(&out, &iana_ids));
+        /* 17 and 18 are unsupported ids */
+        EXPECT_SUCCESS(s2n_stuffer_write_uint16(&out, 17));
+        EXPECT_SUCCESS(s2n_stuffer_write_uint16(&out, 18));
+        EXPECT_SUCCESS(s2n_stuffer_write_uint16(&out, s2n_ecc_evp_supported_curves_list[1]->iana_id));
+
+        EXPECT_NOT_NULL(server_conn = s2n_connection_new(S2N_SERVER));
+                
+        EXPECT_SUCCESS(s2n_parse_client_supported_groups_list(&iana_ids, server_conn->secure.mutually_supported_groups));
+
+        EXPECT_NULL(server_conn->secure.mutually_supported_groups[0].negotiated_curve);
+        EXPECT_EQUAL(server_conn->secure.mutually_supported_groups[1].negotiated_curve, s2n_ecc_evp_supported_curves_list[1]);
+        EXPECT_SUCCESS(s2n_connection_free(server_conn)); 
+    }
+
+    END_TEST();
+    return 0;
+}
