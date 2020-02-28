4f62863eed988035ee2f730475c9161094e9952f
Madeleine <madelea@amazon.com>
2020-02-28 13:34:36 -0800

    Added better description of s2n_blob members

--- utils/s2n_blob.h
+++ utils/s2n_blob.h
@@ -28,9 +28,11 @@ struct s2n_blob {
     /* The size of the data */
     uint32_t size;
 
-    /* The amount of memory allocated for this blob. If this blob was 
-    created with s2n_blob_init(), this value is 0. If s2n_alloc() was called,
-    this value will be greater than 0. */
+    /* The amount of memory allocated for this blob (i.e. the amount of memory
+     * which needs to be freed when the blob is cleaned up). If this blob was 
+     * created with s2n_blob_init(), this value is 0. If s2n_alloc() was called,
+     * this value will be greater than 0. 
+     */
     uint32_t allocated;
 
     /* Is the data mlocked */
