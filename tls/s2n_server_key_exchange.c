d78e7baa61a0ecfee607aad908fc34851a68cc52
Joshua Koo <jokoo@amazon.com>
2020-03-31 16:44:12 -0700

    More null checks

--- tls/s2n_server_key_exchange.c
+++ tls/s2n_server_key_exchange.c
@@ -37,9 +37,12 @@ static int s2n_write_signature_blob(struct s2n_stuffer *out, const struct s2n_pk
 
 int s2n_server_key_recv(struct s2n_connection *conn)
 {
+    notnull_check(conn);
+    notnull_check(conn->secure.cipher_suite);
+    notnull_check(conn->secure.cipher_suite->key_exchange_alg);
+
     struct s2n_hash_state *signature_hash = &conn->secure.signature_hash;
     const struct s2n_kex *key_exchange = conn->secure.cipher_suite->key_exchange_alg;
-    notnull_check(key_exchange);
     struct s2n_stuffer *in = &conn->handshake.io;
     struct s2n_blob data_to_verify = {0};
 
