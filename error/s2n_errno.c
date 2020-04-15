4caa406c233b57e6f0bb7d5a62c83aca237b7e31
Matthew Baldwin <baldwinmatt@users.noreply.github.com>
2020-04-15 16:32:13 -0700

    Fix the off-by-one on TLS1.3 record sizing (#1780)
    * Set S2N_LARGE_RECORD_LENGTH to S2N_TLS13_MAXIMUM_RECORD_LENGTH
    
    * Add tls13 self-talk tests
    
    1. Add `s2n_is_tls_supported()` to ensure we can negotiate TLS1.3 before running tests
    2. Update `s2n_self_talk_nonblocking_test` to cover more scenarios
    3. Add basic tls13 self-talk test
    
    `self_talk_nonblocking_test` is updated to test the following scenarios
    
    - TLS1.2 + iovec + low latency
    - TLS1.2 + no-iovec + low latency
    - TLS1.2 + iovec + throughput
    - TLS1.2 + no-iovec + throughput
    - TLS1,3 + iovec + low latency
    - TLS1,3 + no-iovec + low latency
    - TLS1,3 + iovec + throughput
    - TLS1,3 + no-iovec + throughput
    
    * Update max record lengths in accordance with the RFC
    
    * Restore previous kwstyle limit to 235 for max function length

--- error/s2n_errno.c
+++ error/s2n_errno.c
@@ -158,6 +158,7 @@ static const char *no_such_error = "Internal s2n error";
     ERR_ENTRY(S2N_ERR_FREE_STATIC_BLOB, "Cannot free a static blob") \
     ERR_ENTRY(S2N_ERR_RESIZE_STATIC_BLOB, "Cannot resize a static blob") \
     ERR_ENTRY(S2N_ERR_NO_AVAILABLE_BORINGSSL_API, "BoringSSL does not support this API") \
+    ERR_ENTRY(S2N_ERR_RECORD_LENGTH_TOO_LARGE, "Record length exceeds protocol version maximum") \
     ERR_ENTRY(S2N_ERR_NO_ALERT, "No Alert present") \
     ERR_ENTRY(S2N_ERR_CLIENT_MODE, "operation not allowed in client mode") \
     ERR_ENTRY(S2N_ERR_CLIENT_MODE_DISABLED, "client connections not allowed") \
