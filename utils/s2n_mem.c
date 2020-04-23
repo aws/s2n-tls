03ae8312ab9b78cefb3c34bb6e536cc0a04a92b1
Colm MacCárthaigh <colmmacc@amazon.com>
2020-04-23 05:54:24 +0000

    Revert "Revert "Zero memory as we release it""
    This reverts commit 7735ef0ded9c10ab92a28b18aabbc46716747206.

--- utils/s2n_mem.c
+++ utils/s2n_mem.c
@@ -161,6 +161,12 @@ int s2n_realloc(struct s2n_blob *b, uint32_t size)
 
     /* blob already has space for the request */
     if (size < b->allocated) {
+
+        /* Zero the memory before the we release it */
+        struct s2n_blob slice;
+        GUARD(s2n_blob_slice(b, &slice, size, b->size - size));
+        GUARD(s2n_blob_zero(&slice));
+
         b->size = size;
         return S2N_SUCCESS;
     }
