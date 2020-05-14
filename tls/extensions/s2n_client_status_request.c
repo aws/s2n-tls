44f36bfdb49d9d81f74c9fc50a7333e879f1bf3a
Lindsay Stewart <slindsay@amazon.com>
2020-05-14 01:17:02 -0700

    Add s2n_client_status_request_extension struct

--- tls/extensions/s2n_client_status_request.c
+++ tls/extensions/s2n_client_status_request.c
@@ -22,29 +22,70 @@
 
 #include "utils/s2n_safety.h"
 
-int s2n_extensions_client_status_request_send(struct s2n_connection *conn, struct s2n_stuffer *out)
-{
-    GUARD(s2n_stuffer_write_uint16(out, TLS_EXTENSION_STATUS_REQUEST));
-    GUARD(s2n_stuffer_write_uint16(out, 5));
-    GUARD(s2n_stuffer_write_uint8(out, (uint8_t) conn->config->status_request_type));
-    GUARD(s2n_stuffer_write_uint16(out, 0));
-    GUARD(s2n_stuffer_write_uint16(out, 0));
+static bool s2n_client_status_request_should_send(struct s2n_connection *conn);
+static int s2n_client_status_request_send(struct s2n_connection *conn, struct s2n_stuffer *out);
+static int s2n_client_status_request_recv(struct s2n_connection *conn, struct s2n_stuffer *extension);
 
-    return 0;
+const s2n_extension_type s2n_client_status_request_extension = {
+    .iana_value = TLS_EXTENSION_STATUS_REQUEST,
+    .is_response = false,
+    .send = s2n_client_status_request_send,
+    .recv = s2n_client_status_request_recv,
+    .should_send = s2n_client_status_request_should_send,
+    .if_missing = s2n_extension_noop_if_missing,
+};
+
+static bool s2n_client_status_request_should_send(struct s2n_connection *conn)
+{
+    return conn->config->status_request_type != S2N_STATUS_REQUEST_NONE;
 }
 
-int s2n_recv_client_status_request(struct s2n_connection *conn, struct s2n_stuffer *extension)
+static int s2n_client_status_request_send(struct s2n_connection *conn, struct s2n_stuffer *out)
+{
+    GUARD(s2n_stuffer_write_uint8(out, (uint8_t) conn->config->status_request_type));
+
+    /* responder_id_list
+     *
+     * From https://tools.ietf.org/html/rfc6066#section-8:
+     * A zero-length "responder_id_list" sequence has the special meaning that the responders are implicitly
+     * known to the server, e.g., by prior arrangement */
+    GUARD(s2n_stuffer_write_uint16(out, 0));
+
+    /* request_extensions
+     *
+     * From https://tools.ietf.org/html/rfc6066#section-8:
+     * A zero-length "request_extensions" value means that there are no extensions. */
+    GUARD(s2n_stuffer_write_uint16(out, 0));
+
+    return S2N_SUCCESS;
+}
+
+static int s2n_client_status_request_recv(struct s2n_connection *conn, struct s2n_stuffer *extension)
 {
     if (s2n_stuffer_data_available(extension) < 5) {
         /* Malformed length, ignore the extension */
-        return 0;
+        return S2N_SUCCESS;
     }
+
     uint8_t type;
     GUARD(s2n_stuffer_read_uint8(extension, &type));
     if (type != (uint8_t) S2N_STATUS_REQUEST_OCSP) {
         /* We only support OCSP (type 1), ignore the extension */
-        return 0;
+        return S2N_SUCCESS;
     }
+
     conn->status_type = (s2n_status_request_type) type;
-    return 0;
+    return S2N_SUCCESS;
+}
+
+/* Old-style extension functions -- remove after extensions refactor is complete */
+
+int s2n_extensions_client_status_request_send(struct s2n_connection *conn, struct s2n_stuffer *out)
+{
+    return s2n_extension_send(&s2n_client_status_request_extension, conn, out);
+}
+
+int s2n_recv_client_status_request(struct s2n_connection *conn, struct s2n_stuffer *extension)
+{
+    return s2n_extension_recv(&s2n_client_status_request_extension, conn, extension);
 }
