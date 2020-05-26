1a0ed74ce6a0352cb45aa09b928b146e5865b87b
Ryan Day <rynday@amazon.com>
2020-05-26 23:32:16 +0000

    Fix cipher suite match bug when linking against BoringSSL.

--- tls/s2n_cipher_suites.h
+++ tls/s2n_cipher_suites.h
@@ -38,8 +38,13 @@
 #endif
 
 /* Kept up-to-date by s2n_cipher_suite_match_test */
-#define S2N_CIPHER_SUITE_COUNT          (36 + S2N_PQ_CIPHER_SUITE_COUNT)
+#ifdef OPENSSL_IS_BORINGSSL
+    #define S2N_CIPHER_SUITE_COUNT          (32 + S2N_PQ_CIPHER_SUITE_COUNT)
+#else
+    #define S2N_CIPHER_SUITE_COUNT          (36 + S2N_PQ_CIPHER_SUITE_COUNT)
+#endif
 #define S2N_FIPS_CIPHER_SUITE_COUNT     (17 + S2N_PQ_CIPHER_SUITE_COUNT)
+
 
 /* Record algorithm flags that can be OR'ed */
 #define S2N_TLS12_AES_GCM_AEAD_NONCE     0x01
