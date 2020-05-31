10531f5a475c56d13917e5ff5c69479f5fe43699
Tejeswini Sundaram <ttjsu@amazon.com>
2020-05-31 12:26:19 -0700

    Adding security policy for TLS1.3 with ecc_preferences_20140601

--- tests/unit/s2n_security_policies_test.c
+++ tests/unit/s2n_security_policies_test.c
@@ -174,6 +174,8 @@ int main(int argc, char **argv)
             "default_tls13",
             "test_all",
             "test_all_tls13",
+            "20190801",
+            "20190802"
         };
         for (size_t i = 0; i < s2n_array_len(tls13_security_policy_strings); i++) {
             security_policy = NULL;
