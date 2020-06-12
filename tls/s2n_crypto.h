aa8d23d0abb448f7eebac135ee7818c4c391259b
Brandon Butch <bbutch@amazon.com>
2020-06-12 15:24:34 -0700

    Move defined constants from s2n_crypto.h to s2n_crypto_parameters.h

--- tls/s2n_crypto.h
+++ tls/s2n_crypto.h
@@ -17,6 +17,7 @@
 
 #include "tls/s2n_config.h"
 #include "tls/s2n_signature_scheme.h"
+#include "tls/s2n_crypto_parameters.h"
 
 #include "crypto/s2n_certificate.h"
 #include "crypto/s2n_cipher.h"
@@ -27,38 +28,6 @@
 #include "crypto/s2n_tls13_keys.h"
 #include "crypto/s2n_dhe.h"
 #include "crypto/s2n_ecc_evp.h"
-
-#define S2N_TLS_SECRET_LEN             48
-#define S2N_TLS_RANDOM_DATA_LEN        32
-#define S2N_TLS_SEQUENCE_NUM_LEN        8
-#define S2N_TLS_CIPHER_SUITE_LEN        2
-#define S2N_SSLv2_CIPHER_SUITE_LEN      3
-#define S2N_TLS_FINISHED_LEN           12
-#define S2N_SSL_FINISHED_LEN           36
-#define S2N_TLS_MAX_IV_LEN             16
-
-/* From RFC 5246 6.2.3.3 */
-#define S2N_TLS12_AAD_LEN              13
-#define S2N_TLS_MAX_AAD_LEN            S2N_TLS12_AAD_LEN
-#define S2N_TLS_GCM_FIXED_IV_LEN        4
-#define S2N_TLS_GCM_EXPLICIT_IV_LEN     8
-#define S2N_TLS_GCM_IV_LEN            (S2N_TLS_GCM_FIXED_IV_LEN + S2N_TLS_GCM_EXPLICIT_IV_LEN)
-#define S2N_TLS_GCM_TAG_LEN            16
-
-/* TLS 1.3 uses only implicit IVs - RFC 8446 5.3 */
-#define S2N_TLS13_AAD_LEN               5
-#define S2N_TLS13_RECORD_IV_LEN         0
-#define S2N_TLS13_FIXED_IV_LEN         12
-
-/* From RFC 7905 */
-#define S2N_TLS_CHACHA20_POLY1305_FIXED_IV_LEN    12
-#define S2N_TLS_CHACHA20_POLY1305_EXPLICIT_IV_LEN  0
-#define S2N_TLS_CHACHA20_POLY1305_IV_LEN          12
-#define S2N_TLS_CHACHA20_POLY1305_KEY_LEN         32
-#define S2N_TLS_CHACHA20_POLY1305_TAG_LEN         16
-
-/* RFC 5246 7.4.1.2 */
-#define S2N_TLS_SESSION_ID_MAX_LEN     32
 
 struct s2n_kem_params {
     const struct s2n_kem *kem;
