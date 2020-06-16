b80f0e1e891ec0e981df1215a0992b9d8f7c8114
Felipe R. Monteiro <felisous@amazon.com>
2020-06-16 02:00:06 -0400

    Adds a proof harness for s2n_stuffer_growable_alloc
    Signed-off-by: Felipe R. Monteiro <felisous@amazon.com>

---
+++ tests/cbmc/proofs/s2n_stuffer_growable_alloc/s2n_stuffer_growable_alloc_harness.c
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+#include "api/s2n.h"
+#include "error/s2n_errno.h"
+#include "stuffer/s2n_stuffer.h"
+#include "utils/s2n_mem.h"
+
+#include <assert.h>
+#include <cbmc_proof/proof_allocators.h>
+#include <cbmc_proof/make_common_datastructures.h>
+
+void s2n_stuffer_growable_alloc_harness() {
+    /* Non-deterministic inputs. */
+    struct s2n_stuffer *stuffer = nondet_bool() ? cbmc_allocate_s2n_stuffer(): NULL;
+    __CPROVER_assume(S2N_IMPLIES(stuffer != NULL, s2n_stuffer_is_valid(stuffer)));
+    uint32_t size;
+
+    /* Non-deterministically set initialized (in s2n_mem) to true. */
+    if(nondet_bool()) {
+        s2n_mem_init();
+    }
+
+    /* Operation under verification. */
+    if (s2n_stuffer_growable_alloc(stuffer, size) == S2N_SUCCESS) {
+        /* Post-conditions. */
+        assert(stuffer->growable == 1);
+        assert(stuffer->alloced == 1);
+        assert(stuffer->blob.size == size);
+        assert(s2n_stuffer_is_valid(stuffer));
+    }
+}
