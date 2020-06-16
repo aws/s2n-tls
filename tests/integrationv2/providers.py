88fc7d47c6185e96eaa78caf77fed940bf4df31b
Ryan Day <rynday@amazon.com>
2020-06-16 17:27:04 -0400

    Refactor the way providers pass options. (#2019)
    Update the README.

--- tests/integrationv2/providers.py
+++ tests/integrationv2/providers.py
@@ -30,19 +30,19 @@ class Provider(object):
             raise TypeError
 
         self.options = options
-        if options.mode == Provider.ServerMode:
-            self.cmd_line = self.setup_server(options)
-        elif options.mode == Provider.ClientMode:
-            self.cmd_line = self.setup_client(options)
+        if self.options.mode == Provider.ServerMode:
+            self.cmd_line = self.setup_server()
+        elif self.options.mode == Provider.ClientMode:
+            self.cmd_line = self.setup_client()
 
-    def setup_client(self, options: ProviderOptions):
+    def setup_client(self):
         """
         Provider specific setup code goes here.
         This will probably include creating the command line based on ProviderOptions.
         """
         raise NotImplementedError
 
-    def setup_server(self, options: ProviderOptions):
+    def setup_server(self):
         """
         Provider specific setup code goes here.
         This will probably include creating the command line based on ProviderOptions.
@@ -72,9 +72,9 @@ class Tcpdump(Provider):
     def __init__(self, options: ProviderOptions):
         Provider.__init__(self, options)
 
-    def setup_client(self, options: ProviderOptions):
+    def setup_client(self):
         self.ready_to_test_marker = 'listening on lo'
-        tcpdump_filter = "dst port {}".format(options.port)
+        tcpdump_filter = "dst port {}".format(self.options.port)
 
         cmd_line = ["tcpdump",
             # Line buffer the output
@@ -105,7 +105,7 @@ class S2N(Provider):
         self.ready_to_send_input_marker = None
         Provider.__init__(self, options)
 
-    def setup_client(self, options: ProviderOptions):
+    def setup_client(self):
         """
         Using the passed ProviderOptions, create a command line.
         """
@@ -114,77 +114,77 @@ class S2N(Provider):
         # This is the last thing printed by s2nc before it is ready to send/receive data
         self.ready_to_send_input_marker = 'Cipher negotiated:'
 
-        if options.use_session_ticket is False:
+        if self.options.use_session_ticket is False:
             cmd_line.append('-T')
 
-        if options.insecure is True:
+        if self.options.insecure is True:
             cmd_line.append('--insecure')
-        elif options.client_trust_store is not None:
-            cmd_line.extend(['-f', options.client_trust_store])
+        elif self.options.client_trust_store is not None:
+            cmd_line.extend(['-f', self.options.client_trust_store])
         else:
-            if options.cert is not None:
-                cmd_line.extend(['-f', options.cert])
+            if self.options.cert is not None:
+                cmd_line.extend(['-f', self.options.cert])
 
-        if options.reconnect is True:
+        if self.options.reconnect is True:
             cmd_line.append('-r')
 
-        if options.protocol == Protocols.TLS13:
+        if self.options.protocol == Protocols.TLS13:
             cmd_line.append('--tls13')
 
-        if options.cipher is not None:
-            if options.cipher is Ciphers.KMS_PQ_TLS_1_0_2019_06:
+        if self.options.cipher is not None:
+            if self.options.cipher is Ciphers.KMS_PQ_TLS_1_0_2019_06:
                 cmd_line.extend(['-c', 'KMS-PQ-TLS-1-0-2019-06'])
-            elif options.cipher is Ciphers.PQ_SIKE_TEST_TLS_1_0_2019_11:
+            elif self.options.cipher is Ciphers.PQ_SIKE_TEST_TLS_1_0_2019_11:
                 cmd_line.extend(['-c', 'PQ-SIKE-TEST-TLS-1-0-2019-11'])
             else:
                 cmd_line.extend(['-c', 'test_all'])
         else:
             cmd_line.extend(['-c', 'test_all'])
 
-        if options.client_key_file:
-            cmd_line.extend(['--key', options.client_key_file])
-        if options.client_certificate_file:
-            cmd_line.extend(['--cert', options.client_certificate_file])
+        if self.options.client_key_file:
+            cmd_line.extend(['--key', self.options.client_key_file])
+        if self.options.client_certificate_file:
+            cmd_line.extend(['--cert', self.options.client_certificate_file])
 
-        if options.extra_flags is not None:
-            cmd_line.extend(options.extra_flags)
+        if self.options.extra_flags is not None:
+            cmd_line.extend(self.options.extra_flags)
 
-        cmd_line.extend([options.host, options.port])
+        cmd_line.extend([self.options.host, self.options.port])
 
         # Clients are always ready to connect
         self.set_provider_ready()
 
         return cmd_line
 
-    def setup_server(self, options: ProviderOptions):
+    def setup_server(self):
         """
         Using the passed ProviderOptions, create a command line.
         """
         cmd_line = ['s2nd', '-X', '--self-service-blinding']
 
-        if options.key is not None:
-            cmd_line.extend(['--key', options.key])
-        if options.cert is not None:
-            cmd_line.extend(['--cert', options.cert])
+        if self.options.key is not None:
+            cmd_line.extend(['--key', self.options.key])
+        if self.options.cert is not None:
+            cmd_line.extend(['--cert', self.options.cert])
 
-        if options.insecure is True:
+        if self.options.insecure is True:
             cmd_line.append('--insecure')
 
-        if options.protocol == Protocols.TLS13:
+        if self.options.protocol == Protocols.TLS13:
             cmd_line.append('--tls13')
 
         cmd_line.extend(['-c', 'test_all'])
-        if options.use_client_auth is True:
+        if self.options.use_client_auth is True:
             cmd_line.append('-m')
-            cmd_line.extend(['-t', options.client_certificate_file])
+            cmd_line.extend(['-t', self.options.client_certificate_file])
 
-        if options.reconnects_before_exit is not None:
-            cmd_line.append('--max-conns={}'.format(options.reconnects_before_exit))
+        if self.options.reconnects_before_exit is not None:
+            cmd_line.append('--max-conns={}'.format(self.options.reconnects_before_exit))
 
-        if options.extra_flags is not None:
-            cmd_line.extend(options.extra_flags)
+        if self.options.extra_flags is not None:
+            cmd_line.extend(self.options.extra_flags)
 
-        cmd_line.extend([options.host, options.port])
+        cmd_line.extend([self.options.host, self.options.port])
 
         return cmd_line
 
@@ -238,69 +238,68 @@ class OpenSSL(Provider):
 
         return cmdline + ciphers
 
-    def setup_client(self, options: ProviderOptions):
+    def setup_client(self):
         # s_client prints this message before it is ready to send/receive data
         self.ready_to_send_input_marker = 'Verify return code'
 
         cmd_line = ['openssl', 's_client']
-        cmd_line.extend(['-connect', '{}:{}'.format(options.host, options.port)])
+        cmd_line.extend(['-connect', '{}:{}'.format(self.options.host, self.options.port)])
 
         # Additional debugging that will be captured incase of failure
         cmd_line.extend(['-debug', '-tlsextdebug'])
 
-        if options.cert is not None:
-            cmd_line.extend(['-cert', options.cert])
-        if options.key is not None:
-            cmd_line.extend(['-key', options.key])
+        if self.options.cert is not None:
+            cmd_line.extend(['-cert', self.options.cert])
+        if self.options.key is not None:
+            cmd_line.extend(['-key', self.options.key])
 
         # Unlike s2n, OpenSSL allows us to be much more specific about which TLS
         # protocol to use.
-        if options.protocol == Protocols.TLS13:
+        if self.options.protocol == Protocols.TLS13:
             cmd_line.append('-tls1_3')
-        elif options.protocol == Protocols.TLS12:
+        elif self.options.protocol == Protocols.TLS12:
             cmd_line.append('-tls1_2')
-        elif options.protocol == Protocols.TLS11:
+        elif self.options.protocol == Protocols.TLS11:
             cmd_line.append('-tls1_1')
-        elif options.protocol == Protocols.TLS10:
+        elif self.options.protocol == Protocols.TLS10:
             cmd_line.append('-tls1')
 
-        if options.cipher is not None:
-            cmd_line.extend(self._cipher_to_cmdline(options.protocol, options.cipher))
+        if self.options.cipher is not None:
+            cmd_line.extend(self.cipher_to_cmdline(self.options.protocol, self.options.cipher))
 
-        if options.curve is not None:
-            cmd_line.extend(['-curves', str(options.curve)])
+        if self.options.curve is not None:
+            cmd_line.extend(['-curves', str(self.options.curve)])
 
-        if options.use_client_auth is True:
-            cmd_line.extend(['-key', options.client_key_file])
-            cmd_line.extend(['-cert', options.client_certificate_file])
+        if self.options.use_client_auth is True:
+            cmd_line.extend(['-key', self.options.client_key_file])
+            cmd_line.extend(['-cert', self.options.client_certificate_file])
 
-        if options.reconnect is True:
+        if self.options.reconnect is True:
             cmd_line.append('-reconnect')
 
-        if options.server_name is not None:
-            cmd_line.extend(['-servername', options.server_name])
-            if options.verify_hostname is not None:
-                cmd_line.extend(['-verify_hostname', options.server_name])
+        if self.options.extra_flags is not None:
+            cmd_line.extend(self.options.extra_flags)
 
-
-        if options.extra_flags is not None:
-            cmd_line.extend(options.extra_flags)
+        if self.options.server_name is not None:
+            cmd_line.extend(['-servername', self.options.server_name])
+            if self.options.verify_hostname is not None:
+                cmd_line.extend(['-verify_hostname', self.options.server_name])
 
         # Clients are always ready to connect
         self.set_provider_ready()
 
         return cmd_line
 
-    def setup_server(self, options: ProviderOptions):
+    def setup_server(self):
         # s_server prints this message before it is ready to send/receive data
         self.ready_to_test_marker = 'ACCEPT'
 
         cmd_line = ['openssl', 's_server']
-        cmd_line.extend(['-accept', '{}:{}'.format(options.host, options.port)])
+        cmd_line.extend(['-accept', '{}:{}'.format(self.options.host, self.options.port)])
 
-        if options.reconnects_before_exit is not None:
+        if self.options.reconnects_before_exit is not None:
             # If the user request a specific reconnection count, set it here
-            cmd_line.extend(['-naccept', str(options.reconnects_before_exit)])
+            cmd_line.extend(['-naccept', str(self.options.reconnects_before_exit)])
         else:
             # Exit after the first connection by default
             cmd_line.extend(['-naccept', '1'])
@@ -310,30 +309,30 @@ class OpenSSL(Provider):
 
         cmd_line.append('-state')
 
-        if options.cert is not None:
-            cmd_line.extend(['-cert', options.cert])
-        if options.key is not None:
-            cmd_line.extend(['-key', options.key])
+        if self.options.cert is not None:
+            cmd_line.extend(['-cert', self.options.cert])
+        if self.options.key is not None:
+            cmd_line.extend(['-key', self.options.key])
 
         # Unlike s2n, OpenSSL allows us to be much more specific about which TLS
         # protocol to use.
-        if options.protocol == Protocols.TLS13:
+        if self.options.protocol == Protocols.TLS13:
             cmd_line.append('-tls1_3')
-        elif options.protocol == Protocols.TLS12:
+        elif self.options.protocol == Protocols.TLS12:
             cmd_line.append('-tls1_2')
-        elif options.protocol == Protocols.TLS11:
+        elif self.options.protocol == Protocols.TLS11:
             cmd_line.append('-tls1_1')
-        elif options.protocol == Protocols.TLS10:
+        elif self.options.protocol == Protocols.TLS10:
             cmd_line.append('-tls1')
 
-        if options.cipher is not None:
-            cmd_line.extend(self._cipher_to_cmdline(options.protocol, options.cipher))
-            if options.cipher.parameters is not None:
-                cmd_line.extend(['-dhparam', options.cipher.parameters])
+        if self.options.cipher is not None:
+            cmd_line.extend(self.cipher_to_cmdline(self.options.protocol, self.options.cipher))
+            if self.options.cipher.parameters is not None:
+                cmd_line.extend(['-dhparam', self.options.cipher.parameters])
 
-        if options.curve is not None:
-            cmd_line.extend(['-curves', str(options.curve)])
-        if options.use_client_auth is True:
+        if self.options.curve is not None:
+            cmd_line.extend(['-curves', str(self.options.curve)])
+        if self.options.use_client_auth is True:
             cmd_line.extend(['-verify', '1'])
 
         return cmd_line
@@ -349,31 +348,31 @@ class BoringSSL(Provider):
         self.ready_to_send_input_marker = None
         Provider.__init__(self, options)
 
-    def setup_server(self, options: ProviderOptions):
+    def setup_server(self):
         pytest.skip('BoringSSL does not support server mode at this time')
 
-    def setup_client(self, options: ProviderOptions):
+    def setup_client(self):
         self.ready_to_send_input_marker = 'Cert issuer:'
         cmd_line = ['bssl', 's_client']
-        cmd_line.extend(['-connect', '{}:{}'.format(options.host, options.port)])
-        if options.cert is not None:
-            cmd_line.extend(['-cert', options.cert])
-        if options.key is not None:
-            cmd_line.extend(['-key', options.key])
-        if options.cipher is not None:
-            if options.cipher == Ciphersuites.TLS_CHACHA20_POLY1305_SHA256:
+        cmd_line.extend(['-connect', '{}:{}'.format(self.options.host, self.options.port)])
+        if self.options.cert is not None:
+            cmd_line.extend(['-cert', self.options.cert])
+        if self.options.key is not None:
+            cmd_line.extend(['-key', self.options.key])
+        if self.options.cipher is not None:
+            if self.options.cipher == Ciphersuites.TLS_CHACHA20_POLY1305_SHA256:
                 cmd_line.extend(['-cipher', 'TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256'])
-            elif options.cipher == Ciphersuites.TLS_AES_128_GCM_256:
-                pytest.skip('BoringSSL does not support Cipher {}'.format(options.cipher))
-            elif options.cipher == Ciphersuites.TLS_AES_256_GCM_384:
-                pytest.skip('BoringSSL does not support Cipher {}'.format(options.cipher))
-        if options.curve is not None:
-            if options.curve == Curves.P256:
+            elif self.options.cipher == Ciphersuites.TLS_AES_128_GCM_256:
+                pytest.skip('BoringSSL does not support Cipher {}'.format(self.options.cipher))
+            elif self.options.cipher == Ciphersuites.TLS_AES_256_GCM_384:
+                pytest.skip('BoringSSL does not support Cipher {}'.format(self.options.cipher))
+        if self.options.curve is not None:
+            if self.options.curve == Curves.P256:
                 cmd_line.extend(['-curves', 'P-256'])
-            elif options.curve == Curves.P384:
+            elif self.options.curve == Curves.P384:
                 cmd_line.extend(['-curves', 'P-384'])
-            elif options.curve == Curves.X25519:
-                pytest.skip('BoringSSL does not support curve {}'.format(options.curve))
+            elif self.options.curve == Curves.X25519:
+                pytest.skip('BoringSSL does not support curve {}'.format(self.options.curve))
 
         # Clients are always ready to connect
         self.set_provider_ready()
