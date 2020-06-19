40c47d342baa5348713888a3a9234fbf7f21b03f
Ryan Day <rynday@amazon.com>
2020-06-19 15:33:15 +0000

    Larger refactors to handle ciphers passed to OpenSSL. Update a cipher match in the SNI test which wasn't being used previously.

--- tests/integrationv2/configuration.py
+++ tests/integrationv2/configuration.py
@@ -260,7 +260,7 @@ MULTI_CERT_TEST_CASES= [
                     " even if domain mismatched RSA certificate is available and RSA cipher is higher priority.",
         server_certs=[SNI_CERTS["beaver"], SNI_CERTS["alligator_ecdsa"]],
         client_sni="www.alligator.com",
-        client_ciphers=[Ciphers.ECDHE_RSA_AES128_SHA, Ciphers.ECDHE_ECDSA_AES128_SHA256],
+        client_ciphers=[Ciphers.ECDHE_RSA_AES128_SHA256, Ciphers.ECDHE_ECDSA_AES128_SHA256],
         expected_cert=SNI_CERTS["alligator_ecdsa"],
         expect_matching_hostname=True),
     MultiCertTest(
