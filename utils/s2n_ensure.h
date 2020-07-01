ca62a172a1ebcb9fd336dec1f6cbffa688cb7d06
Matthew Baldwin <baldwinm@amazon.com>
2020-07-01 20:09:19 +0000

    Add (un)?likely macros for branch prediction on ensure condition

--- utils/s2n_ensure.h
+++ utils/s2n_ensure.h
@@ -15,6 +15,9 @@
 
 #pragma once
 
+#define s2n_likely(x) __builtin_expect(!!(x), 1)
+#define s2n_unlikely(x) __builtin_expect(!!(x), 0)
+
 /**
  * s2n_ensure provides low-level safety check functionality
  *
@@ -29,12 +32,14 @@
  */
 #define __S2N_ENSURE( cond, action ) do {if ( !(cond) ) { action; }} while (0)
 
-#define __S2N_ENSURE_CONDITION( cond, action ) __S2N_ENSURE( cond, action )
+#define __S2N_ENSURE_LIKELY( cond, action ) do {if ( s2n_unlikely( !(cond) ) ) { action; }} while (0)
+
+#define __S2N_ENSURE_CONDITION( cond, action ) __S2N_ENSURE_LIKELY( cond, action )
 
 #define __S2N_ENSURE_SAFE_MEMCPY( d , s , n , guard )                            \
   do {                                                                           \
     __typeof( n ) __tmp_n = ( n );                                               \
-    if ( __tmp_n ) {                                                             \
+    if ( s2n_likely( __tmp_n ) ) {                                               \
       void *r = s2n_ensure_memcpy_trace( (d), (s) , (__tmp_n), _S2N_DEBUG_LINE); \
       guard(r);                                                                  \
     }                                                                            \
@@ -43,7 +48,7 @@
 #define __S2N_ENSURE_SAFE_MEMSET( d , c , n , guard )                            \
   do {                                                                           \
     __typeof( n ) __tmp_n = ( n );                                               \
-    if ( __tmp_n ) {                                                             \
+    if ( s2n_likely( __tmp_n ) ) {                                               \
       __typeof( d ) __tmp_d = ( d );                                             \
       guard( __tmp_d );                                                          \
       memset( __tmp_d, (c), __tmp_n);                                            \
