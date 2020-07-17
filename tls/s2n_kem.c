e30f489fbd5e29fdebdc03a57bfc14f52d6ffe11
Rodrigo Rubira Branco (BSDaemon) <rodrigo@kernelhacking.com>
2020-07-17 11:54:34 -0500

    Add a notnull_check in s2n_kem_encapsulate() (#2152)
    * Add a notnull_check in s2n_kem_encapsulate()
    
    * Add a notnull_check in s2n_kem_decapsulate
    
    Co-authored-by: Rodrigo Branco (BSDaemon) <robranco@amazon.com>

--- tls/s2n_kem.c
+++ tls/s2n_kem.c
@@ -157,6 +157,7 @@ int s2n_kem_generate_keypair(struct s2n_kem_params *kem_params)
 int s2n_kem_encapsulate(struct s2n_kem_params *kem_params, struct s2n_blob *ciphertext)
 {
     notnull_check(kem_params);
+    notnull_check(kem_params->kem);
     const struct s2n_kem *kem = kem_params->kem;
     notnull_check(kem->encapsulate);
 
@@ -176,6 +177,7 @@ int s2n_kem_encapsulate(struct s2n_kem_params *kem_params, struct s2n_blob *ciph
 int s2n_kem_decapsulate(struct s2n_kem_params *kem_params, const struct s2n_blob *ciphertext)
 {
     notnull_check(kem_params);
+    notnull_check(kem_params->kem);
     const struct s2n_kem *kem = kem_params->kem;
     notnull_check(kem->decapsulate);
 
