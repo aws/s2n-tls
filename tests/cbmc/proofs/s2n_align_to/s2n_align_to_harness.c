4a725f6da0c8afd83af810806e26db1aae1f5735
Audrey Valentine <audreval@amazon.com>
2020-07-24 20:19:37 +0000

    constant time proofs

---
+++ tests/cbmc/proofs/s2n_align_to/s2n_align_to_harness.c
@@ -0,0 +1,45 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+#include "api/s2n.h"
+#include "utils/s2n_safety.h"
+
+#include <assert.h>
+#include <cbmc_proof/proof_allocators.h>
+#include <cbmc_proof/make_common_datastructures.h>
+
+int s2n_align_to_harness()
+{
+    const uint32_t initial;
+    const uint32_t alignment;
+    /* Division and modulo are too slow in CBMC to perform all necessary checks,
+     * so relevant assertions that can't be used currently have been left in comments. */
+    uint32_t *out = can_fail_malloc(sizeof(uint32_t));
+ /* uint64_t result = (uint64_t) alignment * ((((uint64_t) initial - 1) / (uint64_t) alignment) + 1); */
+
+    if (s2n_align_to(initial, alignment, out) == S2N_SUCCESS) {
+        if (initial == 0) {
+            assert(*out == 0);
+        } else {
+         /* assert(*out >= initial); */
+         /* assert(*out < (uint64_t) initial + (uint64_t) alignment); */
+         /* assert(result <= UINT32_MAX); */
+         /* assert(*out % alignment == 0); */
+        }
+    } else {
+     /* assert(*out % alignment != 0 || out == NULL); */
+     /* assert(result > UINT32_MAX || out == NULL); */
+    }
+}
---
+++ tests/cbmc/proofs/s2n_constant_time_pkcs1_unpad_or_dont/s2n_constant_time_pkcs1_unpad_or_dont_harness.c
@@ -0,0 +1,68 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+#include "api/s2n.h"
+#include "utils/s2n_safety.h"
+
+#include <sys/param.h>
+#include <assert.h>
+
+#include <cbmc_proof/proof_allocators.h>
+
+void s2n_constant_time_pkcs1_unpad_or_dont_harness() {
+    /* Non-deterministic inputs. */
+    const uint32_t len;
+    const uint32_t destlen;
+    const uint32_t srclen;
+    const uint8_t dont;
+    __CPROVER_assume(len < MAX_ARR_LEN);
+    __CPROVER_assume(destlen >= len);
+
+    /* This bound is required for our loop unwindings to be reasonably bound. */
+    __CPROVER_assume(srclen >= len + 1 && srclen <= BOUND);
+    uint8_t * dest = can_fail_malloc(destlen);
+    uint8_t * src = can_fail_malloc(srclen);
+    uint8_t old_src_byte;
+    uint8_t old_dest_byte;
+    uint32_t index;
+
+    /* Pre-conditions. */
+    __CPROVER_assume(dest != NULL);
+    __CPROVER_assume(src != NULL);
+    if (len != 0) {
+        __CPROVER_assume(index < len);
+        old_src_byte = src[srclen - len + index];
+        old_dest_byte = dest[index];
+    }
+
+    s2n_constant_time_pkcs1_unpad_or_dont(dest, src, srclen, len);
+
+    if (len != 0) {
+        bool has_zero = false;
+        for (uint32_t i = 2; i < srclen - len - 1; i++) {
+            if(src[i] == 0x00) {
+                has_zero = true;
+            }
+        }
+
+        /* This statement is the inverse of the failure checks found in s2n_constant_time_pkcs1_unpad_or_dont. */
+        if (srclen >= (len + 3) && src[0] == 0x00 && src[1] == 0x02 && src[srclen - len - 1] == 0x00 && has_zero == false) {
+            assert(dest[index] == old_src_byte);
+        } else {
+            assert(dest[index] == old_dest_byte);
+        }
+        assert(src[srclen - len + index] == old_src_byte);
+    }
+}
