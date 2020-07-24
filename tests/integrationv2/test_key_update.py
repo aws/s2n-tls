b3ede4174f42c7137baeb709caacca506f3d1c19
Madeleine <madelea@amazon.com>
2020-07-24 11:53:40 -0700

    Rebased to fix failing Sidetrail

--- tests/integrationv2/test_key_update.py
+++ tests/integrationv2/test_key_update.py
@@ -1,20 +1,27 @@
 import copy
 import pytest
-import time
 
-from configuration import available_ports, Ciphers, ALL_TEST_CURVES, ALL_TEST_CERTS
+from configuration import available_ports, Ciphers
 from common import ProviderOptions, Protocols, data_bytes
 from fixtures import managed_process
 from providers import Provider, S2N, OpenSSL
-from utils import invalid_test_parameters, get_parameter_name, get_expected_s2n_version
+from utils import invalid_test_parameters
+
 
 @pytest.mark.uncollect_if(func=invalid_test_parameters)
 # These are the only ciphers that utilize the key update feature
-@pytest.mark.parametrize("cipher", [Ciphers.AES128_GCM_SHA256, Ciphers.AES256_GCM_SHA384, Ciphers.CHACHA20_POLY1305_SHA256])
+@pytest.mark.parametrize(
+    "cipher",
+    [
+        Ciphers.AES128_GCM_SHA256,
+        Ciphers.AES256_GCM_SHA384,
+        Ciphers.CHACHA20_POLY1305_SHA256,
+    ],
+)
 def test_s2n_server_key_update(managed_process, cipher):
     host = "localhost"
     port = next(available_ports)
-    
+
     update_requested = b"K"
     server_data = data_bytes(10)
     client_data = data_bytes(10)
@@ -28,19 +35,28 @@ def test_s2n_server_key_update(managed_process, cipher):
         host=host,
         port=port,
         cipher=cipher,
-        data_to_send= [update_requested, client_data],
+        data_to_send=[update_requested, client_data],
         insecure=True,
-        protocol=Protocols.TLS13)
+        protocol=Protocols.TLS13,
+    )
 
     server_options = copy.copy(client_options)
-    
+
     server_options.mode = Provider.ServerMode
     server_options.key = "../pems/ecdsa_p384_pkcs1_key.pem"
     server_options.cert = "../pems/ecdsa_p384_pkcs1_cert.pem"
     server_options.data_to_send = [server_data]
 
-    server = managed_process(S2N, server_options, send_marker=[str(client_data)], timeout=5)
-    client = managed_process(OpenSSL, client_options, send_marker=send_marker_list, close_marker=str(server_data), timeout=5)
+    server = managed_process(
+        S2N, server_options, send_marker=[str(client_data)], timeout=5
+    )
+    client = managed_process(
+        OpenSSL,
+        client_options,
+        send_marker=send_marker_list,
+        close_marker=str(server_data),
+        timeout=5,
+    )
 
     for results in client.get_results():
         assert results.exception is None
@@ -53,9 +69,17 @@ def test_s2n_server_key_update(managed_process, cipher):
         assert results.exit_code == 0
         assert client_data in results.stdout
 
+
 @pytest.mark.uncollect_if(func=invalid_test_parameters)
 # These are the only ciphers that utilize the key update feature
-@pytest.mark.parametrize("cipher", [Ciphers.AES128_GCM_SHA256, Ciphers.AES256_GCM_SHA384, Ciphers.CHACHA20_POLY1305_SHA256])
+@pytest.mark.parametrize(
+    "cipher",
+    [
+        Ciphers.AES128_GCM_SHA256,
+        Ciphers.AES256_GCM_SHA384,
+        Ciphers.CHACHA20_POLY1305_SHA256,
+    ],
+)
 def test_s2n_client_key_update(managed_process, cipher):
     host = "localhost"
     port = next(available_ports)
@@ -77,7 +101,8 @@ def test_s2n_client_key_update(managed_process, cipher):
         cipher=cipher,
         data_to_send=[client_data],
         insecure=True,
-        protocol=Protocols.TLS13)
+        protocol=Protocols.TLS13,
+    )
 
     server_options = copy.copy(client_options)
 
@@ -86,8 +111,20 @@ def test_s2n_client_key_update(managed_process, cipher):
     server_options.cert = "../pems/ecdsa_p384_pkcs1_cert.pem"
     server_options.data_to_send = [update_requested, server_data]
 
-    server = managed_process(OpenSSL, server_options, send_marker=send_marker_list, close_marker=str(client_data), timeout=5)
-    client = managed_process(S2N, client_options, send_marker=[str(server_data)], close_marker=str(server_data), timeout=5)
+    server = managed_process(
+        OpenSSL,
+        server_options,
+        send_marker=send_marker_list,
+        close_marker=str(client_data),
+        timeout=5,
+    )
+    client = managed_process(
+        S2N,
+        client_options,
+        send_marker=[str(server_data)],
+        close_marker=str(server_data),
+        timeout=5,
+    )
 
     for results in client.get_results():
         assert results.exception is None
