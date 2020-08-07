72b2ca061a42840fa4cebcb2b388b62b27a10cb8
Daniil Bondarev <bondarev@amazon.com>
2020-08-07 16:42:11 -0700

    Prevent failures on 2 subsequent warning alerts if S2N_ALERT_IGNORE_WARNINGS is set
    Previously when mode was set to S2N_ALERT_IGNORE_WARNINGS and warning
    was ignored, we did not clean up the conn->alert_in buffer, so when we
    received a second warning we would fail the connection with
    S2N_ERR_ALERT_PRESENT at the beginning of s2n_process_alert_fragment
    function.

--- tests/unit/s2n_self_talk_alerts_test.c
+++ tests/unit/s2n_self_talk_alerts_test.c
@@ -38,6 +38,7 @@
 struct alert_ctx {
     int write_fd;
     int invoked;
+    int count;
 
     uint8_t level;
     uint8_t code;
@@ -111,16 +112,18 @@ int mock_nanoseconds_since_epoch(void *data, uint64_t *nanoseconds)
     return 0;
 }
 
-int client_hello_send_alert(struct s2n_connection *conn, void *ctx)
+int client_hello_send_alerts(struct s2n_connection *conn, void *ctx)
 {
     struct alert_ctx *alert = ctx;
     uint8_t alert_msg[] = { TLS_ALERT, TLS_ALERT_VERSION, TLS_ALERT_LENGTH, alert->level, alert->code };
 
-    if (write(alert->write_fd, alert_msg, sizeof(alert_msg)) != sizeof(alert_msg)) {
-        _exit(100);
-    }
+    for (int i = 0; i < alert->count; i++) {
+        if (write(alert->write_fd, alert_msg, sizeof(alert_msg)) != sizeof(alert_msg)) {
+            _exit(100);
+        }
 
-    alert->invoked = 1;
+        alert->invoked++;
+    }
 
     return 0;
 }
@@ -158,8 +161,8 @@ int main(int argc, char **argv)
         EXPECT_SUCCESS(s2n_io_pair_init(&io_pair));
 
         /* Set up the callback to send an alert after receiving ClientHello */
-        struct alert_ctx warning_alert = {.write_fd = io_pair.server, .invoked = 0, .level = TLS_ALERT_LEVEL_WARNING, .code = TLS_ALERT_UNRECOGNIZED_NAME};
-        EXPECT_SUCCESS(s2n_config_set_client_hello_cb(config, client_hello_send_alert, &warning_alert));
+        struct alert_ctx warning_alert = {.write_fd = io_pair.server, .invoked = 0, .count = 2, .level = TLS_ALERT_LEVEL_WARNING, .code = TLS_ALERT_UNRECOGNIZED_NAME};
+        EXPECT_SUCCESS(s2n_config_set_client_hello_cb(config, client_hello_send_alerts, &warning_alert));
 
         /* Create a child process */
         pid = fork();
@@ -184,7 +187,7 @@ int main(int argc, char **argv)
         EXPECT_SUCCESS(s2n_negotiate(conn, &blocked));
 
         /* Ensure that callback was invoked */
-        EXPECT_EQUAL(warning_alert.invoked, 1);
+        EXPECT_EQUAL(warning_alert.invoked, 2);
 
         for (int i = 1; i < 0xffff; i += 100) {
             char * ptr = buffer;
@@ -223,8 +226,8 @@ int main(int argc, char **argv)
         EXPECT_SUCCESS(s2n_io_pair_init(&io_pair));
 
         /* Set up the callback to send an alert after receiving ClientHello */
-        struct alert_ctx fatal_alert = {.write_fd = io_pair.server, .invoked = 0, .level = TLS_ALERT_LEVEL_FATAL, .code = TLS_ALERT_UNRECOGNIZED_NAME};
-        EXPECT_SUCCESS(s2n_config_set_client_hello_cb(config, client_hello_send_alert, &fatal_alert));
+        struct alert_ctx fatal_alert = {.write_fd = io_pair.server, .invoked = 0, .count = 1, .level = TLS_ALERT_LEVEL_FATAL, .code = TLS_ALERT_UNRECOGNIZED_NAME};
+        EXPECT_SUCCESS(s2n_config_set_client_hello_cb(config, client_hello_send_alerts, &fatal_alert));
 
         /* Create a child process */
         pid = fork();
@@ -269,8 +272,8 @@ int main(int argc, char **argv)
         EXPECT_SUCCESS(s2n_io_pair_init(&io_pair));
 
         /* Set up the callback to send an alert after receiving ClientHello */
-        struct alert_ctx warning_alert = {.write_fd = io_pair.server, .invoked = 0, .level = TLS_ALERT_LEVEL_WARNING, .code = TLS_ALERT_UNRECOGNIZED_NAME};
-        EXPECT_SUCCESS(s2n_config_set_client_hello_cb(config, client_hello_send_alert, &warning_alert));
+        struct alert_ctx warning_alert = {.write_fd = io_pair.server, .invoked = 0, .count = 1, .level = TLS_ALERT_LEVEL_WARNING, .code = TLS_ALERT_UNRECOGNIZED_NAME};
+        EXPECT_SUCCESS(s2n_config_set_client_hello_cb(config, client_hello_send_alerts, &warning_alert));
 
         /* Create a child process */
         pid = fork();
