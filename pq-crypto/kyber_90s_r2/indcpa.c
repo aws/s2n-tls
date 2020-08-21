85b83184d6f8384c447a76ff849cf1cb4818ef71
Brandon Butch <bbutch@amazon.com>
2020-08-21 14:35:27 -0700

    Update kyber_90s_r2 with new PQ random function

--- pq-crypto/kyber_90s_r2/indcpa.c
+++ pq-crypto/kyber_90s_r2/indcpa.c
@@ -189,7 +189,7 @@ int PQCLEAN_KYBER51290S_CLEAN_indcpa_keypair(uint8_t *pk, uint8_t *sk) {
     uint8_t *noiseseed = buf + KYBER_SYMBYTES;
     uint8_t nonce = 0;
 
-    GUARD_AS_POSIX(get_random_bytes(buf, KYBER_SYMBYTES));
+    GUARD_AS_POSIX(s2n_get_random_bytes(buf, KYBER_SYMBYTES));
     hash_g(buf, buf, KYBER_SYMBYTES);
 
     gen_a(a, publicseed);
