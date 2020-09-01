af91a6921d0a089c10d4472a605291921291973b
Wesley Rosenblum <55108558+WesleyRosenblum@users.noreply.github.com>
2020-08-31 22:40:40 -0700

    Checking data size instead of data pointers in s2n_stream_cipher_null_endecrypt  (#2263)
    * Checking data size instead of data pointers
    
    * Adding unit test
    
    * Fixed test name
    
    * Initializing arrays to satisfy cppcheck
    
    * Simplifying array initialization

--- crypto/s2n_stream_cipher_null.c
+++ crypto/s2n_stream_cipher_null.c
@@ -27,10 +27,10 @@ static uint8_t s2n_stream_cipher_null_available()
 
 static int s2n_stream_cipher_null_endecrypt(struct s2n_session_key *key, struct s2n_blob *in, struct s2n_blob *out)
 {
-    S2N_ERROR_IF(out->data < in->data, S2N_ERR_SIZE_MISMATCH);
+    S2N_ERROR_IF(out->size < in->size, S2N_ERR_SIZE_MISMATCH);
 
     if (in->data != out->data) {
-        memcpy_check(in->data, out->data, out->size);
+        memcpy_check(out->data, in->data, out->size);
     }
     return 0;
 }
