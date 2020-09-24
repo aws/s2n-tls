7ff8a5c2461f76433fe92417cce3974f3ce6ccb1
Brandon Butch <bbutch@amazon.com>
2020-09-24 13:09:03 -0700

    Add PQ fuzz tests for recv_public_key

--- tests/testlib/s2n_kem_fuzz_testlib.c
+++ tests/testlib/s2n_kem_fuzz_testlib.c
@@ -43,19 +43,51 @@ int s2n_kem_recv_ciphertext_fuzz_test(const uint8_t *buf, size_t len, struct s2n
     GUARD(s2n_stuffer_growable_alloc(&ciphertext, 8192));
     GUARD(s2n_stuffer_write_bytes(&ciphertext, buf, len));
 
-    if (s2n_kem_recv_ciphertext(&ciphertext, kem_params) != S2N_SUCCESS) {
-        /* The PQ KEM functions are written in such a way that kem->decapsulate should
-         * never fail, even if the ciphertext is not valid. If s2n_kem_recv_ciphertext
-         * failed, we still want to check decapsulate. */
-        if (kem_params->shared_secret.allocated == 0) {
-            GUARD(s2n_alloc(&kem_params->shared_secret, kem_params->kem->shared_secret_key_length));
-        }
-
-        GUARD(kem_params->kem->decapsulate(kem_params->shared_secret.data, ciphertext.blob.data, kem_params->private_key.data));
+    /* Don't GUARD here; this will probably fail. */
+    s2n_kem_recv_ciphertext(&ciphertext, kem_params);
+    /* The PQ KEM functions are written in such a way that kem->decapsulate should
+     * never fail (except for BIKE1_L1_R1), even if the ciphertext is not valid. So,
+     * we check it with GUARD. */
+    if (kem_params->shared_secret.allocated == 0) {
+        /* If s2n_kem_recv_ciphertext failed, this probably did not get allocated. */
+        GUARD(s2n_alloc(&kem_params->shared_secret, kem_params->kem->shared_secret_key_length));
     }
+    GUARD(kem_params->kem->decapsulate(kem_params->shared_secret.data, ciphertext.blob.data, kem_params->private_key.data));
 
+    /* Clean up */
     GUARD(s2n_stuffer_free(&ciphertext));
     GUARD(s2n_free(&kem_params->shared_secret));
+
+    return S2N_SUCCESS;
+}
+
+int s2n_kem_recv_public_key_fuzz_test(const uint8_t *buf, size_t len, struct s2n_kem_params *kem_params) {
+    notnull_check(buf);
+    notnull_check(kem_params);
+    notnull_check(kem_params->kem);
+
+    struct s2n_stuffer public_key = { 0 };
+    GUARD(s2n_stuffer_growable_alloc(&public_key, 8192));
+    GUARD(s2n_stuffer_write_bytes(&public_key, buf, len));
+
+    /* s2n_kem_recv_public_key performs only very basic checks, like ensuring
+     * that the public key size is correct. If the received public key passes,
+     * we continue by calling s2n_kem_send_ciphertext to attempt to use the key
+     * for encryption. */
+    if (s2n_kem_recv_public_key(&public_key, kem_params) == S2N_SUCCESS) {
+        /* The PQ KEM functions are written in such a way that s2n_kem_send_ciphertext
+         * should always succeed, even if the public key is not valid. So, we check it
+         * with GUARD. */
+        struct s2n_stuffer out = {0};
+        GUARD(s2n_stuffer_growable_alloc(&out, 8192));
+        GUARD(s2n_kem_send_ciphertext(&out, kem_params));
+
+        GUARD(s2n_stuffer_free(&out));
+    }
+
+    /* Clean up */
+    GUARD(s2n_stuffer_free(&public_key));
+    GUARD(s2n_kem_free(kem_params));
 
     return S2N_SUCCESS;
 }
--- tests/testlib/s2n_testlib.h
+++ tests/testlib/s2n_testlib.h
@@ -171,5 +171,6 @@ extern const s2n_parsed_extension EMPTY_PARSED_EXTENSIONS[S2N_PARSED_EXTENSIONS_
 #define EXPECT_PARSED_EXTENSION_LIST_EMPTY(list) EXPECT_BYTEARRAY_EQUAL(list.parsed_extensions, EMPTY_PARSED_EXTENSIONS, sizeof(EMPTY_PARSED_EXTENSIONS))
 #define EXPECT_PARSED_EXTENSION_LIST_NOT_EMPTY(list) EXPECT_BYTEARRAY_NOT_EQUAL(list.parsed_extensions, EMPTY_PARSED_EXTENSIONS, sizeof(EMPTY_PARSED_EXTENSIONS))
 
+int s2n_kem_recv_public_key_fuzz_test(const uint8_t *buf, size_t len, struct s2n_kem_params *kem_params);
 int s2n_kem_recv_ciphertext_fuzz_test(const uint8_t *buf, size_t len, struct s2n_kem_params *kem_params);
 int s2n_kem_recv_ciphertext_fuzz_test_init(const char *kat_file_path, struct s2n_kem_params *kem_params);
