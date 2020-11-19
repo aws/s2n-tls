6a5f76777dcb55b97df9994cfa248cb36e20f69e
Felipe R. Monteiro <felisous@amazon.com>
2020-11-19 18:43:03 -0500

    Adds a proof for s2n_hash_const_time_get_currently_in_hash_block
    Signed-off-by: Felipe R. Monteiro <felisous@amazon.com>

---
+++ tests/cbmc/proofs/s2n_hash_const_time_get_currently_in_hash_block/s2n_hash_const_time_get_currently_in_hash_block_harness.c
@@ -0,0 +1,43 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+#include "api/s2n.h"
+#include "crypto/s2n_hash.h"
+
+#include <cbmc_proof/make_common_datastructures.h>
+#include <cbmc_proof/proof_allocators.h>
+
+void s2n_hash_const_time_get_currently_in_hash_block_harness()
+{
+    /* Non-deterministic inputs. */
+    struct s2n_hash_state *state = cbmc_allocate_s2n_hash_state();
+    uint64_t size;
+    uint64_t* out = bounded_malloc(size);
+
+    /* Assumptions. */
+    __CPROVER_assume(s2n_hash_state_is_valid(state));
+    if (state != NULL)
+    {
+        __CPROVER_file_local_s2n_hash_c_s2n_hash_set_impl(state);
+    }
+
+    /* Operation under verification. */
+    if (s2n_hash_const_time_get_currently_in_hash_block(state, out) == S2N_SUCCESS)
+    {
+        /* Post-conditions. */
+        assert(s2n_hash_state_is_valid(state));
+        assert(state->is_ready_for_input);
+    }
+}
