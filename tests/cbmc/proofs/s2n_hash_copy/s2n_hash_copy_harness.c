08d3f7004b0ddba1f07195f6efd61621bb2e9dc4
Felipe R. Monteiro <felisous@amazon.com>
2020-12-07 16:48:15 -0500

    Adds proof harnesses for s2n_hash functions (#2429)
    * Adds a proof harness for s2n_hash_update
    * Adds a proof harness for s2n_hash_copy
    * Adds a proof harness for s2n_hash_digest
    * Adds a proof harness for s2n_hash_reset
    * Updates Makefiles to run proofs using Litani system
    * Adds extra postconditions to s2n_hash proof harnesses
    * Prevents null-pointer dereferences in s2n_hash_evp_copy
    
    Signed-off-by: Felipe R. Monteiro <felisous@amazon.com>

---
+++ tests/cbmc/proofs/s2n_hash_copy/s2n_hash_copy_harness.c
@@ -0,0 +1,42 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+#include "api/s2n.h"
+#include "crypto/s2n_hash.h"
+
+#include <cbmc_proof/make_common_datastructures.h>
+#include <cbmc_proof/proof_allocators.h>
+
+void s2n_hash_copy_harness()
+{
+    /* Non-deterministic inputs. */
+    struct s2n_hash_state *to = cbmc_allocate_s2n_hash_state();
+    struct s2n_hash_state *from = cbmc_allocate_s2n_hash_state();
+
+    /* Assumptions. */
+    __CPROVER_assume(s2n_hash_state_is_valid(to));
+    __CPROVER_assume(s2n_hash_state_is_valid(from));
+    __CPROVER_file_local_s2n_hash_c_s2n_hash_set_impl(to);
+    __CPROVER_file_local_s2n_hash_c_s2n_hash_set_impl(from);
+
+    /* Operation under verification. */
+    if (s2n_hash_copy(to, from) == S2N_SUCCESS)
+    {
+        /* Post-conditions. */
+        assert(s2n_hash_state_is_valid(to));
+        assert(s2n_hash_state_is_valid(from));
+        assert(from->hash_impl->copy != NULL);
+    }
+}
