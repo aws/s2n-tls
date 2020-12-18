062f02f8fe295a94b6bfd666b4b2ad72f1b2b53e
maddeleine <59030281+maddeleine@users.noreply.github.com>
2020-12-18 10:10:32 -0800

    Added psk to key schedule (#2481)
    * Added psk to key schedule

--- tests/unit/s2n_tls13_hybrid_shared_secret_test.c
+++ tests/unit/s2n_tls13_hybrid_shared_secret_test.c
@@ -408,7 +408,7 @@ int main(int argc, char **argv) {
              * the client & server traffic secrets */
             DEFER_CLEANUP(struct s2n_tls13_keys secrets = {0}, s2n_tls13_keys_free);
             EXPECT_SUCCESS(s2n_tls13_keys_init(&secrets, test_vector->cipher_suite->prf_alg));
-            EXPECT_SUCCESS(s2n_tls13_derive_early_secrets(&secrets));
+            EXPECT_SUCCESS(s2n_tls13_derive_early_secrets(&secrets, NULL));
 
             DEFER_CLEANUP(struct s2n_hash_state hash_state, s2n_hash_free);
             EXPECT_SUCCESS(s2n_hash_new(&hash_state));
