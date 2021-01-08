83fd5989014525820c497f77407d9a6e1c13599b
Madeleine <madelea@amazon.com>
2021-01-07 17:44:47 -0800

    Fix for rsa_pss_rsae_test

--- tests/unit/s2n_rsa_pss_rsae_test.c
+++ tests/unit/s2n_rsa_pss_rsae_test.c
@@ -245,7 +245,6 @@ int main(int argc, char **argv)
         EXPECT_SUCCESS(RSA_set0_key(rsa_key, n, e, d));
 
         struct s2n_stuffer message_stuffer, signature_stuffer;
-        struct s2n_stuffer input_stuffer;
 
         for (int i = 0; i < sizeof(test_cases) / sizeof(struct test_case); i++) {
             s2n_stuffer_alloc_ro_from_hex_string(&message_stuffer, test_cases[i].message);
@@ -257,7 +256,6 @@ int main(int argc, char **argv)
 
             s2n_stuffer_free(&message_stuffer);
             s2n_stuffer_free(&signature_stuffer);
-            s2n_stuffer_free(&input_stuffer);
         }
 
         EXPECT_SUCCESS(s2n_pkey_free(&rsa_public_key));
