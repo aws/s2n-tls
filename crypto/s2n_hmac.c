da68848eacd79fdae24f183c4e5ade2569165d26
Felipe R. Monteiro <felisous@amazon.com>
2021-01-13 12:33:18 -0300

    Remove unnecessary check
    Signed-off-by: Felipe R. Monteiro <felisous@amazon.com>

--- crypto/s2n_hmac.c
+++ crypto/s2n_hmac.c
@@ -268,7 +268,6 @@ int s2n_hmac_update(struct s2n_hmac_state *state, const void *in, uint32_t size)
     const uint32_t HIGHEST_32_BIT = 4294949760;
     ENSURE_POSIX(size <= (UINT32_MAX - HIGHEST_32_BIT), S2N_ERR_INTEGER_OVERFLOW);
     uint32_t value = (HIGHEST_32_BIT + size) % state->hash_block_size;
-    ENSURE_POSIX(value <= (UINT32_MAX - state->currently_in_hash_block), S2N_ERR_INTEGER_OVERFLOW);
     state->currently_in_hash_block += value;
     state->currently_in_hash_block %= state->hash_block_size;
 
