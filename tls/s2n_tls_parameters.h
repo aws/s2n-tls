198c06f1ef63222e8f7e417bbcddce9574472fad
Brandon Butch <bbutch@amazon.com>
2021-01-21 14:28:49 -0800

    Add kyber512r3 KEM
    * Adds KAT test
    * Updates Makefiles/CMakeLists appropriately
    * Updates the kyber512r3 crypto code in a few places to integrate it
      properly within s2n

--- tls/s2n_tls_parameters.h
+++ tls/s2n_tls_parameters.h
@@ -70,6 +70,8 @@
 #define TLS_PQ_KEM_EXTENSION_ID_SIKE_P434_R2 19
 #define TLS_PQ_KEM_EXTENSION_ID_KYBER_512_R2 23
 #define TLS_PQ_KEM_EXTENSION_ID_KYBER_512_90S_R2 24
+/* TODO The round 3 extension values are temporary placeholders until the draft spec (draft-campagna-tls-bike-sike-hybrid) gets updated */
+#define TLS_PQ_KEM_EXTENSION_ID_KYBER_512_R3 25
 
 /* TLS 1.3 hybrid post-quantum definitions are from the proposed reserved range defined
  * in https://tools.ietf.org/html/draft-stebila-tls-hybrid-design. Values for interoperability
