0cb1db5352123fd6eb64df3eaa937a16248a8715
Brandon Butch <bbutch@amazon.com>
2021-01-25 15:20:57 -0800

    Fix eta1->eta2 typo in kyber_r3 function

--- pq-crypto/kyber_r3/cbd.c
+++ pq-crypto/kyber_r3/cbd.c
@@ -99,6 +99,6 @@ void cbd_eta1(poly *r, const uint8_t buf[KYBER_ETA1 * KYBER_N / 4]) {
     cbd3(r, buf);
 }
 
-void cbd_eta2(poly *r, const uint8_t buf[KYBER_ETA1 * KYBER_N / 4]) {
+void cbd_eta2(poly *r, const uint8_t buf[KYBER_ETA2 * KYBER_N / 4]) {
     cbd2(r, buf);
 }
