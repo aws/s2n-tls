2a330bdacdc6a2840539952be21f1485939b6f47
Cameron Bytheway <bytheway.cameron@gmail.com>
2021-03-03 14:17:20 -0800

    preserve memalign check

--- utils/s2n_mem.c
+++ utils/s2n_mem.c
@@ -97,7 +97,7 @@ static int s2n_mem_malloc_mlock_impl(void **ptr, uint32_t requested, uint32_t *a
     GUARD(s2n_align_to(requested, page_size, &allocate));
 
     *ptr = NULL;
-    POSIX_ENSURE_OK(posix_memalign(ptr, page_size, allocate), S2N_ERR_ALLOC);
+    POSIX_ENSURE(posix_memalign(ptr, page_size, allocate) == 0, S2N_ERR_ALLOC);
     *allocated = allocate;
 
 /*
@@ -193,7 +193,9 @@ int s2n_realloc(struct s2n_blob *b, uint32_t size)
     }
 
     struct s2n_blob new_memory = {.data = NULL, .size = size, .allocated = 0, .growable = 1};
-    POSIX_GUARD(s2n_mem_malloc_cb((void **) &new_memory.data, new_memory.size, &new_memory.allocated));
+    if (s2n_mem_malloc_cb((void **) &new_memory.data, new_memory.size, &new_memory.allocated) != 0) {
+        S2N_ERROR_PRESERVE_ERRNO();
+    }
 
     POSIX_ENSURE(new_memory.allocated >= new_memory.size, S2N_ERR_ALLOC);
     POSIX_ENSURE(new_memory.data != NULL, S2N_ERR_ALLOC);
