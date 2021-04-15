91db7cd7955a65166df91f7116e68a4519c64bfd
Wesley Rosenblum <wesleyr@amazon.com>
2021-04-15 09:57:21 -0700

    Print env

--- codebuild/bin/s2n_after_codebuild.sh
+++ codebuild/bin/s2n_after_codebuild.sh
@@ -18,8 +18,12 @@ set -ex
 # Upload Code Coverage Information to CodeCov.io
 if [[ "$CODECOV_IO_UPLOAD" == "true" ]]; then
     if [[ "$FUZZ_COVERAGE" == "true" ]]; then
+        echo "Printing env for codecov";
+        env;
         bash <(curl -s https://codecov.io/bash) -f coverage/fuzz/codecov.txt -F ${TESTS};
     else
+        echo "Printing env for codecov";
+        env;
         bash <(curl -s https://codecov.io/bash) -F ${TESTS};
     fi
 fi
