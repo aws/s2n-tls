3af80dd9cdcbca76ac5bfadd96adc11639a47dc1
Ryan Day <rynday@amazon.com>
2021-04-15 19:45:59 +0000

    Update wording

--- codebuild/bin/s2n_after_codebuild.sh
+++ codebuild/bin/s2n_after_codebuild.sh
@@ -18,10 +18,10 @@ set -ex
 # Upload Code Coverage Information to CodeCov.io
 if [[ "$CODECOV_IO_UPLOAD" == "true" ]]; then
     if [[ "$FUZZ_COVERAGE" == "true" ]]; then
-        # Re-enable the new code coverage solution 
+        # Re-enable the new code coverage solution when applicable
         # codebuild/bin/codecov_uploader.sh -f coverage/fuzz/codecov.txt -F ${TESTS};
     else
-        # Re-enable the new code coverage solution 
+        # Re-enable the new code coverage solution when applicable
         # codebuild/bin/codecov_uploader.sh -F ${TESTS};
     fi
 fi
