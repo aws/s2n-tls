9b5f4a8026f5ec52d426b945247d5f1f74f8d6cb
Lindsay Stewart <stewart.r.lindsay@gmail.com>
2021-04-26 14:16:56 -0700

    Address @SalusaSecondus's comment

--- tls/s2n_config.c
+++ tls/s2n_config.c
@@ -688,6 +688,9 @@ int s2n_config_set_session_tickets_onoff(struct s2n_config *config, uint8_t enab
 
     config->use_tickets = enabled;
     if (config->initial_tickets_to_send == 0) {
+        /* Normally initial_tickets_to_send is set via s2n_config_set_initial_ticket_count.
+         * However, s2n_config_set_initial_ticket_count calls this method.
+         * So we set initial_tickets_to_send directly to avoid infinite recursion. */
         config->initial_tickets_to_send = 1;
     }
 
