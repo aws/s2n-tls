8f558ee93537a40746fca5a7a4ff93557cbb5dd1
Saswat Padhi <saspadhi@amazon.com>
2021-05-07 17:32:26 -0400

    Formally verify no memory leaks in s2n_stuffer (#2813)
    The harness for `s2n_stuffer` deallocator (`s2n_stuffer_free`) is strengthened with the `--memory-leak-check` flag, and we now explicitly document all error cases where memory might not be `free`d.

--- tests/cbmc/proofs/s2n_stuffer_free/s2n_stuffer_free_harness.c
+++ tests/cbmc/proofs/s2n_stuffer_free/s2n_stuffer_free_harness.c
@@ -22,11 +22,36 @@
 
 void s2n_stuffer_free_harness()
 {
+    /* Non-deterministic inputs. */
     struct s2n_stuffer *stuffer = cbmc_allocate_s2n_stuffer();
 
+    /* Assumptions. */
     nondet_s2n_mem_init();
+    __CPROVER_assume(s2n_result_is_ok(s2n_stuffer_validate(stuffer)));
+    const bool old_alloced = stuffer ? stuffer->alloced : false;
+    struct s2n_blob old_blob;
+    old_blob.data = stuffer ? stuffer->blob.data : NULL;
+    old_blob.growable = stuffer ? stuffer->blob.growable : NULL;
 
-    if (s2n_stuffer_free(stuffer) == S2N_SUCCESS && stuffer != NULL) {
+    /* Operation under verification. */
+    int result = s2n_stuffer_free(stuffer);
+    if (result == S2N_SUCCESS && stuffer != NULL) {
         assert_all_zeroes(stuffer, sizeof(*stuffer));
+    } else if (result == S2N_FAILURE && s2n_errno == S2N_ERR_FREE_STATIC_BLOB) {
+        assert(!old_blob.growable);
     }
+
+    /* Cleanup after expected error cases, for memory leak check. */
+    if ((result == S2N_FAILURE && s2n_errno == S2N_ERR_NOT_INITIALIZED) || !old_blob.growable || !old_alloced) {
+        /**
+         * 1. `s2n_free` failed _before_ calling `free`, or
+         * 2. `stuffer` had a static blow (i.e. with `blob.growable` was `0`), or
+         * 3. `stuffer` did not own its blob (i.e. `alloced` was `0`).
+         *    Note that `stuffer` is zero-ed out (without `free`-ing the blob),
+         *    so we can't use `stuffer->alloced` and `stuffer->blob.data` here.
+         */
+        free(old_blob.data);
+    }
+    /* 3. free our heap-allocated `stuffer` since `s2n_stuffer_free` only `free`s the contents. */
+    free(stuffer);
 }
