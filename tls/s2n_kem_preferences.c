4d9e8fabda10d487ed2452f57d9ca270c75f531b
Brandon Butch <bbutch@amazon.com>
2021-06-01 13:32:34 -0700

    Clarify SIKE comments

--- tls/s2n_kem_preferences.c
+++ tls/s2n_kem_preferences.c
@@ -21,7 +21,8 @@ const struct s2n_kem *pq_kems_r1[2] = {
     &s2n_sike_p503_r1,
 };
 
-/* Extension list for round 2 and round 1 PQ KEMs, in order of preference */
+/* Extension list for round 2 and round 1 PQ KEMs, in order of preference.
+ * s2n_sike_p434_r3 is compatible with, and has replaced, s2n_sike_p434_r2. */
 const struct s2n_kem *pq_kems_r2r1[4] = {
     &s2n_bike1_l1_r2,
     &s2n_sike_p434_r3,
@@ -29,6 +30,7 @@ const struct s2n_kem *pq_kems_r2r1[4] = {
     &s2n_sike_p503_r1,
 };
 
+/* s2n_sike_p434_r3 is compatible with, and has replaced, s2n_sike_p434_r2. */
 const struct s2n_kem *pq_kems_r2r1_2020_07[5] = {
     &s2n_kyber_512_r2,
     &s2n_bike1_l1_r2,
@@ -43,12 +45,14 @@ const struct s2n_kem *pq_kems_sike_r1[1] = {
 };
 
 /* Extension list for SIKE P434 Round 2 and SIKE P503 Round 1 only (for testing),
- * in order of preference */
+ * in order of preference. s2n_sike_p434_r3 is compatible with, and has replaced,
+ * s2n_sike_p434_r2. */
 const struct s2n_kem *pq_kems_sike_r2r1[2] = {
     &s2n_sike_p434_r3,
     &s2n_sike_p503_r1,
 };
 
+/* sike_p434_r3 has replaced sike_p434_r2 in all KEM groups */
 const struct s2n_kem_group *pq_kem_groups_r2[] = {
 #if EVP_APIS_SUPPORTED
         &s2n_x25519_kyber_512_r2,
