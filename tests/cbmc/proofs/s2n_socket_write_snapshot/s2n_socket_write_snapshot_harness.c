337754b6757346ce5c063d47c860987803e7a07a
Priya Srikumar <priyasrikumar1@gmail.com>
2021-06-11 11:55:11 -0400

    Fixing autogenerated CPP comments

--- tests/cbmc/proofs/s2n_socket_write_snapshot/s2n_socket_write_snapshot_harness.c
+++ tests/cbmc/proofs/s2n_socket_write_snapshot/s2n_socket_write_snapshot_harness.c
@@ -1,5 +1,5 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
+/* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved. */
+/* SPDX-License-Identifier: Apache-2.0 */
 
 /**
  * @file s2n_socket_write_snapshot_harness.c
