51b881186605753023912e1ba67a00054e1f0ccd
Sanal <pkz@88665a1cd03a.ant.amazon.com>
2021-07-02 08:12:51 -0400

    comment out the namespaces used for assembly functions

--- pq-crypto/kyber_r3/avx2/kyber512r3_ntt_avx2.h
+++ pq-crypto/kyber_r3/avx2/kyber512r3_ntt_avx2.h
@@ -3,24 +3,24 @@
 #include <stdint.h>
 #include <immintrin.h>
 
-//#define ntt_avx2_asm S2N_KYBER_512_R3_NAMESPACE(ntt_avx2_asm)
+#define ntt_avx2_asm S2N_KYBER_512_R3_NAMESPACE(ntt_avx2_asm)
 void ntt_avx2_asm(__m256i *r, const __m256i *kyber512_qdata_avx2);
 
-//#define invntt_avx_asm S2N_KYBER_512_R3_NAMESPACE(invntt_avx_asm)
+#define invntt_avx_asm S2N_KYBER_512_R3_NAMESPACE(invntt_avx_asm)
 void invntt_avx_asm(__m256i *r, const __m256i *kyber512_qdata_avx2);
 
-//#define nttunpack_avx2_asm S2N_KYBER_512_R3_NAMESPACE(nttunpack_avx2_asm)
+#define nttunpack_avx2_asm S2N_KYBER_512_R3_NAMESPACE(nttunpack_avx2_asm)
 void nttunpack_avx2_asm(__m256i *r, const __m256i *kyber512_qdata_avx2);
 
-//#define basemul_avx2_asm S2N_KYBER_512_R3_NAMESPACE(basemul_avx2_asm)
+#define basemul_avx2_asm S2N_KYBER_512_R3_NAMESPACE(basemul_avx2_asm)
 void basemul_avx2_asm(__m256i *r,
                                        const __m256i *a,
                                        const __m256i *b,
                                        const __m256i *kyber512_qdata_avx2);
 
-//#define ntttobytes_avx2_asm S2N_KYBER_512_R3_NAMESPACE(ntttobytes_avx2_asm)
+#define ntttobytes_avx2_asm S2N_KYBER_512_R3_NAMESPACE(ntttobytes_avx2_asm)
 void ntttobytes_avx2_asm(uint8_t *r, const __m256i *a, const __m256i *kyber512_qdata_avx2);
 
-//#define nttfrombytes_avx2_asm S2N_KYBER_512_R3_NAMESPACE(nttfrombytes_avx2_asm)
+#define nttfrombytes_avx2_asm S2N_KYBER_512_R3_NAMESPACE(nttfrombytes_avx2_asm)
 void nttfrombytes_avx2_asm(__m256i *r, const uint8_t *a, const __m256i *kyber512_qdata_avx2);
 
--- pq-crypto/kyber_r3/avx2/kyber512r3_reduce_avx2.h
+++ pq-crypto/kyber_r3/avx2/kyber512r3_reduce_avx2.h
@@ -3,9 +3,9 @@
 #include "../kyber512r3_params.h"
 #include <immintrin.h>
 
-//#define reduce_avx2_asm S2N_KYBER_512_R3_NAMESPACE(reduce_avx2_asm)
+#define reduce_avx2_asm S2N_KYBER_512_R3_NAMESPACE(reduce_avx2_asm)
 void reduce_avx2_asm(__m256i *r, const __m256i *kyber512_qdata_avx2);
 
-//#define tomont_avx2_asm S2N_KYBER_512_R3_NAMESPACE(tomont_avx2_asm)
+#define tomont_avx2_asm S2N_KYBER_512_R3_NAMESPACE(tomont_avx2_asm)
 void tomont_avx2_asm(__m256i *r, const __m256i *kyber512_qdata_avx2);
 
