576f719b6cb052b6301f81491ea0770383888ff4
sidhusn <sidhusn@amazon.com>
2021-08-03 18:51:06 +0000

    adding benchmark header files

---
+++ tests/benchmark/s2n_neg_client_benchmark.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+#ifndef S2N_NEG_CLIENT_BENCHMARK_H
+#define S2N_NEG_CLIENT_BENCHMARK_H
+class Client {
+public:
+    int start_benchmark_client(int argc, char **argv);
+};
+#endif
