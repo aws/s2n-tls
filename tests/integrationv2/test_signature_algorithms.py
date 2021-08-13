98cd7cf30c9896d98ba7a8bfec1b14065fa15a49
Lindsay Stewart <slindsay@amazon.com>
2021-08-12 23:44:11 -0700

    Get rid of 'unsupported' cipher list

--- tests/integrationv2/test_signature_algorithms.py
+++ tests/integrationv2/test_signature_algorithms.py
@@ -26,16 +26,6 @@ all_sigs = [
     Signatures.RSA_PSS_RSAE_SHA256,
 ]
 
-# These ciphers don't print out the proper debugging information from s_client,
-# so we can't verify the signature algorithm used. When s2n provides the signature
-# algorithm to the client, we can enable these.
-unsupported_ciphers = [
-    Ciphers.AES128_SHA,
-    Ciphers.AES256_SHA,
-    Ciphers.AES128_SHA256,
-    Ciphers.AES256_SHA256,
-]
-
 
 def signature_marker(mode, signature):
     return to_bytes("{mode} signature negotiated: {type}+{digest}" \
@@ -58,9 +48,6 @@ def skip_ciphers(*args, **kwargs):
         return True
 
     if protocol < sigalg.min_protocol:
-        return True
-
-    if cipher in unsupported_ciphers:
         return True
 
     return invalid_test_parameters(*args, **kwargs)
@@ -151,8 +138,18 @@ def test_s2n_client_signature_algorithms(managed_process, cipher, provider, prot
 
     expected_version = get_expected_s2n_version(protocol, provider)
 
+    # In versions before TLS1.3, the server uses the negotiated signature scheme for the
+    # ServerKeyExchange message. The server only sends the ServerKeyExchange message when using
+    # a key exchange method that provides forward secrecy, ie, NOT static RSA.
+    # So if using RSA key exchange, there is no actual "negotiated" signature scheme, because
+    # the server never sends the client a signature scheme.
+    #
+    # This mostly has to be inferred from the RFCs, but this blog post is a pretty good summary
+    # of the situation: https://timtaubert.de/blog/2016/07/the-evolution-of-signatures-in-tls/
+    server_sigalg_used = not cipher.iana_standard_name.startswith("TLS_RSA_WITH_")
+
     for results in client.get_results():
         results.assert_success()
         assert to_bytes("Actual protocol version: {}".format(expected_version)) in results.stdout
-        assert signature_marker(Provider.ServerMode, signature) in results.stdout
+        assert signature_marker(Provider.ServerMode, signature) in results.stdout or not server_sigalg_used
         assert (signature_marker(Provider.ClientMode, signature) in results.stdout) == client_auth
