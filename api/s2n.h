lrstewart/s2n

Support public certificates without corresponding secret keys

api/s2n.h
@@ -186,6 +186,8 @@ extern struct s2n_cert_chain_and_key *s2n_cert_chain_and_key_new(void);
 S2N_API
 extern int s2n_cert_chain_and_key_load_pem(struct s2n_cert_chain_and_key *chain_and_key, const char *chain_pem, const char *private_key_pem);
 S2N_API
+extern int s2n_cert_chain_load_pem_bytes(struct s2n_cert_chain_and_key *chain_and_key, uint8_t *chain_pem, uint32_t chain_pem_len);
+S2N_API
 extern int s2n_cert_chain_and_key_load_pem_bytes(struct s2n_cert_chain_and_key *chain_and_key, uint8_t *chain_pem, uint32_t chain_pem_len, uint8_t *private_key_pem, uint32_t private_key_pem_len);
 S2N_API
 extern int s2n_cert_chain_and_key_free(struct s2n_cert_chain_and_key *cert_and_key);

crypto/s2n_certificate.c
@@ -356,6 +356,11 @@ static int s2n_cert_chain_and_key_set_names(struct s2n_cert_chain_and_key *chain
 
 int s2n_cert_chain_and_key_load(struct s2n_cert_chain_and_key *chain_and_key)
 {
+    POSIX_ENSURE_REF(chain_and_key);
+    POSIX_ENSURE_REF(chain_and_key->cert_chain);
+    POSIX_ENSURE_REF(chain_and_key->cert_chain->head);
+    POSIX_ENSURE_REF(chain_and_key->private_key);
+
     /* Parse the leaf cert for the public key and certificate type */
     DEFER_CLEANUP(struct s2n_pkey public_key = {0}, s2n_pkey_free);
     s2n_pkey_type pkey_type = S2N_PKEY_TYPE_UNKNOWN;
@@ -364,7 +369,9 @@ int s2n_cert_chain_and_key_load(struct s2n_cert_chain_and_key *chain_and_key)
     POSIX_GUARD(s2n_cert_set_cert_type(chain_and_key->cert_chain->head, pkey_type));
 
     /* Validate the leaf cert's public key matches the provided private key */
-    POSIX_GUARD(s2n_pkey_match(&public_key, chain_and_key->private_key));
+    if (s2n_pkey_check_key_exists(chain_and_key->private_key) == S2N_SUCCESS) {
+        POSIX_GUARD(s2n_pkey_match(&public_key, chain_and_key->private_key));
+    }
 
     /* Populate name information from the SAN/CN for the leaf certificate */
     POSIX_GUARD(s2n_cert_chain_and_key_set_names(chain_and_key, &chain_and_key->cert_chain->head->raw));
@@ -379,6 +386,16 @@ int s2n_cert_chain_and_key_load_pem(struct s2n_cert_chain_and_key *chain_and_key
     POSIX_GUARD(s2n_cert_chain_and_key_set_cert_chain(chain_and_key, chain_pem));
     POSIX_GUARD(s2n_cert_chain_and_key_set_private_key(chain_and_key, private_key_pem));
 
+    POSIX_GUARD(s2n_cert_chain_and_key_load(chain_and_key));
+
+    return S2N_SUCCESS;
+}
+
+int s2n_cert_chain_load_pem_bytes(struct s2n_cert_chain_and_key *chain_and_key, uint8_t *chain_pem, uint32_t chain_pem_len)
+{
+    POSIX_ENSURE_REF(chain_and_key);
+
+    POSIX_GUARD(s2n_cert_chain_and_key_set_cert_chain_bytes(chain_and_key, chain_pem, chain_pem_len));
     POSIX_GUARD(s2n_cert_chain_and_key_load(chain_and_key));
 
     return S2N_SUCCESS;

tests/unit/s2n_certificate_test.c
@@ -42,6 +42,11 @@ static uint8_t verify_host_fn(const char *host_name, size_t host_name_len, void 
     return verify_data->allow;
 }
 
+static int s2n_noop_async_pkey_fn(struct s2n_connection *conn, struct s2n_async_pkey_op *op)
+{
+    return S2N_SUCCESS;
+}
+
 static S2N_RESULT s2n_compare_cert_chain(struct s2n_connection *conn, struct s2n_cert_chain_and_key *test_peer_chain)
 {
     RESULT_ENSURE_REF(conn);
@@ -98,6 +103,35 @@ int main(int argc, char **argv)
     struct s2n_cert_chain_and_key *chain_and_key = NULL;
     EXPECT_SUCCESS(
         s2n_test_cert_chain_and_key_new(&chain_and_key, S2N_DEFAULT_TEST_CERT_CHAIN, S2N_DEFAULT_TEST_PRIVATE_KEY));
+
+    /* Test s2n_cert_chain_load_pem_bytes */
+    {
+        uint32_t pem_len = 0;
+        uint8_t pem[S2N_CERT_DER_SIZE] = { 0 };
+        EXPECT_SUCCESS(s2n_read_test_pem_and_len(S2N_DEFAULT_ECDSA_TEST_CERT_CHAIN, pem, &pem_len, sizeof(pem)));
+
+        /* Load only a public certificate */
+        struct s2n_cert_chain_and_key *cert_only_chain = s2n_cert_chain_and_key_new();
+        EXPECT_NOT_NULL(cert_only_chain);
+        EXPECT_SUCCESS(s2n_cert_chain_load_pem_bytes(cert_only_chain, pem, pem_len));
+        EXPECT_FAILURE(s2n_pkey_check_key_exists(cert_only_chain->private_key));
+
+        /* Add cert chain to config */
+        struct s2n_config *config = s2n_config_new();
+        EXPECT_FALSE(config->no_signing_key);
+        EXPECT_SUCCESS(s2n_config_add_cert_chain_and_key_to_store(config, cert_only_chain));
+        EXPECT_TRUE(config->no_signing_key);
+
+        /* Add config to connection */
+        struct s2n_connection *conn = s2n_connection_new(S2N_CLIENT);
+        EXPECT_FAILURE_WITH_ERRNO(s2n_connection_set_config(conn, config), S2N_ERR_NO_PRIVATE_KEY);
+        EXPECT_SUCCESS(s2n_config_set_async_pkey_callback(config, s2n_noop_async_pkey_fn));
+        EXPECT_SUCCESS(s2n_connection_set_config(conn, config));
+
+        EXPECT_SUCCESS(s2n_connection_free(conn));
+        EXPECT_SUCCESS(s2n_config_free(config));
+        EXPECT_SUCCESS(s2n_cert_chain_and_key_free(cert_only_chain));
+    }
 
     /* Test s2n_cert_chain_get_length */ 
     {

aws/s2n-tls

api/s2n.h
@@ -195,6 +195,8 @@ extern int s2n_cert_chain_and_key_load_pem(struct s2n_cert_chain_and_key *chain_
 S2N_API
 extern int s2n_cert_chain_and_key_load_pem_bytes(struct s2n_cert_chain_and_key *chain_and_key, uint8_t *chain_pem, uint32_t chain_pem_len, uint8_t *private_key_pem, uint32_t private_key_pem_len);
 S2N_API
+extern int s2n_cert_chain_and_key_load_public_pem_bytes(struct s2n_cert_chain_and_key *chain_and_key, uint8_t *chain_pem, uint32_t chain_pem_len);
+S2N_API
 extern int s2n_cert_chain_and_key_free(struct s2n_cert_chain_and_key *cert_and_key);
 S2N_API
 extern int s2n_cert_chain_and_key_set_ctx(struct s2n_cert_chain_and_key *cert_and_key, void *ctx);

crypto/s2n_certificate.c
@@ -356,6 +356,11 @@ static int s2n_cert_chain_and_key_set_names(struct s2n_cert_chain_and_key *chain
 
 int s2n_cert_chain_and_key_load(struct s2n_cert_chain_and_key *chain_and_key)
 {
+    POSIX_ENSURE_REF(chain_and_key);
+    POSIX_ENSURE_REF(chain_and_key->cert_chain);
+    POSIX_ENSURE_REF(chain_and_key->cert_chain->head);
+    POSIX_ENSURE_REF(chain_and_key->private_key);
+
     /* Parse the leaf cert for the public key and certificate type */
     DEFER_CLEANUP(struct s2n_pkey public_key = {0}, s2n_pkey_free);
     s2n_pkey_type pkey_type = S2N_PKEY_TYPE_UNKNOWN;
@@ -364,7 +369,9 @@ int s2n_cert_chain_and_key_load(struct s2n_cert_chain_and_key *chain_and_key)
     POSIX_GUARD(s2n_cert_set_cert_type(chain_and_key->cert_chain->head, pkey_type));
 
     /* Validate the leaf cert's public key matches the provided private key */
-    POSIX_GUARD(s2n_pkey_match(&public_key, chain_and_key->private_key));
+    if (s2n_pkey_check_key_exists(chain_and_key->private_key) == S2N_SUCCESS) {
+        POSIX_GUARD(s2n_pkey_match(&public_key, chain_and_key->private_key));
+    }
 
     /* Populate name information from the SAN/CN for the leaf certificate */
     POSIX_GUARD(s2n_cert_chain_and_key_set_names(chain_and_key, &chain_and_key->cert_chain->head->raw));
@@ -381,6 +388,13 @@ int s2n_cert_chain_and_key_load_pem(struct s2n_cert_chain_and_key *chain_and_key
 
     POSIX_GUARD(s2n_cert_chain_and_key_load(chain_and_key));
 
+    return S2N_SUCCESS;
+}
+
+int s2n_cert_chain_and_key_load_public_pem_bytes(struct s2n_cert_chain_and_key *chain_and_key, uint8_t *chain_pem, uint32_t chain_pem_len)
+{
+    POSIX_GUARD(s2n_cert_chain_and_key_set_cert_chain_bytes(chain_and_key, chain_pem, chain_pem_len));
+    POSIX_GUARD(s2n_cert_chain_and_key_load(chain_and_key));
     return S2N_SUCCESS;
 }
 

tests/unit/s2n_certificate_test.c
@@ -42,6 +42,11 @@ static uint8_t verify_host_fn(const char *host_name, size_t host_name_len, void 
     return verify_data->allow;
 }
 
+static int s2n_noop_async_pkey_fn(struct s2n_connection *conn, struct s2n_async_pkey_op *op)
+{
+    return S2N_SUCCESS;
+}
+
 static S2N_RESULT s2n_compare_cert_chain(struct s2n_connection *conn, struct s2n_cert_chain_and_key *test_peer_chain)
 {
     RESULT_ENSURE_REF(conn);
@@ -98,6 +103,35 @@ int main(int argc, char **argv)
     struct s2n_cert_chain_and_key *chain_and_key = NULL;
     EXPECT_SUCCESS(
         s2n_test_cert_chain_and_key_new(&chain_and_key, S2N_DEFAULT_TEST_CERT_CHAIN, S2N_DEFAULT_TEST_PRIVATE_KEY));
+
+    /* Test s2n_cert_chain_and_key_load_public_pem_bytes */
+    {
+        uint32_t pem_len = 0;
+        uint8_t pem[S2N_CERT_DER_SIZE] = { 0 };
+        EXPECT_SUCCESS(s2n_read_test_pem_and_len(S2N_DEFAULT_ECDSA_TEST_CERT_CHAIN, pem, &pem_len, sizeof(pem)));
+
+        /* Load only a public certificate */
+        struct s2n_cert_chain_and_key *cert_only_chain = s2n_cert_chain_and_key_new();
+        EXPECT_NOT_NULL(cert_only_chain);
+        EXPECT_SUCCESS(s2n_cert_chain_and_key_load_public_pem_bytes(cert_only_chain, pem, pem_len));
+        EXPECT_FAILURE(s2n_pkey_check_key_exists(cert_only_chain->private_key));
+
+        /* Add cert chain to config */
+        struct s2n_config *config = s2n_config_new();
+        EXPECT_FALSE(config->no_signing_key);
+        EXPECT_SUCCESS(s2n_config_add_cert_chain_and_key_to_store(config, cert_only_chain));
+        EXPECT_TRUE(config->no_signing_key);
+
+        /* Add config to connection */
+        struct s2n_connection *conn = s2n_connection_new(S2N_CLIENT);
+        EXPECT_FAILURE_WITH_ERRNO(s2n_connection_set_config(conn, config), S2N_ERR_NO_PRIVATE_KEY);
+        EXPECT_SUCCESS(s2n_config_set_async_pkey_callback(config, s2n_noop_async_pkey_fn));
+        EXPECT_SUCCESS(s2n_connection_set_config(conn, config));
+
+        EXPECT_SUCCESS(s2n_connection_free(conn));
+        EXPECT_SUCCESS(s2n_config_free(config));
+        EXPECT_SUCCESS(s2n_cert_chain_and_key_free(cert_only_chain));
+    }
 
     /* Test s2n_cert_chain_get_length */ 
     {