c096a55ff513de32cc69f6136614177239e76981
Lindsay Stewart <slindsay@amazon.com>
2021-09-14 10:47:50 -0700

    Allow QUIC to be enabled per-connection (#3048)

--- tls/s2n_quic_support.c
+++ tls/s2n_quic_support.c
@@ -42,9 +42,17 @@ int s2n_config_enable_quic(struct s2n_config *config)
     return S2N_SUCCESS;
 }
 
+int s2n_connection_enable_quic(struct s2n_connection *conn)
+{
+    POSIX_ENSURE_REF(conn);
+    conn->quic_enabled = true;
+    return S2N_SUCCESS;
+}
+
 bool s2n_connection_is_quic_enabled(struct s2n_connection *conn)
 {
-    return conn && conn->config && conn->config->quic_enabled;
+    return (conn && conn->quic_enabled) ||
+           (conn && conn->config && conn->config->quic_enabled);
 }
 
 int s2n_connection_set_quic_transport_parameters(struct s2n_connection *conn,
