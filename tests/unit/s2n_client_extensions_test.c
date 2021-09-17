2668d55bbaad27f1610da09e94460fd3a1bbf254
maddeleine <59030281+maddeleine@users.noreply.github.com>
2021-09-17 15:22:30 -0700

    Turn On Client OCSP Stapled Test (#3055)

--- tests/unit/s2n_client_extensions_test.c
+++ tests/unit/s2n_client_extensions_test.c
@@ -940,8 +940,7 @@ int main(int argc, char **argv)
         EXPECT_EQUAL(s2n_connection_is_ocsp_stapled(server_conn), 1);
 
         /* Verify that the client received an OCSP response. */
-        /* Currently fails test. Remove when https://github.com/awslabs/s2n/issues/2239 is fixed */
-        /* EXPECT_EQUAL(s2n_connection_is_ocsp_stapled(client_conn), 1); */
+        EXPECT_EQUAL(s2n_connection_is_ocsp_stapled(client_conn), 1);
 
         EXPECT_NOT_NULL(server_ocsp_reply = s2n_connection_get_ocsp_response(client_conn, &length));
         EXPECT_EQUAL(length, sizeof(server_ocsp_status));
