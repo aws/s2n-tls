929b1d2b08dee1e427ba752c653d102c8b6d09cb
ranshid <88133677+ranshid@users.noreply.github.com>
2021-09-19 15:45:05 +0300

    Update crypto/s2n_hash.c
    Co-authored-by: Lindsay Stewart <stewart.r.lindsay@gmail.com>

--- crypto/s2n_hash.c
+++ crypto/s2n_hash.c
@@ -94,9 +94,7 @@ static int s2n_low_level_hash_new(struct s2n_hash_state *state)
      * being used. For the s2n_low_level_hash implementation, new is a no-op.
      */
 
-    state->is_ready_for_input = 0;
-    state->currently_in_hash = 0;
-    memset(&state->digest,0,sizeof(state->digest));
+    *state = (struct s2n_hash_state) { 0 };
     return S2N_SUCCESS;
 }
 
