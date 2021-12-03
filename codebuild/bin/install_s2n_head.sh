3c92bb9632e6dc7f5aff08b27a172d014dd0203e
Madeleine <madelea@amazon.com>
2021-12-03 14:24:21 -0800

    Added script to compile main version of s2n

---
+++ codebuild/bin/install_s2n_head.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#  http://aws.amazon.com/apache2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+set -ex
+pushd "$(pwd)"
+
+usage() {
+    echo "install_s2n_head.sh build_dir"
+    exit 1
+}
+
+if [ "$#" -ne "1" ]; then
+    usage
+fi
+
+BUILD_DIR=$1
+source codebuild/bin/jobs.sh
+cd "$BUILD_DIR"
+
+mkdir s2n_head
+cd s2n_head
+
+# Clone the most recent s2n commit
+git clone https://github.com/aws/s2n-tls
+
+mkdir ../build
+cd ../build
+
+cmake ../s2n_head/s2n-tls -DCMAKE_PREFIX_PATH="$BASE_S2N_DIR"/test-deps/openssl-1.1.1/
+make -j $JOBS
+
+# Create symbolic link for s2nc and s2nd
+ln -sf "$BUILD_DIR"/build/bin/s2nc "$BASE_S2N_DIR"/bin/s2nc_head
+ln -sf "$BUILD_DIR"/build/bin/s2nd "$BASE_S2N_DIR"/bin/s2nd_head
+
+popd
+
+exit 0
--- tests/integrationv2/common.py
+++ tests/integrationv2/common.py
@@ -403,6 +403,7 @@ class ProviderOptions(object):
             verify_hostname=None,
             server_name=None,
             protocol=None,
+            older_version=None,
             env_overrides=dict()):
 
         # Client or server
@@ -460,6 +461,9 @@ class ProviderOptions(object):
 
         # Extra flags to pass to the provider
         self.extra_flags = extra_flags
+
+        # Boolean whether the provider is an older version of s2n
+        self.older_version = older_version
 
         # Extra environment parameters
         self.env_overrides = env_overrides
