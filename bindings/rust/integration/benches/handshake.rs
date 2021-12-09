70aea9ca52c8edf4fbacdfdcde6700553d504748
Doug Chapman <dougch@amazon.com>
2021-12-09 21:41:11 +0000

    s2nc cargo harness

--- bindings/rust/integration/benches/handshake.rs
+++ bindings/rust/integration/benches/handshake.rs
@@ -2,16 +2,22 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use criterion::{criterion_group, criterion_main, Criterion};
-use subprocess::Exec;
+use s2n_tls::{
+    raw::security,
+    testing::{build_config, s2n_tls_pair},
+};
 
 pub fn handshake(c: &mut Criterion) {
     let mut group = c.benchmark_group("s2n-tls_client_server");
 
-        group.bench_function(format!("handshake_"), move |b| {
+    for policy in security::ALL_POLICIES {
+        let config = build_config(policy).unwrap();
+        group.bench_function(format!("handshake_{:?}", policy), move |b| {
             // This does include connection initalization overhead.
             // TODO: create a separate benchamrk that excludes this step.
-            b.iter(|| Exec::shell("pstree").join());
+            b.iter(|| s2n_tls_pair(config.clone()));
         });
+    }
 
     group.finish();
 }
