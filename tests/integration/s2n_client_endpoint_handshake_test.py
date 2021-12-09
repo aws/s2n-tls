c0902d38bd5fabf56fc0bcb7d7fc656ab37bf2a2
toidiu <apoorv@toidiu.com>
2021-12-08 23:27:04 -0800

    apply fix to s2n_pq_handshake_test

--- tests/integration/s2n_client_endpoint_handshake_test.py
+++ tests/integration/s2n_client_endpoint_handshake_test.py
@@ -118,7 +118,7 @@ def try_client_handshake(endpoint, arguments, expected_cipher):
     """
     s2nc_cmd = ["../../bin/s2nc", "-f", "./trust-store/ca-bundle.crt", "-a", "http/1.1"] + arguments + [str(endpoint)]
     currentDir = os.path.dirname(os.path.realpath(__file__))
-    s2nc = subprocess.Popen(s2nc_cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, cwd=currentDir, universal_newlines=True)
+    s2nc = subprocess.Popen(s2nc_cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, cwd=currentDir)
 
     try:
         outs, errs = s2nc.communicate(timeout=5)
