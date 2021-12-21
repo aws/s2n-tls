0e97b5859e45dde919853905ec7723b0ab09fc1a
Doug Chapman <dougch@amazon.com>
2021-12-20 16:42:05 -0800

    turn down tox logging, add some to harness

--- bindings/rust/integration/benches/s2nc.rs
+++ bindings/rust/integration/benches/s2nc.rs
@@ -3,7 +3,7 @@
 
 use criterion::{criterion_group, criterion_main, Criterion};
 use std::{
-    env,
+    dbg, env,
     io::{self, Write},
     process::Command,
 };
@@ -16,7 +16,7 @@ pub fn s2nc(c: &mut Criterion) {
             let s2nc_args = env::var("S2NC_ARGS").unwrap();
             let s2nc_args_split = s2nc_args.split(' ');
             let s2nc_args_vec = s2nc_args_split.collect::<Vec<&str>>();
-            println!("DEBUG: S2NC_ARGS {:?}", s2nc_args_vec);
+            dbg!("DEBUG: S2NC_ARGS {:?}", &s2nc_args_vec);
             assert_ne!(s2nc_args.len(), 0);
             let output = Command::new("/opt/s2n/bin/s2nc")
                 .args(s2nc_args_vec)
@@ -25,6 +25,7 @@ pub fn s2nc(c: &mut Criterion) {
 
             io::stdout().write_all(&output.stdout).unwrap();
             io::stderr().write_all(&output.stderr).unwrap();
+            dbg!("DEBUG: return code {:?}", &output.status);
         });
     });
 
