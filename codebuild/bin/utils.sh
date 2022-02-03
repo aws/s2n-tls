1e8fbcf5a5e6cdcf97e9b5c228c3f7658305a5b0
Doug Chapman <dougch@amazon.com>
2022-02-02 17:34:53 -0800

    ci: Move much of the baseline buildspec into a script; create an inline folder for specs not directly used by CB

---
+++ codebuild/bin/utils.sh
@@ -0,0 +1,12 @@
+# Utility functions
+get_latest_release(){
+    export LATEST_RELEASE_URL=$(gh api /repos/aws/s2n-tls/releases/latest|jq -r '.tarball_url')
+    export LATEST_RELEASE_VER=$(echo $RELEASE_URL | sed 's|.*/||')
+}
+
+gh_login(){
+  # Takes secrets manager key as an argument
+  TOKEN=$(aws secretsmanager get-secret-value --secret-id "$1" --query 'SecretString' --output text |jq -r '.secret_key')
+  echo "$TOKEN"|gh auth login --with-token
+  gh auth status
+}
