2aa2bd81c46d85f972bd58ee1566d1c50231e90b
Lindsay Stewart <slindsay@amazon.com>
2022-02-03 20:31:51 -0800

    Enable PQ in FIPS mode with awslc (#3183)

---
+++ tests/unit/s2n_openssl_test.c
@@ -0,0 +1,40 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+#include "s2n_test.h"
+#include "crypto/s2n_openssl.h"
+
+int main(int argc, char **argv)
+{
+    BEGIN_TEST();
+
+    const char* env_libcrypto = getenv("S2N_LIBCRYPTO");
+    if (env_libcrypto == NULL) {
+        END_TEST();
+    }
+
+    if (strcmp(env_libcrypto, "boringssl") == 0) {
+        EXPECT_FALSE(s2n_libcrypto_is_awslc());
+        EXPECT_TRUE(s2n_libcrypto_is_boringssl());
+    } else if (strcmp(env_libcrypto, "awslc") == 0 || strcmp(env_libcrypto, "awslc-fips") == 0) {
+        EXPECT_TRUE(s2n_libcrypto_is_awslc());
+        EXPECT_FALSE(s2n_libcrypto_is_boringssl());
+    } else {
+        EXPECT_FALSE(s2n_libcrypto_is_awslc());
+        EXPECT_FALSE(s2n_libcrypto_is_boringssl());
+    }
+
+    END_TEST();
+}
