bd481b6b641cfd2932c606e9d430fdd68416e525
Doug Chapman <dougch@amazon.com>
2022-03-23 10:45:26 -0700

    addtional dashboard feedback

--- .github/bin/label_issues.sh
+++ .github/bin/label_issues.sh
@@ -2,23 +2,34 @@
 set -eu
 
 update_label() {
-# $1 is a \n separated list of users
+# $1 is a username
+USER=$1
 # $2 is a cutoff date; use $(date +%Y-%m-%d) for now
+DATE=$2
 
-for USER in $1; do
-  echo "Looking up issues for $USER, continue?"
-  read 
-  gh api -X GET search/issues -f q="repo:aws/s2n-tls is:open -label:s2n-core author:$USER created:<$2" > $USER.json
+  gh api -X GET search/issues -f q="repo:aws/s2n-tls is:open -label:s2n-core author:${USER} created:<${DATE}" > ${USER}.json
   jq  -c -r '.items|.[]|.number' $USER.json > ${USER}_ISSUES.log
+  if [ $(cat ${USER}_ISSUES.log|wc -l) -eq 0 ]; then
+    echo "No issues for ${USER}"
+    return
+  fi
+  echo "Found $(cat ${USER}_ISSUES.log|wc -l) issues for $USER, continue?"
+  read
   for issue in $(cat ${USER}_ISSUES.log); do
-     echo "Updating $issue"
-     echo '["s2n-core"]'| gh api --silent -X POST repos/$OWNER/$REPO/issues/$issue/labels --input -
+    echo "Updating $issue"
+    echo '["s2n-core"]'| gh api --silent -X POST repos/aws/s2n-tls/issues/$issue/labels --input -
   done
   echo "Done with $USER"
-done
 }
 
 internal="toidiu
+zaherd
+NLMalloy
+ttjsu-aws
+rday
+agray256
+tawdry-audrey
+salusasecondus
 dougch
 lrstewart
 goatgoose
@@ -32,5 +43,10 @@ baldwinmatt
 soco
 alexeblee"
 
-update $internal $(date +%Y-%m-%d)
-update_label $maint 2019-10-23
+for USER in ${internal}; do
+  update_label ${USER} $(date +%Y-%m-%d)
+done
+
+for USER in ${maint}; do
+  update_label ${USER} 2019-10-23
+done
