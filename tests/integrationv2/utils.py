4a0e5dc43b7ccd31cc70c73be64257357d5adea6
Sam Clark <vclarksa@amazon.com>
2022-03-24 20:10:21 -0400

    only disable s2n -> s2n tests with awslc with fips

--- tests/integrationv2/utils.py
+++ tests/integrationv2/utils.py
@@ -77,10 +77,10 @@ def invalid_test_parameters(*args, **kwargs):
             if certificate is not None:
                 if "openssl-1.0.2-fips" in get_flag(S2N_PROVIDER_VERSION) and "ECDSA" not in certificate.name:
                     return True
-            
+
             # if s2n is built with awslc and TLS version is < 1.3, it can't connect to itself
             if protocol is not None:
-                if "awslc" in get_flag(S2N_PROVIDER_VERSION) and protocol != Protocols.TLS13:
+                if "awslc-fips" in get_flag(S2N_PROVIDER_VERSION) and protocol != Protocols.TLS13:
                     return True
 
     if cipher is not None:
