dd4ff6f7e6c8f2b25b5093261a13487464d332a0
Torben Hansen <50673096+torben-hansen@users.noreply.github.com>
2022-03-27 09:26:50 -0700

    No need to use two stanzas

--- crypto/s2n_openssl.h
+++ crypto/s2n_openssl.h
@@ -40,13 +40,9 @@
 
 #if (S2N_OPENSSL_VERSION_AT_LEAST(1, 1, 0)) && (!defined(OPENSSL_IS_BORINGSSL)) && (!defined(OPENSSL_IS_AWSLC))
 #define s2n_evp_ctx_init(ctx) POSIX_GUARD_OSSL(EVP_CIPHER_CTX_init(ctx), S2N_ERR_DRBG)
-#else
-#define s2n_evp_ctx_init(ctx) EVP_CIPHER_CTX_init(ctx)
-#endif
-
-#if (S2N_OPENSSL_VERSION_AT_LEAST(1, 1, 0)) && (!defined(OPENSSL_IS_BORINGSSL)) && (!defined(OPENSSL_IS_AWSLC))
 #define s2n_evp_ctx_init_result(ctx) RESULT_GUARD_OSSL(EVP_CIPHER_CTX_init(ctx), S2N_ERR_DRBG)
 #else
+#define s2n_evp_ctx_init(ctx) EVP_CIPHER_CTX_init(ctx)
 #define s2n_evp_ctx_init_result(ctx) EVP_CIPHER_CTX_init(ctx)
 #endif
 
