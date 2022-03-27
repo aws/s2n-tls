beeabfcb77ccb762c81a7e5866ccaf7b7f54e1ca
Torben Hansen <50673096+torben-hansen@users.noreply.github.com>
2022-03-27 09:12:02 -0700

    alignment

--- utils/s2n_blob.h
+++ utils/s2n_blob.h
@@ -47,18 +47,18 @@ extern int s2n_blob_char_to_lower(struct s2n_blob *b);
 extern int s2n_hex_string_to_bytes(const uint8_t *str, struct s2n_blob *blob);
 extern int s2n_blob_slice(const struct s2n_blob *b, struct s2n_blob *slice, uint32_t offset, uint32_t size);
 
-#define s2n_stack_blob(name, requested_size, maximum)			\
-    size_t name ## _requested_size = (requested_size);			\
-    uint8_t name ## _buf[(maximum)] = {0};				\
-    POSIX_ENSURE_LTE(name ## _requested_size, (maximum));			\
-    struct s2n_blob name = {0};						\
+#define s2n_stack_blob(name, requested_size, maximum)       \
+    size_t name ## _requested_size = (requested_size);      \
+    uint8_t name ## _buf[(maximum)] = {0};                  \
+    POSIX_ENSURE_LTE(name ## _requested_size, (maximum));   \
+    struct s2n_blob name = {0};                             \
     POSIX_GUARD(s2n_blob_init(&name, name ## _buf, name ## _requested_size))
 
-#define s2n_stack_blob_result(name, requested_size, maximum)           \
+#define s2n_stack_blob_result(name, requested_size, maximum)    \
     size_t name ## _requested_size = (requested_size);          \
-    uint8_t name ## _buf[(maximum)] = {0};              \
-    RESULT_ENSURE_LTE(name ## _requested_size, (maximum));           \
-    struct s2n_blob name = {0};                     \
+    uint8_t name ## _buf[(maximum)] = {0};                      \
+    RESULT_ENSURE_LTE(name ## _requested_size, (maximum));      \
+    struct s2n_blob name = {0};                                 \
     RESULT_GUARD_POSIX(s2n_blob_init(&name, name ## _buf, name ## _requested_size))
 
 #define S2N_BLOB_LABEL(name, str) \
