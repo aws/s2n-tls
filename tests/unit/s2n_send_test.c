1d022f55a40d79d729fb7c6fd36b9bb6806c278b
carl Lundin <lundinc@amazon.com>
2022-05-25 15:08:58 -0700

    fix: Accurately track wire_bytes_out
    The wire_bytes_out variable on the s2n_connection object
    was over reporting how many bytes have been sent.
    
    The s2n_record_writev function in s2n_record_write.c is incrementing the
    amount of bytes written to the conn->out stuffer for the TLS record that
    was created.
    
    These bytes are also reported in s2n_send when they are sent over the TCP
    socket.
    
    This causes record bytes to be counted twice, giving a misleading
    wire_bytes_out metric when calling s2n_connection_get_wire_bytes_out.

--- tests/unit/s2n_send_test.c
+++ tests/unit/s2n_send_test.c
@@ -19,6 +19,7 @@
 #include "api/s2n.h"
 
 bool s2n_custom_send_fn_called = false;
+static uint64_t sent_bytes = 0;
 
 int s2n_expect_concurrent_error_send_fn(void *io_context, const uint8_t *buf, uint32_t len)
 {
@@ -30,6 +31,37 @@ int s2n_expect_concurrent_error_send_fn(void *io_context, const uint8_t *buf, ui
     EXPECT_FAILURE_WITH_ERRNO(result, S2N_ERR_REENTRANCY);
     return result;
 }
+
+static int s2n_track_sent_bytes_fn(void *io_context, const uint8_t *buf, uint32_t len)
+{
+    struct s2n_connection *conn = (struct s2n_connection*) io_context;
+    s2n_custom_send_fn_called = true;
+
+    sent_bytes = len;
+
+    return len;
+}
+
+static int s2n_track_sent_bytes_partial_send_fn(void *io_context, const uint8_t *buf, uint32_t len)
+{
+    struct s2n_connection *conn = (struct s2n_connection*) io_context;
+
+    /* Break loop on second call. */
+    if (s2n_custom_send_fn_called) {
+        errno = EPIPE;
+        return -1;
+    }
+
+    int partial_read = len-3;
+
+    sent_bytes = partial_read;
+    errno = EAGAIN;
+
+    s2n_custom_send_fn_called = true;
+
+    return partial_read;
+}
+
 
 int main(int argc, char **argv)
 {
@@ -54,6 +86,59 @@ int main(int argc, char **argv)
         EXPECT_FAILURE_WITH_ERRNO(s2n_send(conn, test_data, sizeof(test_data), &blocked),
                 S2N_ERR_IO);
         EXPECT_TRUE(s2n_custom_send_fn_called);
+        EXPECT_EQUAL(0, conn->wire_bytes_out);
+
+        /* Cleanup */
+        EXPECT_SUCCESS(s2n_connection_free(conn));
+    }
+
+    /* s2n_send tracks conn->wire_bytes_out on send */
+    {
+        struct s2n_connection *conn;
+        EXPECT_NOT_NULL(conn = s2n_connection_new(S2N_CLIENT));
+        EXPECT_OK(s2n_connection_set_secrets(conn));
+
+        EXPECT_EQUAL(0, conn->wire_bytes_out);
+
+        EXPECT_SUCCESS(s2n_connection_set_send_cb(conn, s2n_track_sent_bytes_fn));
+        EXPECT_SUCCESS(s2n_connection_set_send_ctx(conn, (void*) conn));
+
+        /* Send test data */
+        uint8_t test_data[] = "hello world";
+        s2n_blocked_status blocked = 0;
+
+        s2n_custom_send_fn_called = false;
+        EXPECT_EQUAL(sizeof(test_data), s2n_send(conn, test_data, sizeof(test_data), &blocked));
+        EXPECT_TRUE(s2n_custom_send_fn_called);
+        EXPECT_EQUAL(sent_bytes, conn->wire_bytes_out);
+        EXPECT_EQUAL(sent_bytes, s2n_connection_get_wire_bytes_out(conn));
+        EXPECT_EQUAL(conn->wire_bytes_out, s2n_connection_get_wire_bytes_out(conn));
+
+        /* Cleanup */
+        EXPECT_SUCCESS(s2n_connection_free(conn));
+    }
+
+    /* s2n_send tracks conn->wire_bytes_out on send */
+    {
+        struct s2n_connection *conn;
+        EXPECT_NOT_NULL(conn = s2n_connection_new(S2N_CLIENT));
+        EXPECT_OK(s2n_connection_set_secrets(conn));
+
+        EXPECT_EQUAL(0, conn->wire_bytes_out);
+
+        EXPECT_SUCCESS(s2n_connection_set_send_cb(conn, s2n_track_sent_bytes_partial_send_fn));
+        EXPECT_SUCCESS(s2n_connection_set_send_ctx(conn, (void*) conn));
+
+        /* Send test data */
+        uint8_t test_data[] = "hello world";
+        s2n_blocked_status blocked = 0;
+
+        s2n_custom_send_fn_called = false;
+        EXPECT_FAILURE_WITH_ERRNO(s2n_send(conn, test_data, sizeof(test_data), &blocked), S2N_ERR_IO);
+        EXPECT_TRUE(s2n_custom_send_fn_called);
+        EXPECT_EQUAL(sent_bytes, conn->wire_bytes_out);
+        EXPECT_EQUAL(sent_bytes, s2n_connection_get_wire_bytes_out(conn));
+        EXPECT_EQUAL(conn->wire_bytes_out, s2n_connection_get_wire_bytes_out(conn));
 
         /* Cleanup */
         EXPECT_SUCCESS(s2n_connection_free(conn));
