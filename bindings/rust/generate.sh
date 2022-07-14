0dc2ff468399ebb59f4dfcd48516ca770d3f2125
Justin W Smith <103147162+justsmth@users.noreply.github.com>
2022-07-14 19:40:00 -0400

    Fix oops

--- bindings/rust/generate.sh
+++ bindings/rust/generate.sh
@@ -34,17 +34,19 @@ cp -r \
   s2n-tls-sys/lib/
 
 # generate the bindings modules from the copied sources
-cd generate && cargo run -- ../s2n-tls-sys && cd ..
+pushd generate
+cargo run -- ../s2n-tls-sys
+popd 
 
 # make sure everything builds and passes sanity checks
 pushd s2n-tls-sys
 cargo test
 cargo test --features pq
-cargo test --features quic \
-cargo test --features internal \
-cargo test --release \
-cargo publish --dry-run --allow-dirty \
-cargo publish --dry-run --allow-dirty --all-features \
+cargo test --features quic
+cargo test --features internal
+cargo test --release
+cargo publish --dry-run --allow-dirty 
+cargo publish --dry-run --allow-dirty --all-features
 popd
 
 pushd integration
