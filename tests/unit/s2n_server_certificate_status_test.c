0cc0d4b398067aeb65e95af4b5d4d80de7f2a0ed
Sam Clark <3758302+goatgoose@users.noreply.github.com>
2022-08-14 21:12:45 -0400

    use runtime check for ocsp stapling support

--- tests/unit/s2n_server_certificate_status_test.c
+++ tests/unit/s2n_server_certificate_status_test.c
@@ -159,14 +159,14 @@ int main(int argc, char **argv)
 
         EXPECT_SUCCESS(s2n_tls13_server_status_request_extension.send(conn, &stuffer));
 
-#if defined(OPENSSL_IS_BORINGSSL)
-        /* s2n_x509_validator_validate_cert_stapled_ocsp_response returns untrusted error if ocsp is not supported */
-        EXPECT_FAILURE_WITH_ERRNO(s2n_tls13_server_status_request_extension.recv(conn, &stuffer),
-                S2N_ERR_CERT_UNTRUSTED);
-#else
-        EXPECT_FAILURE_WITH_ERRNO(s2n_tls13_server_status_request_extension.recv(conn, &stuffer),
-                S2N_ERR_INVALID_OCSP_RESPONSE);
-#endif
+        if (s2n_x509_ocsp_stapling_supported()) {
+            EXPECT_FAILURE_WITH_ERRNO(s2n_tls13_server_status_request_extension.recv(conn, &stuffer),
+                    S2N_ERR_INVALID_OCSP_RESPONSE);
+        } else {
+            /* s2n_x509_validator_validate_cert_stapled_ocsp_response returns untrusted error if ocsp is not supported */
+            EXPECT_FAILURE_WITH_ERRNO(s2n_tls13_server_status_request_extension.recv(conn, &stuffer),
+                    S2N_ERR_CERT_UNTRUSTED);
+        }
 
         EXPECT_SUCCESS(s2n_connection_free(conn));
     }
