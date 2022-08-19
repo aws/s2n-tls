b7a55d03e543ae07844f56fd6d349298dae215fd
toidiu <apoorv@toidiu.com>
2022-08-19 15:41:22 -0700

    openssl3 integration: fix padding

--- crypto/s2n_cbc_cipher_3des.c
+++ crypto/s2n_cbc_cipher_3des.c
@@ -57,7 +57,7 @@ static int s2n_cbc_cipher_3des_set_decryption_key(struct s2n_session_key *key, s
 {
     POSIX_ENSURE_EQ(in->size, 192 / 8);
 
-    EVP_CIPHER_CTX_set_padding(key->evp_cipher_ctx, EVP_CIPH_NO_PADDING);
+    EVP_CIPHER_CTX_set_padding(key->evp_cipher_ctx, 0);
     POSIX_GUARD_OSSL(EVP_DecryptInit_ex(key->evp_cipher_ctx, EVP_des_ede3_cbc(), NULL, in->data, NULL), S2N_ERR_KEY_INIT);
 
     return 0;
@@ -67,7 +67,7 @@ static int s2n_cbc_cipher_3des_set_encryption_key(struct s2n_session_key *key, s
 {
     POSIX_ENSURE_EQ(in->size, 192 / 8);
 
-    EVP_CIPHER_CTX_set_padding(key->evp_cipher_ctx, EVP_CIPH_NO_PADDING);
+    EVP_CIPHER_CTX_set_padding(key->evp_cipher_ctx, 0);
     POSIX_GUARD_OSSL(EVP_EncryptInit_ex(key->evp_cipher_ctx, EVP_des_ede3_cbc(), NULL, in->data, NULL), S2N_ERR_KEY_INIT);
 
     return 0;
