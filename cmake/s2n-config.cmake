e8a6bcc35bdd556e85d8a5530a782858b0cf9c8c
DougCh <54039637+dougch@users.noreply.github.com>
2022-09-09 16:45:59 -0700

    Revert "Allow static and shared libs to be mixed. (#3467)"
    This reverts commit 60005ef4c75fd6362c5aa9357b35de99326e3207.

--- cmake/s2n-config.cmake
+++ cmake/s2n-config.cmake
@@ -8,19 +8,9 @@ endif()
 list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/modules")
 find_dependency(crypto)
 
-# Allow static or shared lib to be used.
-# If both are installed, choose based on BUILD_SHARED_LIBS.
 if (BUILD_SHARED_LIBS)
-    if (EXISTS "${CMAKE_CURRENT_LIST_DIR}/shared")
-        include(${CMAKE_CURRENT_LIST_DIR}/shared/@PROJECT_NAME@-targets.cmake)
-    else()
-        include(${CMAKE_CURRENT_LIST_DIR}/static/@PROJECT_NAME@-targets.cmake)
-    endif()
+    include(${CMAKE_CURRENT_LIST_DIR}/shared/@PROJECT_NAME@-targets.cmake)
 else()
-    if (EXISTS "${CMAKE_CURRENT_LIST_DIR}/static")
-        include(${CMAKE_CURRENT_LIST_DIR}/static/@PROJECT_NAME@-targets.cmake)
-    else()
-        include(${CMAKE_CURRENT_LIST_DIR}/shared/@PROJECT_NAME@-targets.cmake)
-    endif()
+    include(${CMAKE_CURRENT_LIST_DIR}/static/@PROJECT_NAME@-targets.cmake)
 endif()
 
