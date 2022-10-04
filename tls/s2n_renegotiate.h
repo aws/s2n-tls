9480afde02fa7e81a25a82c9433a49d255082251
Lindsay Stewart <slindsay@amazon.com>
2022-10-04 12:04:20 -0700

    Add API to handle renegotiation

--- tls/s2n_renegotiate.h
+++ tls/s2n_renegotiate.h
@@ -15,11 +15,19 @@
 
 #pragma once
 
+#include "utils/s2n_result.h"
+
 struct s2n_connection;
 struct s2n_config;
+
+extern const s2n_blocked_status S2N_BLOCKED_ON_APPLICATION_DATA;
 
 typedef enum { S2N_RENEGOTIATE_REJECT, S2N_RENEGOTIATE_ACCEPT} s2n_renegotiate_response;
 typedef int (*s2n_renegotiate_request_cb)(struct s2n_connection *conn, void *context, s2n_renegotiate_response *response);
 int s2n_config_set_renegotiate_request_cb(struct s2n_config *config, s2n_renegotiate_request_cb cb, void *ctx);
 
 int s2n_renegotiate_wipe(struct s2n_connection *conn);
+int s2n_renegotiate(struct s2n_connection *conn, uint8_t *app_data_buf, ssize_t app_data_buf_size,
+        ssize_t *app_data_size, s2n_blocked_status *blocked);
+
+S2N_RESULT s2n_renegotiate_validate(struct s2n_connection *conn);
