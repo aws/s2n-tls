ebc1863d7a3a5f3e40cf6004146e2d9ac4832fee
Harrison Kaiser <uwaces@gmail.com>
2022-10-28 11:58:33 -0400

    Fix time line send data - resolve flakiness
    Previously these tests would inexplicably fail, and often need to be
    automatically retried in order to PASS. This was a result of the time
    line for sending and receiving data being unclear. By making the order
    of events (including send times *and* close times) the tests no longer
    mysteriously fail.
    
    One particularly tricky aspect of this is that the data must be sent
    after the handshake is complete. But not all cli utilities are equally
    clear about when that moment is. S2N always prints "s2n is ready", but
    GnuTLS is not so clear. The only tool the framework provides to find
    this moment is a so-called send marker. That is a string to search for
    on stdout after which data is to be provided to stdin. To find those
    strings and ensure that the first instance of them was on stdout when
    the handshake was ready took some time.
    
    This strategy for making the tests more reliable also required that we
    have a send marker on the server. In this case I used the connected
    client to send a send marker to the server, which will appear on its
    stdout.
    
    Lastly I found that, due to a small send buffer and resulting message
    fragmentation, GnuTLS and OpenSSL would often choose to print the
    received app data in a non-contiguous fashion on stdout. So I needed
    to resort to a subsequence (not substring) check to ensure the peer
    received the application data.
    
    Currently I am limiting the selection of certs only to ensure the test
    takes a reasonable amount of time. In a test framework that has better
    support for parallelism I would advise using them all, but the test is
    already around ~15 minutes (not including the CI setup and teardown).

--- tests/integrationv2/test_buffered_send.py
+++ tests/integrationv2/test_buffered_send.py
@@ -1,6 +1,6 @@
 import pytest
 
-from configuration import available_ports, PROTOCOLS, Protocols, ALL_TEST_CIPHERS, Certificates
+from configuration import available_ports, PROTOCOLS, ALL_TEST_CIPHERS, Certificates
 from common import ProviderOptions, data_bytes
 from fixtures import managed_process # lgtm [py/unused-import]
 from providers import Provider, S2N, OpenSSL, GnuTLS
@@ -24,28 +24,75 @@ SEND_BUFFER_SIZES = [
 ]
 
 TEST_CERTS = [
-    Certificates.RSA_4096_SHA512,
-    Certificates.ECDSA_384,
-    Certificates.RSA_PSS_2048_SHA256
+    Certificates.RSA_2048_SHA256,
+    Certificates.RSA_PSS_2048_SHA256,
+    Certificates.ECDSA_256
 ]
+
+def is_subsequence(s, t):
+    """predicate: is s a subsequence of t?
+
+    Parameters
+    ----------
+    s: bytes
+        subsequence to look for
+    t: str, bytes
+        sequence to look in
+
+    Returns
+    -------
+    bool
+        True if s is a subsequence, False if not 
+    """
+    s = str(s)[2:-1] # Remove the b' and ' from the converted bytes
+    t = str(t)
+    s_len, t_len = len(s), len(t)
+    s_index, t_index = 0, 0
+    while (s_index < s_len and t_index < t_len):
+        if (s[s_index] == t[t_index]):
+            s_index += 1
+        t_index += 1
+    return s_index == s_len
+
 
 @pytest.mark.uncollect_if(func=invalid_test_parameters)
 @pytest.mark.parametrize("cipher", ALL_TEST_CIPHERS, ids=get_parameter_name)
-@pytest.mark.parametrize("other_provider", [S2N, OpenSSL, GnuTLS], ids=get_parameter_name)
+@pytest.mark.parametrize("other_provider", [GnuTLS, OpenSSL, S2N], ids=get_parameter_name)
 @pytest.mark.parametrize("provider", [S2N], ids=get_parameter_name)
 @pytest.mark.parametrize("protocol", PROTOCOLS, ids=get_parameter_name)
 @pytest.mark.parametrize("certificate", TEST_CERTS, ids=get_parameter_name)
 @pytest.mark.parametrize("buffer_size", SEND_BUFFER_SIZES, ids=get_parameter_name)
 @pytest.mark.parametrize("fragment_preference", [None, "--prefer-low-latency", "--prefer-throughput"], ids=get_parameter_name)
 def test_s2n_buffered_send_server(managed_process, cipher, other_provider, provider, protocol, certificate, buffer_size, fragment_preference):
+    # Communication Timeline
+    # Client [S2N|OpenSSL|GnuTLS]  | Server [S2N]
+    # Handshake                    | Handshake
+    #  Handshake finish indicated
+    #  by the client send marker
+    # Send Server Send Marker      | Receive the Server Send Marker "SENT_FROM_CLIENT"
+    #                              | Send Data Bytes to Client (stresses the send buffer - center of test)
+    #                              | Send Client Close Marker "SENT_FROM_SERVER"
+    # Close                        | Close
+    
+    # for the corresponding provider, these strings appear when on stdout when the handshake is finished
+    starting_client_send_markers = {
+        GnuTLS: "Handshake was completed",
+        OpenSSL: "write to",
+        S2N: "ready"
+    }
+    starting_client_send_marker = starting_client_send_markers[other_provider]
+
+    starting_server_send_marker = "SENT_FROM_CLIENT"
+    client_inital_app_data = to_bytes(starting_server_send_marker)
+    client_close_marker = server_sent_final = "SENT_FROM_SERVER"
+    data_bytes_server = data_bytes(SEND_DATA_SIZE) + to_bytes(server_sent_final)
+
     port = next(available_ports)
-    random_bytes = data_bytes(SEND_DATA_SIZE)
 
     client_options = ProviderOptions(
         mode=Provider.ClientMode,
         port=port,
-        cipher=cipher,
-        data_to_send=None,
+        data_to_send=client_inital_app_data,
         insecure=True,
         protocol=protocol)
 
@@ -56,42 +103,54 @@ def test_s2n_buffered_send_server(managed_process, cipher, other_provider, provi
         mode=Provider.ServerMode,
         port=port,
         cipher=cipher,
-        data_to_send=random_bytes,
+        data_to_send=data_bytes_server,
         insecure=True,
         protocol=protocol,
         key=certificate.key,
         cert=certificate.cert,
         extra_flags=extra_flags)
 
-    server = managed_process(provider, server_options, timeout=5)
-    client = managed_process(other_provider, client_options, timeout=5)
+    server = managed_process(provider, server_options, timeout=30, send_marker=[starting_server_send_marker])
+    client = managed_process(other_provider, client_options, timeout=30,
+        send_marker=[starting_client_send_marker], close_marker=client_close_marker)
 
     for results in client.get_results():
-        if other_provider is S2N:
-            assert(len(results.stderr) == 0)
-            assert(to_bytes("CONNECTED") in results.stdout)
+        # for small buffer sizes the received data will not be contiguous on stdout
+        assert(is_subsequence(data_bytes_server, results.stdout))
         results.assert_success()
 
     for results in server.get_results():
+        # the server should close without error
+        # but there is otherwise nothing of interest on stdout 
         results.assert_success()
 
 
 @pytest.mark.uncollect_if(func=invalid_test_parameters)
 @pytest.mark.parametrize("cipher", ALL_TEST_CIPHERS, ids=get_parameter_name)
 @pytest.mark.parametrize("other_provider", [S2N], ids=get_parameter_name)
-@pytest.mark.parametrize("provider", [OpenSSL], ids=get_parameter_name)
+@pytest.mark.parametrize("provider", [S2N, OpenSSL], ids=get_parameter_name)
 @pytest.mark.parametrize("protocol", PROTOCOLS, ids=get_parameter_name)
 @pytest.mark.parametrize("certificate", TEST_CERTS, ids=get_parameter_name)
 @pytest.mark.parametrize("buffer_size", SEND_BUFFER_SIZES, ids=get_parameter_name)
 def test_s2n_buffered_send_client(managed_process, cipher, other_provider, provider, protocol, certificate, buffer_size):
+    # Communication Timeline
+    # Client [S2N]                 | Server [S2N|OpenSSL]
+    # Handshake                    | Handshake
+    #  Handshake finish indicated
+    #  "s2n is ready"
+    # Send Data Bytes to Server    | Receive the Data Bytes
+    #  stresses the send buffer
+    #  center of test
+    # Send Server Close Marker     | Receive the close Marker
+    # Close                        | Close
     port = next(available_ports)
-    random_bytes = data_bytes(SEND_DATA_SIZE)
+    server_close_marker = client_sent_final = "SENT_FROM_CLIENT"
+    client_data_to_send = data_bytes(SEND_DATA_SIZE) + to_bytes(client_sent_final)
 
     client_options = ProviderOptions(
         mode=Provider.ClientMode,
         port=port,
-        cipher=cipher,
-        data_to_send=None,
+        data_to_send=client_data_to_send,
         insecure=True,
         protocol=protocol,
         extra_flags=['--buffered-send', buffer_size])
@@ -100,20 +159,17 @@ def test_s2n_buffered_send_client(managed_process, cipher, other_provider, provi
         mode=Provider.ServerMode,
         port=port,
         cipher=cipher,
-        data_to_send=random_bytes,
         insecure=True,
         protocol=protocol,
         key=certificate.key,
         cert=certificate.cert)
 
-    server = managed_process(provider, server_options, timeout=5)
-    client = managed_process(other_provider, client_options, timeout=5)
+    server = managed_process(provider, server_options, close_marker=server_close_marker, timeout=30)
+    client = managed_process(other_provider, client_options, send_marker=["s2n is ready"], timeout=30)
 
     for results in client.get_results():
-        assert(len(results.stderr) == 0)
-        assert(to_bytes("CONNECTED") in results.stdout)
         results.assert_success()
 
     for results in server.get_results():
+        assert(is_subsequence(client_data_to_send, results.stdout))
         results.assert_success()
-
