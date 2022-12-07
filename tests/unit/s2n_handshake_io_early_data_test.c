c7455b11cc68a31bebe3596ea9c24f2c8435d455
Harrison Kaiser <uwaces@gmail.com>
2022-12-07 16:42:01 -0500

    Responded to comments.

--- tests/unit/s2n_handshake_io_early_data_test.c
+++ tests/unit/s2n_handshake_io_early_data_test.c
@@ -26,31 +26,11 @@ int main(int argc, char **argv)
     BEGIN_TEST();
 
     uint8_t bad_record[] = {
-        /* ContentType opaque_type = application_data */
-        0x17,
-        /* ProtocolVersion legacy_record_version = 0x0303 */
-        0x03,
-        0x03,
-        /* uint16 length */
-        0x00,
-        0x10,
-        /* opaque encrypted_record[TLSCiphertext.length] */
-        0x00,
-        0x01,
-        0x02,
-        0x03,
-        0x04,
-        0x05,
-        0x06,
-        0x07,
-        0x08,
-        0x09,
-        0x0A,
-        0x0B,
-        0x0C,
-        0x0D,
-        0x0E,
-        0x0F,
+        0x17,       /* ContentType opaque_type = application_data */
+        0x03, 0x03, /* ProtocolVersion legacy_record_version = 0x0303 */
+        0x00, 0x10, /* uint16 length */
+        0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+        0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, /* opaque encrypted_record[TLSCiphertext.length] */
     };
 
     struct s2n_cipher_suite *test_cipher_suite = &s2n_tls13_aes_256_gcm_sha384;
