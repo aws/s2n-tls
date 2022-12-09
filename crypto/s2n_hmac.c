f4cf5643dac6b6d6a338c707ec441681eb203165
harrisonkaiser <60789001+harrisonkaiser@users.noreply.github.com>
2022-12-09 14:00:11 -0500

    Update crypto/s2n_hmac.c
    Co-authored-by: maddeleine <59030281+maddeleine@users.noreply.github.com>

--- crypto/s2n_hmac.c
+++ crypto/s2n_hmac.c
@@ -12,7 +12,7 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
-/* this file is patched by Siderail, clang-format invalidates patches */
+/* this file is patched by Sidetrail, clang-format invalidates patches */
 /* clang-format off */
 
 #include <openssl/md5.h>
