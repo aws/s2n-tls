7e739b905423fceb605da563095570c4e2e5bfe2
Doug Chapman <dougch@amazon.com>
2022-12-13 16:54:40 -0800

    Add comment around s2nc_args

--- bindings/rust/integration/benches/s2nc.rs
+++ bindings/rust/integration/benches/s2nc.rs
@@ -6,6 +6,11 @@ use std::{env, process::Command, time::Duration};
 
 pub fn s2nc(c: &mut Criterion) {
     let mut group = c.benchmark_group("s2nc");
+    /*
+    example S2NC_ARGS:
+    "--non-blocking -T -f ../integration/trust-store/ca-bundle.crt -c test_all_tls13 www.netflix.com 443"
+    Set in tests/integrationv2/providers.py line 379-ish
+    */
     let s2nc_env: &str = &env::var("S2NC_ARGS").unwrap();
     let s2nc_split = s2nc_env.split(' ').collect::<Vec<&str>>();
     let test_name = format!("s2nc_{}_{}", s2nc_split[5], s2nc_split[6]);
