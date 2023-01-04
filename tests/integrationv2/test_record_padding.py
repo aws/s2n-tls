6ae6ce0cae089e6a47e51b8a8a2a2517cf559fb1
Frank Lee <frnlee@amazon.com>
2023-01-04 21:22:15 +0000

    py lint

--- tests/integrationv2/test_record_padding.py
+++ tests/integrationv2/test_record_padding.py
@@ -3,19 +3,19 @@ import pytest
 
 from configuration import available_ports, ALL_TEST_CURVES, MINIMAL_TEST_CERTS
 from common import Ciphers, ProviderOptions, Protocols, data_bytes
-from fixtures import managed_process # lgtm [py/unused-import]
+from fixtures import managed_process  # lgtm [py/unused-import]
 from providers import Provider, S2N, OpenSSL
 from utils import invalid_test_parameters, get_parameter_name, get_expected_s2n_version, to_bytes
 
 PADDING_SIZE_MIN = 0
-PADDING_SIZE_SMALL = 250 
+PADDING_SIZE_SMALL = 250
 PADDING_SIZE_MEDIUM = 1000
 PADDING_SIZE_MAX = 1 << 14
 
 PADDING_SIZES = [
-    PADDING_SIZE_MIN, 
-    PADDING_SIZE_SMALL, 
-    PADDING_SIZE_MEDIUM, 
+    PADDING_SIZE_MIN,
+    PADDING_SIZE_SMALL,
+    PADDING_SIZE_MEDIUM,
     PADDING_SIZE_MAX
 ]
 
@@ -24,13 +24,15 @@ PAYLOAD_SIZE_MEDIUM = 100
 # openssl has a max fragment length of 4096 bytes
 PAYLOAD_SIZE_LARGE = 4096
 
-OPENSSL_PAYLOAD_SIZES = [PAYLOAD_SIZE_SMALL, PAYLOAD_SIZE_MEDIUM, PAYLOAD_SIZE_LARGE]
+OPENSSL_PAYLOAD_SIZES = [PAYLOAD_SIZE_SMALL,
+                         PAYLOAD_SIZE_MEDIUM, PAYLOAD_SIZE_LARGE]
 
 # https://www.rfc-editor.org/rfc/rfc5116
 TLS13_CIPHERS_WITH_16_BYTE_TAGS = [
     Ciphers.AES128_GCM_SHA256,
     Ciphers.AES256_GCM_SHA384,
 ]
+
 
 @pytest.mark.uncollect_if(func=invalid_test_parameters)
 @pytest.mark.parametrize("cipher", TLS13_CIPHERS_WITH_16_BYTE_TAGS, ids=get_parameter_name)
@@ -40,7 +42,7 @@ TLS13_CIPHERS_WITH_16_BYTE_TAGS = [
 @pytest.mark.parametrize("certificate", MINIMAL_TEST_CERTS, ids=get_parameter_name)
 @pytest.mark.parametrize("padded_block_size", PADDING_SIZES, ids=get_parameter_name)
 @pytest.mark.parametrize("payload_size", OPENSSL_PAYLOAD_SIZES, ids=get_parameter_name)
-def test_s2n_13_server_handles_padded_records(managed_process, cipher, provider, curve, protocol, certificate, 
+def test_s2n_13_server_handles_padded_records(managed_process, cipher, provider, curve, protocol, certificate,
                                               padded_block_size, payload_size):
     port = next(available_ports)
 
@@ -69,7 +71,7 @@ def test_s2n_13_server_handles_padded_records(managed_process, cipher, provider,
 
     s2nd = managed_process(S2N, server_options, timeout=5)
     openssl = managed_process(provider, client_options, timeout=5)
-    
+
     expected_total_length = None
     if padded_block_size == 0:
         # if we aren't padding to some known block size, then the expected total length is
@@ -77,7 +79,7 @@ def test_s2n_13_server_handles_padded_records(managed_process, cipher, provider,
         expected_total_length = payload_size + 16 + 1
     else:
         expected_total_length = padded_block_size + 16
-    
+
     # In the TLS record header, the last two bytes are reserved for length
     hex_string = "{:04x}".format(expected_total_length)
     first_byte, second_byte = hex_string[:2], hex_string[2:]
