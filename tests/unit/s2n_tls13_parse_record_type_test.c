5d43a012cc1325992d306dbea103cc2c88198e3c
Frank Lee <frnlee@amazon.com>
2023-01-04 21:02:33 +0000

    more duvet fixes

--- tests/unit/s2n_tls13_parse_record_type_test.c
+++ tests/unit/s2n_tls13_parse_record_type_test.c
@@ -98,8 +98,8 @@ int main(int argc, char **argv)
         /** test: padding without record type should fail
          *= https://tools.ietf.org/rfc/rfc8446#section-5.4
          *= type=test
-         *# if a receiving implementation does not
-         *# find a non-zero octet in the cleartext, it must terminate the
+         *# If a receiving implementation does not
+         *# find a non-zero octet in the cleartext, it MUST terminate the
          *# connection with an "unexpected_message" alert.
          **/
         S2N_BLOB_FROM_HEX(no_type, "00");
