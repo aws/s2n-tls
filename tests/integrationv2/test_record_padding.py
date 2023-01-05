2dc4ecc2c07b831626aec1d8c4c147e552db2967
Frank Lee <frnlee@amazon.com>
2023-01-05 20:57:19 +0000

    add padding comment

--- tests/integrationv2/test_record_padding.py
+++ tests/integrationv2/test_record_padding.py
@@ -1,7 +1,7 @@
 import copy
 import pytest
 
-from configuration import available_ports, ALL_TEST_CURVES, MINIMAL_TEST_CERTS
+from configuration import available_ports, ALL_TEST_CIPHERS, ALL_TEST_CURVES, MINIMAL_TEST_CERTS
 from common import Ciphers, ProviderOptions, Protocols, data_bytes
 from fixtures import managed_process  # lgtm [py/unused-import]
 from providers import Provider, S2N, OpenSSL
@@ -34,10 +34,12 @@ TLS13_CIPHERS_WITH_16_BYTE_TAGS = [
 ]
 
 
+@pytest.mark.skip
 @pytest.mark.uncollect_if(func=invalid_test_parameters)
 @pytest.mark.parametrize("cipher", TLS13_CIPHERS_WITH_16_BYTE_TAGS, ids=get_parameter_name)
 @pytest.mark.parametrize("provider", [OpenSSL])
 @pytest.mark.parametrize("curve", ALL_TEST_CURVES, ids=get_parameter_name)
+# only tls 1.3 supports record padding
 @pytest.mark.parametrize("protocol", [Protocols.TLS13], ids=get_parameter_name)
 @pytest.mark.parametrize("certificate", MINIMAL_TEST_CERTS, ids=get_parameter_name)
 @pytest.mark.parametrize("padded_block_size", PADDING_SIZES, ids=get_parameter_name)
@@ -45,10 +47,6 @@ TLS13_CIPHERS_WITH_16_BYTE_TAGS = [
 def test_s2n_13_server_handles_padded_records(managed_process, cipher, provider, curve, protocol, certificate,
                                               padded_block_size, payload_size):
     port = next(available_ports)
-
-    extra_flags = None
-    if padded_block_size > 0:
-        extra_flags = ['-record_padding', padded_block_size]
 
     random_bytes = data_bytes(payload_size)
     client_options = ProviderOptions(
@@ -60,7 +58,7 @@ def test_s2n_13_server_handles_padded_records(managed_process, cipher, provider,
         data_to_send=random_bytes,
         insecure=True,
         protocol=protocol,
-        extra_flags=extra_flags,
+        extra_flags=['-record_padding', padded_block_size]
     )
 
     server_options = copy.copy(client_options)
@@ -102,3 +100,51 @@ def test_s2n_13_server_handles_padded_records(managed_process, cipher, provider,
         # verify that the cipher was correctly negotiated
         assert to_bytes("Cipher negotiated: {}".format(
             cipher.name)) in server_results.stdout
+
+
+@pytest.mark.uncollect_if(func=invalid_test_parameters)
+@pytest.mark.parametrize("cipher", ALL_TEST_CIPHERS, ids=get_parameter_name)
+@pytest.mark.parametrize("provider", [OpenSSL])
+@pytest.mark.parametrize("curve", ALL_TEST_CURVES, ids=get_parameter_name)
+# only tls 1.3 supports record padding
+@pytest.mark.parametrize("protocol", [Protocols.TLS13], ids=get_parameter_name)
+@pytest.mark.parametrize("certificate", MINIMAL_TEST_CERTS, ids=get_parameter_name)
+@pytest.mark.parametrize("padded_block_size", PADDING_SIZES, ids=get_parameter_name)
+def test_s2n_13_client_handles_padded_records(managed_process, cipher, provider, curve, protocol, certificate,
+                                              padded_block_size):
+    port = next(available_ports)
+
+    random_bytes = data_bytes(4095)
+    server_options = ProviderOptions(
+        mode=Provider.ServerMode,
+        port=port,
+        cipher=cipher,
+        curve=curve,
+        cert=certificate.cert,
+        key=certificate.key,
+        insecure=True,
+        protocol=protocol,
+        # openssl errors when sending session tickets and when padding is set > 2075 bytes.
+        extra_flags=['-num_tickets', 0, '-record_padding', padded_block_size]
+    )
+
+    client_options = copy.copy(server_options)
+    client_options.mode = Provider.ClientMode
+    client_options.extra_flags = None
+    client_options.data_to_send = random_bytes
+
+    s2nc = managed_process(S2N, client_options, timeout=5)
+    openssl = managed_process(provider, server_options, timeout=5)
+
+    expected_version = get_expected_s2n_version(protocol, provider)
+
+    for client_results in s2nc.get_results():
+        client_results.assert_success()
+        assert to_bytes("Actual protocol version: {}".format(
+            expected_version)) in client_results.stdout
+        assert to_bytes("Cipher negotiated: {}".format(
+            cipher.name)) in client_results.stdout
+
+    for server_results in openssl.get_results():
+        server_results.assert_success()
+        assert random_bytes in server_results.stdout
