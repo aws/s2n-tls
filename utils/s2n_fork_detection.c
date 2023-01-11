625f9a249ed8bb2459cec340230dd7116852f2c4
Joel Knight <knight.joel@gmail.com>
2023-01-11 12:47:46 -0700

    Repair build on OpenBSD (#3670)

--- utils/s2n_fork_detection.c
+++ utils/s2n_fork_detection.c
@@ -20,7 +20,7 @@
 typedef struct _opaque_pthread_once_t __darwin_pthread_once_t;
 typedef __darwin_pthread_once_t pthread_once_t;
     #define _DARWIN_C_SOURCE
-#elif defined(__FreeBSD__)
+#elif defined(__FreeBSD__) || defined(__OpenBSD__)
     /* FreeBSD requires POSIX compatibility off for its syscalls (enables __BSD_VISIBLE)
      * Without the below line, <sys/mman.h> cannot be imported (it requires __BSD_VISIBLE) */
     #undef _POSIX_C_SOURCE
