0b9ee75ae56b718e0a29df8d704d16e0afd26f0c
Harrison Kaiser <uwaces@gmail.com>
2023-01-12 12:18:43 -0500

    Build test

--- tests/unit/s2n_build_test.c
+++ tests/unit/s2n_build_test.c
@@ -1,19 +1,25 @@
 /*
- * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
- *
- *  http://aws.amazon.com/apache2.0
- *
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
+* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License").
+* You may not use this file except in compliance with the License.
+* A copy of the License is located at
+*
+*  http://aws.amazon.com/apache2.0
+*
+* or in the "license" file accompanying this file. This file is distributed
+* on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+* express or implied. See the License for the specific language governing
+* permissions and limitations under the License.
+*/
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
+
+#include <openssl/opensslv.h>
+#include <openssl/crypto.h>
+
+#include <string.h>
 
 #include "s2n_test.h"
 
@@ -22,69 +28,87 @@ extern char **environ;
 
 int main(int argc, char **argv)
 {
-    BEGIN_TEST();
+   (void) argc, (void) argv;
+   BEGIN_TEST();
 
-    (void) argc, (void) argv;
-    printf("s2n_build_test:\nPrinting Environment Variables:\n");
-    for (char **env = environ; *env != 0; env++)
-    {
-        // printf("%s\n", *env);
-        (void) env;
-    }
+   /*
+    * In the case that libcrypto is dynamicly linked there is a chance that the
+    * build version and the linked version don't match. OPENSSL_VERSION_TEXT is
+    * a macro defined in openssl/crypto.h and should be returned from OpenSSL_version.
+    * In the event that we are dynamicly linked to the wrong libcrypto the function,
+    * call will return a diffrent version text than the one we used at a source level.
+    *
+    * If libcrypto is staticly linked, this is sure to be true.
+    */
+   EXPECT_EQUAL(0, strcmp(OPENSSL_VERSION_TEXT, OpenSSL_version(OPENSSL_VERSION)));
 
-    const char *s2n_build_preset = getenv("S2N_BUILD_PRESET");
-    EXPECT_NOT_NULL(s2n_build_preset);
-    const char *s2n_libcrypto = getenv("S2N_LIBCRYPTO");
-    EXPECT_NOT_NULL(s2n_build_preset);
+   /*
+    * The build configurations in CI are defined by S2N_BUILD_PRESET.
+    *
+    */
+   const char *s2n_build_preset = getenv("S2N_BUILD_PRESET");
+   if (s2n_build_preset == NULL) {
+       /* Apparently we aren't in CI. */
+       END_TEST();
+   }
 
-    #define MATCHES(x) strcmp(s2n_build_preset, x) == 0
-    #define CHK_LC(x) EXPECT_EQUAL(strcmp(s2n_libcrypto, x), 0)
-    /* Verify that the environment  */
-    if (MATCHES("awslc_gcc4-8")
-            || MATCHES("awslc_gcc9")) {
-        CHK_LC("awslc");
-    } else if (MATCHES("awslc-fips_gcc4-8")
-            || MATCHES("awslc-fips_gcc9")) {
-        CHK_LC("awslc-fips");
-    } else if (MATCHES("libressl_gcc6")
-            || MATCHES("libressl_gcc9")) {
-        CHK_LC("libressl");
-    } else if (MATCHES("boringssl")) {
-        CHK_LC("boringssl");
-    } else if (MATCHES("openssl-1-0-2")) {
-        CHK_LC("openssl-1.0.2");
-    } else if (MATCHES("openssl-1.0.2-fips")) {
-        CHK_LC("openssl-1.0.2-fips");
-    } else if (MATCHES("openssl-1.1.1_gcc4-8")
-            || MATCHES("openssl-1.1.1_gcc6")
-            || MATCHES("openssl-1.1.1_gcc6_softcrypto")
-            || MATCHES("openssl-1.1.1_gcc9")) {
-        CHK_LC("openssl-1.1.1");
-    } else if (MATCHES("openssl-3.0")) {
-        CHK_LC("openssl-3.0");
-    }
+   /* CMake generated Makefiles can select a default LIBCRYPTO,
+    * lets make sure that didn't happen. */
+   const char *s2n_libcrypto = getenv("S2N_LIBCRYPTO");
+   EXPECT_NOT_NULL(s2n_build_preset);
 
-    FILE *fp;
-    char buffer[1035];
+#define CONTAINS(x) strcasestr(s2n_build_preset, x) != NULL
+#define CHK_LC(x) EXPECT_EQUAL(strcmp(s2n_libcrypto, x), 0)
+   /* Verify that the environment  */
+   if (CONTAINS("awslc")) {
+       if (CONTAINS("fips")) {
+           CHK_LC("awslc-fips");
+       } else {
+           CHK_LC("awslc");
+       }
+   } else if (CONTAINS("libressl")) {
+       CHK_LC("libressl");
+   } else if (CONTAINS("boringssl")) {
+       CHK_LC("boringssl");
+   } else if (CONTAINS("openssl")) {
+       if (CONTAINS("fips")) {
+           if (CONTAINS("1-0-2") || CONTAINS("1.0.2")) {
+               CHK_LC("openssl-1.0.2-fips");
+           } else if (CONTAINS("1.1.1") || CONTAINS("1-1-1")) {
+               CHK_LC("openssl-1.1.1-fips");
+           } else if (CONTAINS("3-0") || CONTAINS("3.0")) {
+               CHK_LC("openssl-3.0-fips");
+           }
+       } else {
+           if (CONTAINS("1-0-2") || CONTAINS("1.0.2")) {
+               CHK_LC("openssl-1.0.2");
+           } else if (CONTAINS("1.1.1") || CONTAINS("1-1-1")) {
+               CHK_LC("openssl-1.1.1");
+           } else if (CONTAINS("3-0") || CONTAINS("3.0")) {
+               CHK_LC("openssl-3.0");
+           }
+       }
+   } else {
+       printf("\nTest didn't handle this combination of envariables\n");
+       printf("S2N_BUILD_PRESET == %s\n", s2n_build_preset);
+       printf("S2N_LIBCRYPTO    == %s\n", s2n_libcrypto);
+       EXPECT_TRUE(0);
+   }
 
-    /* Open the command for reading. */
-    /* TODO get the location of a binary (s2nd would work for instance) */
-    fp = popen("which s2nc", "r");
-    if (fp == NULL) {
-        printf("Failed to run command\n" );
-        exit(1);
-    }
+   /* Now that we can rely on the value of S2N_LIBCRYPTO lets check that it matches the version
+    * that libcrypto reports. */
+   const char *openssl_version = OpenSSL_version(OPENSSL_VERSION);
+   char s2n_libcrypto_copy[31] = { 0 };
+   strncpy(s2n_libcrypto_copy, s2n_libcrypto, 30);
+   char *token = strtok(s2n_libcrypto_copy, "-");
+   /* The name of the library should be included (AWS, BoringSSL, LibreSSL, OpenSSL, ect) */
+   EXPECT_NOT_NULL(strcasestr(openssl_version, token));
+   /* The version number, if present should also be there. */
+   strtok(NULL, "-");
+   if (token != NULL) {
+       EXPECT_NOT_NULL(strcasestr(openssl_version, token));
+   }
 
-    /* Read the output a line at a time - output it. */
-    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
-        printf("%s", buffer);
-    }
-
-    /* close */
-    pclose(fp);
-
-    return 0;
-
-    END_TEST();
-    return 0;
+   END_TEST();
+   return 0;
 }
