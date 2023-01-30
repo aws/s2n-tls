51be594b7597c4ae327cb32c3ce0e5c236c87f4c
Harrison Kaiser <uwaces@gmail.com>
2023-01-30 11:10:48 -0500

    Respond to PR comments

--- tests/unit/s2n_build_test.c
+++ tests/unit/s2n_build_test.c
@@ -30,23 +30,25 @@ struct libcrypto_info {
     bool is_fips;
 };
 
-void init_libcrypto_info(struct libcrypto_info *li)
+void init_libcrypto_info(struct libcrypto_info *libcrypto_info)
 {
-    if (li != NULL) {
-        li->name = NULL;
-        li->major_version = -1;
-        li->minor_version = -1;
-        li->patch_version = -1;
-        li->is_fips = false;
+    if (libcrypto_info != NULL) {
+        libcrypto_info->name = NULL;
+        libcrypto_info->major_version = -1;
+        libcrypto_info->minor_version = -1;
+        libcrypto_info->patch_version = -1;
+        libcrypto_info->is_fips = false;
     }
 }
 
 int number_from_version(char *version)
 {
-    if (version == NULL)
+    if (version == NULL) {
         return -1;
-    if (version[0] == '0')
+    }
+    if (version[0] == '0') {
         return 0;
+    }
     int i = atoi(version);
     /* atoi returns 0 on error */
     if (i == 0) {
@@ -56,47 +58,46 @@ int number_from_version(char *version)
     }
 }
 
-int convert_version_number_to_string(struct libcrypto_info *li, char *version_string_out)
+int convert_version_number_to_string(struct libcrypto_info *libcrypto_info, char *version_string_out)
 {
     /* Format major minor and patch version in a normalized version string (e.x. "1.1.1", "3.0", "3", "")
      * If there is no major version, return -1 and leave version_string_out untouched. */
-    if (li->major_version != -1) {
-        sprintf(version_string_out, "%d", li->major_version);
+    if (libcrypto_info->major_version != -1) {
+        sprintf(version_string_out, "%d", libcrypto_info->major_version);
         version_string_out = &version_string_out[1];
     } else {
         return -1;
     }
-    if (li->minor_version != -1) {
-        sprintf(version_string_out, ".%d", li->minor_version);
+    if (libcrypto_info->minor_version != -1) {
+        sprintf(version_string_out, ".%d", libcrypto_info->minor_version);
         version_string_out = &version_string_out[2];
     }
-    if (li->patch_version != -1) {
-        sprintf(version_string_out, ".%d", li->patch_version);
+    if (libcrypto_info->patch_version != -1) {
+        sprintf(version_string_out, ".%d", libcrypto_info->patch_version);
     }
     return 0;
 }
 
-int extract_libcrypto_info_from_s2n_libcrypto(char *s2n_libcrypto_copy, struct libcrypto_info *li)
+int extract_libcrypto_info_from_s2n_libcrypto(char *s2n_libcrypto_name, struct libcrypto_info *libcrypto_info)
 {
-    if (li == NULL)
+    if (libcrypto_info == NULL || s2n_libcrypto_name == NULL || strlen(s2n_libcrypto_name) == 0) {
         return -1;
-    if (strlen(s2n_libcrypto_copy) == 0)
-        return -1;
-    li->name = strtok(s2n_libcrypto_copy, "-");
+    }
+    libcrypto_info->name = strtok(s2n_libcrypto_name, "-");
     char *rest = strtok(NULL, "");
 
     if (rest != NULL && isdigit(rest[0])) {
-        char *saved = NULL;
-        li->major_version = number_from_version(strtok(rest, ".-"));
-        if (li->major_version != -1) {
+        char *saved = strtok(rest, ".-");
+        libcrypto_info->major_version = number_from_version(saved);
+        if (libcrypto_info->major_version != -1) {
             saved = strtok(NULL, ".-");
-            li->minor_version = number_from_version(saved);
+            libcrypto_info->minor_version = number_from_version(saved);
         }
-        if (li->minor_version != -1) {
+        if (libcrypto_info->minor_version != -1) {
             saved = strtok(NULL, ".-");
-            li->patch_version = number_from_version(saved);
+            libcrypto_info->patch_version = number_from_version(saved);
         }
-        if (li->patch_version != -1) {
+        if (libcrypto_info->patch_version != -1) {
             rest = strtok(NULL, "");
         } else {
             rest = saved;
@@ -104,51 +105,44 @@ int extract_libcrypto_info_from_s2n_libcrypto(char *s2n_libcrypto_copy, struct l
     }
 
     if (rest != NULL) {
-        li->is_fips = (strstr(rest, "fips") != NULL);
+        libcrypto_info->is_fips = (strstr(rest, "fips") != NULL);
     }
     return 0;
 }
 
-int extract_libcrypto_info_from_s2n_build_preset(char *s2n_build_preset_copy, struct libcrypto_info *li)
+int extract_libcrypto_info_from_s2n_build_preset(char *s2n_build_preset_copy, struct libcrypto_info *libcrypto_info)
 {
-    if (li == NULL)
+    if (libcrypto_info == NULL || strlen(s2n_build_preset_copy) == 0) {
         return -1;
-    if (strlen(s2n_build_preset_copy) == 0)
-        return -1;
-    li->name = strtok(s2n_build_preset_copy, "-_");
+    }
+    libcrypto_info->name = strtok(s2n_build_preset_copy, "-_");
     char *rest = strtok(NULL, "");
     if (rest != NULL && isdigit(rest[0])) {
-        li->major_version = number_from_version(strtok(rest, ".-"));
-        if (li->major_version != -1) {
-            li->minor_version = number_from_version(strtok(NULL, ".-"));
+        libcrypto_info->major_version = number_from_version(strtok(rest, ".-"));
+        if (libcrypto_info->major_version != -1) {
+            libcrypto_info->minor_version = number_from_version(strtok(NULL, ".-"));
         }
-        if (li->minor_version != -1) {
-            li->patch_version = number_from_version(strtok(NULL, "-_"));
+        if (libcrypto_info->minor_version != -1) {
+            libcrypto_info->patch_version = number_from_version(strtok(NULL, "-_"));
         }
         rest = strtok(NULL, "");
     }
 
     if (rest != NULL) {
-        li->is_fips = (strstr(rest, "fips") != NULL);
+        libcrypto_info->is_fips = (strstr(rest, "fips") != NULL);
     }
     return 0;
 }
 
 int main()
 {
-    /* There are three different specifications of libcrypto which must be in sync.
-     *  1 The #include headers (-I) (checked with macros, e.g. OPENSSL_VERSION_TEXT and OPENSSL_IS_AWSLC)
-     *  2 The (dynamically) linked library (-L) (checked by calling function e.g. SSLeay_version)
-     *  3 The intent of the CI (declared by environment variables S2N_BUILD_PRESET and S2N_LIBCYPTO)
-     * BEGIN_TEST calls s2n_init which checks that (1) and (2) match. */
-    BEGIN_TEST();
-    /* The rest of the test checks that (3) matches (1) or (2). */
+    /* Ensure the libcrypto we build with, link against and intend to use are all the same.  */
+    BEGIN_TEST(); /* s2n_init, called by BEGIN_TEST, checks that the build and link libcrypto are identical.  */
 
-    /* If both S2N_BUILD_PRESET and S2N_LIBCRYPTO are not defined, we aren't in a CI, end the test. */
+    /* Without these variables there is no particular intended version of libcrypto, and thus nothing to check.  */
     const char *s2n_build_preset = getenv("S2N_BUILD_PRESET");
     const char *s2n_libcrypto = getenv("S2N_LIBCRYPTO");
     if (s2n_build_preset == NULL && s2n_libcrypto == NULL) {
-        /* No intent declared. */
         END_TEST();
         return 0;
     }
@@ -201,17 +195,16 @@ int main()
     /* Check libcrypto name matches the intent of the CI.  */
     {
         if (strcmp(s2n_libcrypto_intent->name, "awslc") == 0) {
-            /* aws lc version strings have a storied history:
-             *     Originally a fork from BoringSSL, AWSLC's OPENSSL_VERSION_TEXT was defined to be "OpenSSL 1.1.1 (compatible; BoringSSL)".
-             *     AWC-LC commit 8f184f5d69604cc4645bafec47c2d6d9929cb50f on 4/11/22 modified it to be  "OpenSSL 1.1.1 (compatible; AWL-LC)"
-             * Unfortunately in both cases we can't do a straight forward comparison against "S2N_LIBCRYPTO=awslc".
-             * We can however rely on the macro OPENSSL_IS_AWSLC.  */
+/* Special case awslc since it is a fork of BoringSSL and therefore the version returned by SSLeay_version
+ * is inaccurate in early releases of awslc.
+ */
 #ifndef OPENSSL_IS_AWSLC
             FAIL_MSG("CI intended AWS-LC, but LIBCRYPTO isn't AWS-LC. ");
 #endif
         } else {
             /* Any other library should have the name of the library (modulo case) in its version string.  */
-            EXPECT_NOT_NULL(strcasestr(SSLeay_version(SSLEAY_VERSION), s2n_libcrypto_intent->name));
+            const char *ssleay_version_text = SSLeay_version(SSLEAY_VERSION);
+            EXPECT_NOT_NULL(strcasestr(ssleay_version_text, s2n_libcrypto_intent->name));
         }
     };
 
@@ -219,7 +212,8 @@ int main()
     {
         char intent_version[20] = { 0 };
         if (convert_version_number_to_string(s2n_libcrypto_intent, intent_version) != -1) {
-            EXPECT_NOT_NULL(strstr(SSLeay_version(SSLEAY_VERSION), intent_version));
+            const char *ssleay_version_text = SSLeay_version(SSLEAY_VERSION);
+            EXPECT_NOT_NULL(strstr(ssleay_version_text, intent_version));
         }
     };
 
