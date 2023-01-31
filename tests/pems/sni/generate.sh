a58b308637e736f7024ad45aed8c63a51a267e6c
Cameron Bytheway <bythewc@amazon.com>
2023-01-30 20:05:23 -0700

    fix: pass an empty string to host verify without usable identifiers (#3793)

---
+++ tests/pems/sni/generate.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#  http://aws.amazon.com/apache2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+
+set -eu
+
+cd $(dirname $0)
+
+../gen_self_signed_cert.sh --cn '' --prefix 'without_cn'
+../gen_self_signed_cert.sh --ip '::1' --prefix 'ip_v6_lo'
+
