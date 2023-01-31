7e3c5f2e7d43f9213c15eaa8e400ba9fc35b8a0e
aditishri18 <115036984+aditishri18@users.noreply.github.com>
2023-01-31 11:25:20 -0800

    Blob Initialization fix-Test_1 (#3790)

--- tests/unit/s2n_stuffer_base64_test.c
+++ tests/unit/s2n_stuffer_base64_test.c
@@ -26,7 +26,8 @@ int main(int argc, char **argv)
     uint8_t hello_world_base64[] = "SGVsbG8gd29ybGQhAA==";
     struct s2n_stuffer stuffer = { 0 }, known_data = { 0 }, scratch = { 0 }, entropy = { 0 }, mirror = { 0 };
     uint8_t pad[50];
-    struct s2n_blob r = { .data = pad, .size = sizeof(pad) };
+    struct s2n_blob r = { 0 };
+    EXPECT_SUCCESS(s2n_blob_init(&r, pad, sizeof(pad)));
 
     BEGIN_TEST();
     EXPECT_SUCCESS(s2n_disable_tls13_in_test());
