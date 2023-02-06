ea27de7e3cb901128561294b30200b20435c7049
Harrison Kaiser <uwaces@gmail.com>
2023-02-06 12:21:09 -0500

    Switch to ctest

--- .github/s2n_osx.sh
+++ .github/s2n_osx.sh
@@ -27,4 +27,5 @@ cmake . -Bbuild -GNinja \
 -DCMAKE_PREFIX_PATH=${OPENSSL_1_1_1_INSTALL_DIR} ..
 
 cmake --build ./build -j $(nproc)
-time CTEST_PARALLEL_LEVEL=$(nproc) cmake --build ./build --target test ARGS="-L unit --output-on-failure"
+cd ./build
+time ctest -L unit --output-on-failure -j$(nproc)
