a811153f73d3955d5c5b9a0f82ef934291338164
Aditi Shrivastava <shriadi@amazon.com>
2023-02-15 14:09:09 -0800

    Indentation fix.

--- tls/s2n_cbc.c
+++ tls/s2n_cbc.c
@@ -85,7 +85,7 @@ int s2n_verify_cbc(struct s2n_connection *conn, struct s2n_hmac_state *hmac, str
     /* Check the maximum amount that could theoretically be padding */
     uint32_t check = MIN(255, (payload_and_padding_size - 1));
 
-    /*Check to ensure check >= padding_length*/
+    /* Check to ensure check >= padding_length */
     POSIX_ENSURE_GTE(check, padding_length);
 
     uint32_t cutoff = check - padding_length;
