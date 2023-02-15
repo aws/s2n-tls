2b1c6231b9f3c9e49afa6af4e9a4259903867f1c
Cameron Bytheway <bytheway.cameron@gmail.com>
2023-02-15 14:10:16 -0700

    pr feedback

--- .github/s2n_bsd.sh
+++ .github/s2n_bsd.sh
@@ -15,6 +15,8 @@
 set -eu
 export CTEST_OUTPUT_ON_FAILURE=1
 
+# FreeBSD and OpenBSD have different ways of getting CPU count
+# see https://serverfault.com/questions/203207/how-do-i-count-the-number-of-processors-on-an-openbsd-system
 if [ "$(uname)" = 'FreeBSD' ]; then
     export CTEST_PARALLEL_LEVEL=$(sysctl hw.ncpu | awk '{print $2}')
 else
@@ -28,18 +30,18 @@ onerror() {
 
 mkdir -p output
 
-cmake . -Brelease -GNinja -DCMAKE_BUILD_TYPE=Release || onerror
-cmake --build ./release -j $CTEST_PARALLEL_LEVEL || onerror
+cmake . -Brelease -GNinja -DCMAKE_BUILD_TYPE=Release
+cmake --build ./release -j $CTEST_PARALLEL_LEVEL
 ninja -C release test || onerror
-mv release/Testing/Temporary output/release || onerror
+mv release/Testing/Temporary output/release
 # reduce the number of files to copy back
-rm -rf release || onerror
+rm -rf release
 
-cmake . -Bbuild -GNinja -DCMAKE_BUILD_TYPE=Debug || onerror
-cmake --build ./build -j $CTEST_PARALLEL_LEVEL || onerror
+cmake . -Bbuild -GNinja -DCMAKE_BUILD_TYPE=Debug
+cmake --build ./build -j $CTEST_PARALLEL_LEVEL
 ninja -C build test || onerror
-mv build/Testing/Temporary output/debug || onerror
+mv build/Testing/Temporary output/debug
 # reduce the number of files to copy back
-rm -rf build || onerror
+rm -rf build
 
 exit $errors
