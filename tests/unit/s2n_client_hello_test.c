83a39ee8aab66dced35697902ba1f8d246cdae14
Wesley Rosenblum <wesleyr@amazon.com>
2023-02-22 21:02:25 +0000

    Use DEFER_CLEANUP

--- tests/unit/s2n_client_hello_test.c
+++ tests/unit/s2n_client_hello_test.c
@@ -286,8 +286,9 @@ int main(int argc, char **argv)
 
             /* Generate a session id if the negotiated protocol is less than TLS1.3 */
             {
-                struct s2n_connection *conn;
-                EXPECT_NOT_NULL(conn = s2n_connection_new(S2N_CLIENT));
+                DEFER_CLEANUP(struct s2n_connection *conn = s2n_connection_new(S2N_CLIENT),
+                    s2n_connection_ptr_free) ;
+                EXPECT_NOT_NULL(conn);
                 struct s2n_stuffer *hello_stuffer = &conn->handshake.io;
                 conn->actual_protocol_version = S2N_TLS12;
                 EXPECT_TRUE(conn->client_protocol_version >= S2N_TLS13);
@@ -298,8 +299,6 @@ int main(int argc, char **argv)
                 uint8_t session_id_length = 0;
                 EXPECT_SUCCESS(s2n_stuffer_read_uint8(hello_stuffer, &session_id_length));
                 EXPECT_EQUAL(session_id_length, S2N_TLS_SESSION_ID_MAX_LEN);
-
-                EXPECT_SUCCESS(s2n_connection_free(conn));
             }
 
             EXPECT_SUCCESS(s2n_disable_tls13_in_test());
