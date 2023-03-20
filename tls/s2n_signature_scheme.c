f4ea6542a756411393cfdbf382d892ee0cb8f561
Lindsay Stewart <slindsay@amazon.com>
2023-03-20 15:36:32 -0700

    Put sha512 back

--- tls/s2n_signature_scheme.c
+++ tls/s2n_signature_scheme.c
@@ -291,20 +291,25 @@ const struct s2n_signature_scheme* const s2n_sig_scheme_pref_list_20230317[] = {
     /* RSA */
     &s2n_rsa_pss_rsae_sha256,
     &s2n_rsa_pss_rsae_sha384,
+    &s2n_rsa_pss_rsae_sha512,
     &s2n_rsa_pkcs1_sha256,
     &s2n_rsa_pkcs1_sha384,
+    &s2n_rsa_pkcs1_sha512,
 
     /* TLS1.2 with ECDSA */
     &s2n_ecdsa_sha256, /* same iana value as TLS 1.3 s2n_ecdsa_secp256r1_sha256 */
     &s2n_ecdsa_sha384, /* same iana value as TLS 1.3 s2n_ecdsa_secp384r1_sha384 */
+    &s2n_ecdsa_sha512,
 
     /* TLS1.3 with ECDSA */
     &s2n_ecdsa_secp256r1_sha256,
     &s2n_ecdsa_secp384r1_sha384,
+    &s2n_ecdsa_secp521r1_sha512,
 
     /* TLS1.3 with RSA-PSS */
     &s2n_rsa_pss_pss_sha256,
     &s2n_rsa_pss_pss_sha384,
+    &s2n_rsa_pss_pss_sha512,
 };
 
 const struct s2n_signature_preferences s2n_signature_preferences_20230317 = {
