579d485adff0e9422852fad3bb551a9489d12b03
James Mayclin <maycj@amazon.com>
2023-05-17 00:35:29 +0000

    add pointer free function

--- crypto/s2n_openssl_x509.h
+++ crypto/s2n_openssl_x509.h
@@ -16,10 +16,13 @@
 #pragma once
 
 #include <openssl/x509.h>
+#include <openssl/asn1.h>
 #include <stdint.h>
 
 #include "utils/s2n_safety.h"
 
 DEFINE_POINTER_CLEANUP_FUNC(X509 *, X509_free);
 
-int s2n_openssl_x509_stack_pop_free(STACK_OF(X509) **cert_chain);
+S2N_CLEANUP_RESULT s2n_openssl_x509_stack_pop_free(STACK_OF(X509) **cert_chain);
+
+S2N_CLEANUP_RESULT s2n_openssl_asn1_time_free_pointer(ASN1_GENERALIZEDTIME **time);
