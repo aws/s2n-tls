1c8c0ea9136e926136de66295d708081442f99b6
DougCh <dougch@amazon.com>
2023-05-19 15:10:27 -0700

    PR feedback

--- flake.nix
+++ flake.nix
@@ -101,6 +101,8 @@
             # Re-include cmake to update the environment with a new libcrypto.
             buildInputs = [ pkgs.cmake openssl_1_1_1 ];
             S2N_LIBCRYPTO = "openssl-1.1.1";
+            # Integ s_client/server tests expect openssl 1.1.1.
+            # GnuTLS-cli and serv utilities needed for some integration tests.
             shellHook = ''
               echo Setting up $S2N_LIBCRYPTO enviornment from flake.nix...
               export PATH=${openssl_1_1_1}/bin:${gnutls-3-7}/bin:$PATH
@@ -114,6 +116,8 @@
             # Re-include cmake to update the environment with a new libcrypto.
             buildInputs = [ pkgs.cmake libressl ];
             S2N_LIBCRYPTO = "libressl";
+            # Integ s_client/server tests expect openssl 1.1.1.
+            # GnuTLS-cli and serv utilities needed for some integration tests.
             shellHook = ''
               echo Setting up $S2N_LIBCRYPTO enviornment from flake.nix...
               export PATH=${openssl_1_1_1}/bin:${gnutls-3-7}/bin:$PATH
