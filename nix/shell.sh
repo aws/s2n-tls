a7ccb7e5676249b512aead923d2323bfef0b56e7
Doug Chapman <54039637+dougch@users.noreply.github.com>
2023-06-06 16:26:42 -0700

    nix devShell with aws-lc (#4028)

--- nix/shell.sh
+++ nix/shell.sh
@@ -113,7 +113,7 @@ function test_toolchain_counts {
     # This is a starting point for a unit test of the devShell.
     # The choosen S2N_LIBCRYPTO should be 2, and the others should be zero.
     banner "Checking the CMAKE_INCLUDE_PATH for libcrypto counts"
-    echo $CMAKE_INCLUDE_PATH|gawk 'BEGIN{RS=":"; o10=0; o11=0; o3=0;awslc=0}
+    echo $CMAKE_INCLUDE_PATH|gawk 'BEGIN{RS=":"; o10=0; o11=0; o3=0;awslc=0;libre=0}
       /openssl-3.0/{o3++}
       /openssl-1.1/{o11++}
       /openssl-1.0/{o10++}
