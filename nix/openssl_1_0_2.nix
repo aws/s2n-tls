bb8774d7dd53c6fc320658d5cbf50a4c7905f8b7
DougCh <dougch@amazon.com>
2023-06-07 20:52:34 +0000

    Drop the -d build flag for openssl102  on nix aarch64-linux

--- nix/openssl_1_0_2.nix
+++ nix/openssl_1_0_2.nix
@@ -11,9 +11,17 @@ pkgs.stdenv.mkDerivation rec {
 
   buildInputs = [ pkgs.gnumake pkgs.perl534 ];
 
-  configurePhase = ''
-    ./config -d shared -g3 -fPIC no-libunbound no-gmp no-jpake no-krb5 no-md2 no-rc5 no-rfc3779 no-sctp no-ssl-trace no-store no-zlib no-hw no-mdc2 no-seed no-idea enable-ec_nistp_64_gcc_128 no-camellia no-bf no-ripemd no-dsa no-ssl2 no-capieng -DSSL_FORBID_ENULL -DOPENSSL_NO_DTLS1 -DOPENSSL_NO_HEARTBEATS --prefix=$out
-  '';
+  configurePhase = let
+    default_options =
+      "shared -g3 -fPIC no-libunbound no-gmp no-jpake no-krb5 no-md2 no-rc5 no-rfc3779 no-sctp no-ssl-trace no-store no-zlib no-hw no-mdc2 no-seed no-idea enable-ec_nistp_64_gcc_128 no-camellia no-bf no-ripemd no-dsa no-ssl2 no-capieng -DSSL_FORBID_ENULL -DOPENSSL_NO_DTLS1 -DOPENSSL_NO_HEARTBEATS --prefix=$out";
+  in {
+    x86_64-linux = ''
+      ./config -d ${default_options}
+    '';
+    aarch64-linux = ''
+      ./config ${default_options}
+    '';
+  }.${pkgs.stdenv.hostPlatform.system};
 
   buildPhase = ''
     make depend -j $(nproc)
