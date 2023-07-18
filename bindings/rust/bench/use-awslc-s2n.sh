e845c71373a6813cf8cc84aca5c6f25af355842f
tinzh <jmzhang@amazon.com>
2023-07-18 16:27:42 +0000

    moved s2n-tls and aws-lc builds to target

--- bindings/rust/bench/use-awslc-s2n.sh
+++ bindings/rust/bench/use-awslc-s2n.sh
@@ -11,8 +11,8 @@ set -e
 pushd "$(dirname "$0")" > /dev/null
 bench_dir="$(pwd)"
 
-aws_lc_build_dir="$bench_dir"/target/aws-lc-build
-aws_lc_install_dir="$bench_dir"/target/aws-lc-install
+s2n_tls_build_dir="$bench_dir"/target/s2n-tls-build
+aws_lc_dir="$bench_dir"/target/aws-lc
 
 # go to repo directory
 pushd "$(dirname "$0")/../../../" > /dev/null
@@ -20,44 +20,45 @@ repo_dir="$(pwd)"
 popd > /dev/null
 
 # if libs2n not found, build it
-if [ ! -e "$repo_dir"/build/lib/libs2n.a ]
+if [ ! -e "$s2n_tls_build_dir"/lib/libs2n.a ]
 then
     # if aws-lc not found, build it
-    if [ ! -e "$aws_lc_install_dir"/lib/libcrypto.a ]
+    if [ ! -e "$aws_lc_dir"/install/lib/libcrypto.a ]
     then
-        # clean up directories
-        rm -rf "$aws_lc_build_dir" "$aws_lc_install_dir"
-        mkdir -p "$aws_lc_build_dir" "$aws_lc_install_dir"
-
-        # clone aws-lc
-        cd "$aws_lc_build_dir"
+        # clone fresh aws-lc
+        cd "$bench_dir"/target
+        rm -rf aws-lc
         git clone --depth=1 https://github.com/aws/aws-lc
         cd aws-lc
 
         # build aws-lc to libcrypto-root
-        cmake -B build -DCMAKE_INSTALL_PREFIX="$aws_lc_install_dir" -DBUILD_TESTING=OFF -DBUILD_LIBSSL=OFF
+        cmake -B build -DCMAKE_INSTALL_PREFIX="$aws_lc_dir"/install -DBUILD_TESTING=OFF -DBUILD_LIBSSL=OFF
         cmake --build ./build -j $(nproc)
         make -C build install
     else
         echo "using libcrypto.a at libcrypto-root/lib/"
     fi
 
+    # clean up directories
+    rm -rf "$s2n_tls_build_dir"
+    mkdir -p "$s2n_tls_build_dir"
+
     # build s2n-tls to s2n-tls/build
-    cd $repo_dir
-    cmake . -Bbuild -DCMAKE_PREFIX_PATH="$aws_lc_install_dir" -DS2N_INTERN_LIBCRYPTO=ON -DBUILD_TESTING=OFF
-    cmake --build ./build -j $(nproc)
+    cd "$repo_dir"
+    cmake . -B "$s2n_tls_build_dir" -DCMAKE_PREFIX_PATH="$aws_lc_dir"/install -DS2N_INTERN_LIBCRYPTO=ON -DBUILD_TESTING=OFF
+    cmake --build "$s2n_tls_build_dir" -j $(nproc)
 else
     echo "using libs2n.a at build/lib/"
 fi
 
 # tell s2n-tls-sys crate where s2n-tls was built with .cargo/config.toml
-cd bindings/rust/bench
+cd "$repo_dir"/bindings/rust/bench
 mkdir -p .cargo
 # if .cargo/config.toml doesn't already have an [env] header, add it
 if [[ ! -f .cargo/config.toml || "$(cat .cargo/config.toml)" != *"[env]"* ]]; then
 echo "[env]
-S2N_TLS_LIB_DIR = \"$repo_dir/build/lib\"
-LD_LIBRARY_PATH = \"$repo_dir/build/lib\"" >> .cargo/config.toml
+S2N_TLS_LIB_DIR = \"$s2n_tls_build_dir/lib\"
+LD_LIBRARY_PATH = \"$s2n_tls_build_dir/lib\"" >> .cargo/config.toml
 fi
 
 # force rebuild of s2n-tls-sys and benches
