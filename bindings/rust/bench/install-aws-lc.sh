5fb385eb467b2ee69da04cf44889d0fcd8909447
tinzh <jmzhang@amazon.com>
2023-07-20 00:09:40 +0000

    changed how aws-lc works

--- bindings/rust/bench/install-aws-lc.sh
+++ bindings/rust/bench/install-aws-lc.sh
@@ -3,22 +3,46 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-# sets bench crate to use aws-lc with s2n-tls
-# all artifacts are in target/aws-lc and target/s2n-tls-build
+# sets up bench crate to use aws-lc for either s2n-tls or rustls if desired
+# to run with aws-lc, use and cargo command with:
+# --config aws-lc-config/s2n.toml
+# --config aws-lc-config/rustls.toml
+# or both
 
-set -e
+set -e 
 
 # go to bench directory
 pushd "$(dirname "$0")" > /dev/null
 bench_dir="$(pwd)"
 
+
+
+# ----- rustls -----
+
+# clone rustls to bench/target/rustls and checkout compatible version
+rm -rf target/rustls
+git clone https://github.com/rustls/rustls target/rustls
+cd target/rustls
+git checkout 'v/0.21.5'
+
+# go to dir with rustls crate
+cd rustls
+rustls_dir="$(pwd)"
+
+# change rustls to use aws-lc-rs
+sed -i 's|ring = .*|ring = { package = "aws-lc-rs" }|' Cargo.toml
+
+
+
+# ----- s2n-tls -----
+
+# put all build artifacts in target
 s2n_tls_build_dir="$bench_dir"/target/s2n-tls-build
 aws_lc_dir="$bench_dir"/target/aws-lc
 
 # go to repo directory
-pushd "$(dirname "$0")/../../../" > /dev/null
+cd "$bench_dir"/../../../ > /dev/null
 repo_dir="$(pwd)"
-popd > /dev/null
 
 # if libs2n not found, build it
 if [ ! -e "$s2n_tls_build_dir"/lib/libs2n.a ]
@@ -52,17 +76,9 @@ else
     echo "using libs2n.a at target/s2n-tls-build/lib"
 fi
 
-# tell s2n-tls-sys crate where s2n-tls was built with .cargo/config.toml
-cd "$repo_dir"/bindings/rust/bench
-mkdir -p .cargo
-# if .cargo/config.toml doesn't already have an [env] header, add it
-if [[ ! -f .cargo/config.toml || "$(cat .cargo/config.toml)" != *"[env]"* ]]; then
-echo "[env]
-S2N_TLS_LIB_DIR = \"$s2n_tls_build_dir/lib\"
-LD_LIBRARY_PATH = \"$s2n_tls_build_dir/lib\"" >> .cargo/config.toml
-fi
-
 # force rebuild of s2n-tls-sys and benches
 rm -rf ../target/release target/release
+
+
 
 popd > /dev/null
