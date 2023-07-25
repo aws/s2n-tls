2999f2f1ae8dc445535d90b75feb6089e8218291
James Mayclin <maycj@amazon.com>
2023-07-25 11:31:45 -0700

    Update tests/unit/s2n_resume_test.c
    Co-authored-by: Lindsay Stewart <stewart.r.lindsay@gmail.com>

--- tests/unit/s2n_resume_test.c
+++ tests/unit/s2n_resume_test.c
@@ -593,7 +593,7 @@ int main(int argc, char **argv)
             EXPECT_SUCCESS(s2n_connection_free(conn));
         };
 
-        /* erroneous secret size*/
+        /* Erroneous secret size */
         {
             struct {
                 uint32_t secret_size;
