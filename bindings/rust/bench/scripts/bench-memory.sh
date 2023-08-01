9465e1a9bb0f094fa7318625d10eadac1490f522
Justin Zhang <76919968+tinzh@users.noreply.github.com>
2023-08-01 12:05:55 -0700

    refactor(bench): move around and update scripts in bench crate (#4115)

--- bindings/rust/bench/scripts/bench-memory.sh
+++ bindings/rust/bench/scripts/bench-memory.sh
@@ -5,14 +5,15 @@
 
 # Use Valgrind and Massif to heap profile the memory taken by different TLS libraries
 # Uses Valgrind monitor commands to take snapshots of heap size while making connections
+# All given arguments (ex. `--config aws-lc-config/s2n.toml` to use AWS-LC) are passed to Cargo
 
-# Snapshots get stored in target memory/[library-name]/ as [number].snapshot
+# Snapshots get stored in target/memory/[library-name]/ as [number].snapshot
 
 set -e
 
 pushd "$(dirname "$0")"/.. > /dev/null
 
-cargo build --release --bin memory "$@"
+cargo build --release --bin memory --bin graph_memory "$@"
 
 valgrind --tool=massif --depth=1 --massif-out-file="target/memory/massif.out" --time-unit=ms target/release/memory 
 rm target/memory/massif.out
