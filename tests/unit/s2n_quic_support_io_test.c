982940db54fb90af4aaf204963a208f33d3b06e5
Appelmans <madelea@amazon.com>
2023-10-04 16:22:33 -0700

    Fixed bug

--- tests/unit/s2n_quic_support_io_test.c
+++ tests/unit/s2n_quic_support_io_test.c
@@ -462,7 +462,6 @@ int main(int argc, char **argv)
             EXPECT_SUCCESS(s2n_stuffer_write_uint24(&stuffer, TEST_DATA_SIZE));
             EXPECT_SUCCESS(s2n_stuffer_write_bytes(&stuffer, TEST_DATA, TEST_DATA_SIZE));
 
-            s2n_blocked_status blocked = 0;
             EXPECT_FAILURE_WITH_ERRNO(s2n_recv_quic_post_handshake_message(conn, &blocked), S2N_ERR_UNSUPPORTED_WITH_QUIC);
         };
 
@@ -484,7 +483,6 @@ int main(int argc, char **argv)
             /* Construct ST handshake message */
             EXPECT_SUCCESS(s2n_stuffer_write_bytes(&stuffer, ticket_message, sizeof(ticket_message)));
 
-            s2n_blocked_status blocked = 0;
             EXPECT_SUCCESS(s2n_recv_quic_post_handshake_message(conn, &blocked));
 
             /* Callback was triggered */
@@ -508,7 +506,6 @@ int main(int argc, char **argv)
             /* Mock receiving a fragmented handshake message */
             EXPECT_SUCCESS(s2n_stuffer_write_bytes(&stuffer, ticket_message, i));
 
-            s2n_blocked_status blocked = 0;
             EXPECT_FAILURE_WITH_ERRNO(s2n_recv_quic_post_handshake_message(conn, &blocked), S2N_ERR_IO_BLOCKED);
             EXPECT_EQUAL(blocked, S2N_BLOCKED_ON_READ);
 
