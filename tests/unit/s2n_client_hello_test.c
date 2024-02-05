fb67a0f0a2ed5bd240e5715f01e0844b04fa939c
Appelmans <madelea@amazon.com>
2024-02-05 11:44:51 -0800

    Clang format

--- tests/unit/s2n_client_hello_test.c
+++ tests/unit/s2n_client_hello_test.c
@@ -1705,7 +1705,7 @@ int main(int argc, char **argv)
             uint8_t data[] = { 1, 2, 3, 4, 5 };
             EXPECT_SUCCESS(s2n_blob_init(&client_hello.compression_methods, data, sizeof(data)));
             EXPECT_FAILURE_WITH_ERRNO(s2n_client_hello_get_compression_methods(&client_hello, &list, list_length, &out_length),
-                S2N_ERR_INSUFFICIENT_MEM_SIZE);
+                    S2N_ERR_INSUFFICIENT_MEM_SIZE);
         };
 
         /* Retrieves the compression methods list */
