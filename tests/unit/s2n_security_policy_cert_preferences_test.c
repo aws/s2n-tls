7b6803c9827fc9a8814399c7df82b8a9611145a6
James Mayclin <maycj@amazon.com>
2024-02-21 17:58:30 +0000

    address pr feedback
    - add more explicit comment for valid chain scope

--- tests/unit/s2n_security_policy_cert_preferences_test.c
+++ tests/unit/s2n_security_policy_cert_preferences_test.c
@@ -181,7 +181,7 @@ int main(int argc, char **argv)
         struct s2n_cert_chain cert_chain = { 0 };
         struct s2n_cert_chain_and_key chain = { 0 };
 
-        /* valid chain */
+        /* a valid certificate chain passes validation */
         {
             EXPECT_OK(s2n_test_construct_cert_chain(certs, &cert_chain, &chain, &valid_info));
             EXPECT_OK(s2n_security_policy_validate_certificate_chain(&test_sp, &chain));
