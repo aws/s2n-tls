8f94725aac62e6f4d6c06a2ff75e11d41be2b6fa
James Mayclin <maycj@amazon.com>
2024-02-21 20:15:41 +0000

    address pr feedback
    - remove unnecessary uint16_t max check

--- crypto/s2n_openssl_x509.c
+++ crypto/s2n_openssl_x509.c
@@ -129,7 +129,6 @@ S2N_RESULT s2n_openssl_x509_get_cert_info(X509 *cert, struct s2n_cert_info *info
     }
 
     RESULT_ENSURE(info->public_key_nid > 0, S2N_ERR_CERT_TYPE_UNSUPPORTED);
-    RESULT_ENSURE(info->public_key_nid < UINT16_MAX, S2N_ERR_CERT_TYPE_UNSUPPORTED);
 
     return S2N_RESULT_OK;
 }
