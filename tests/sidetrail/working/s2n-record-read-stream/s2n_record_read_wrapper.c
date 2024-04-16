0c84d074d17802dfbc726ac47679145faea7cb26
Sam Clark <3758302+goatgoose@users.noreply.github.com>
2024-04-16 16:29:36 -0400

    fix(sidetrail): Invalid stream cipher struct in proof wrapper (#4484)

--- tests/sidetrail/working/s2n-record-read-stream/s2n_record_read_wrapper.c
+++ tests/sidetrail/working/s2n-record-read-stream/s2n_record_read_wrapper.c
@@ -71,7 +71,7 @@ int s2n_record_parse_wrapper(int *xor_pad,
 			     uint8_t content_type
 )
 {
-  __VERIFIER_ASSERT_MAX_LEAKAGE(0);
+  __VERIFIER_ASSERT_MAX_LEAKAGE(5);
   __VERIFIER_assume(encrypted_length > 0);
   __VERIFIER_assume(padding_length >= 0);
   __VERIFIER_assume(padding_length < 256);
@@ -99,7 +99,7 @@ int s2n_record_parse_wrapper(int *xor_pad,
   
   struct s2n_cipher stream_cipher = {
     .type = S2N_STREAM,
-    .io.cbc.decrypt = decrypt_stream,
+    .io.stream.decrypt = decrypt_stream,
   };
   
   struct s2n_record_algorithm record_algorithm = {
