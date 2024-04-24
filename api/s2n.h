c552c51a5d4becf744045fedd3a51d231f3831a4
Sam Clark <3758302+goatgoose@users.noreply.github.com>
2024-04-24 10:44:37 -0400

    openbsd testing

--- api/s2n.h
+++ api/s2n.h
@@ -22,17 +22,12 @@
 
 #pragma once
 
-#if ((__GNUC__ >= 4) || defined(__clang__)) && defined(S2N_EXPORTS)
-    /**
-     * Marks a function as belonging to the public s2n API.
-     */
-    #define S2N_API __attribute__((visibility("default")))
-#else
+#ifndef S2N_API
     /**
      * Marks a function as belonging to the public s2n API.
      */
     #define S2N_API
-#endif /* __GNUC__ >= 4 || defined(__clang__) */
+#endif
 
 #ifdef __cplusplus
 extern "C" {
