8d234dc5167a8be90a8e879ee9fffbd8113921a8
Doug Chapman <dougch@amazon.com>
2024-05-02 19:33:56 +0000

    fix nix fmt issue

--- nix/pyenv.nix
+++ nix/pyenv.nix
@@ -3,14 +3,13 @@ let
   pythonPkgs = pkgs.python310Packages;
   cryptography = pythonPkgs.cryptography;
   pyOpenSSL = pythonPkgs.pyopenssl;
-in pkgs.python310.withPackages (ps:
-  [
-    ps.pep8
-    ps.pytest
-    ps.pytest-xdist
-    ps.pytest-rerunfailures
-    ps.typing-extensions
-    ps.setuptools-rust
-    ps.cryptography
-  ])
+in pkgs.python310.withPackages (ps: [
+  ps.pep8
+  ps.pytest
+  ps.pytest-xdist
+  ps.pytest-rerunfailures
+  ps.typing-extensions
+  ps.setuptools-rust
+  ps.cryptography
+])
 
