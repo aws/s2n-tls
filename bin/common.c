9294e019fd2644cf673956f61338bbbf6721559e
Appelmans <madelea@amazon.com>
2024-05-02 12:50:01 -0700

    Fixing cppcheck

--- bin/common.c
+++ bin/common.c
@@ -379,7 +379,7 @@ int s2n_setup_server_connection(struct s2n_connection *conn, int fd, struct s2n_
     if (settings.deserialize_in) {
         size_t deserialize_length = 0;
         GUARD_RETURN(get_file_size(settings.deserialize_in, &deserialize_length), "Failed to read serialize-in file size");
-        GUARD_RETURN(deserialize_length <= UINT32_MAX, "serialize-in file size is too large");
+        ENSURE_EXIT(deserialize_length <= UINT32_MAX, "serialize-in file size is too large");
         uint8_t *mem = malloc(deserialize_length);
         GUARD_EXIT_NULL(mem);
         GUARD_RETURN(load_file_to_array(settings.deserialize_in, mem, deserialize_length), "Failed to read serialize-in file");
