c5402b31ee4523debcf6e16a08240a3f9ebb4dbc
James Mayclin <maycj@amazon.com>
2024-05-08 18:02:31 +0000

    address ci failure
    - add copyright annotations to all files

--- tests/interop/common/src/lib.rs
+++ tests/interop/common/src/lib.rs
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 use std::{env, fmt::Display, str::FromStr};
 
 /// `Common` provides a crate with functionality that other TLS implementors
