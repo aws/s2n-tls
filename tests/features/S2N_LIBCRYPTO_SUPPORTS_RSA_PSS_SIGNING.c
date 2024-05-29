0805a637cbe8f98c35b60ceb8cca08e58ece0bc7
Jou Ho <jouh@amazon.com>
2024-05-29 23:43:08 +0000

    add version check back for RSA_PSS_CERTS_SUPPORTED macro

--- tests/features/S2N_LIBCRYPTO_SUPPORTS_RSA_PSS_SIGNING.c
+++ tests/features/S2N_LIBCRYPTO_SUPPORTS_RSA_PSS_SIGNING.c
@@ -15,14 +15,6 @@
 
 #include <openssl/rsa.h>
 
-/* OpenSSL 1.1.1d 10 Sep 2019 is broken, so disable on that version. For further info see: crypto/evp/p_lib.c:469
- *
- * This feature requires this Openssl commit for Openssl 1.1.x versions: openssl/openssl@4088b92
- */
-#if defined(OPENSSL_VERSION_NUMBER) && OPENSSL_VERSION_NUMBER <= 0x1010104fL
-    #error "Version of OpenSSL does not support RSA-PSS"
-#endif
-
 #if defined(LIBRESSL_VERSION_NUMBER)
     #error "LibSSL does not support RSA-PSS"
 #endif
