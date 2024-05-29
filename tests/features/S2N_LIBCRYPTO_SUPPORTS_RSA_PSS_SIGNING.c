207350fff49f1ed7e704d159ac2d889704db99b1
Jou Ho <jouh@amazon.com>
2024-05-29 21:10:12 +0000

    add more conditional compilation

--- tests/features/S2N_LIBCRYPTO_SUPPORTS_RSA_PSS_SIGNING.c
+++ tests/features/S2N_LIBCRYPTO_SUPPORTS_RSA_PSS_SIGNING.c
@@ -19,10 +19,16 @@
  *
  * This feature requires this Openssl commit for Openssl 1.1.x versions: openssl/openssl@4088b92
  */
-#if defined(OPENSSL_VERSION_NUMBER)
-#if OPENSSL_VERSION_NUMBER <= 0x1010104fL
-#error "Version of OpenSSL does not support RSA-PSS"
+#if defined(OPENSSL_VERSION_NUMBER) && OPENSSL_VERSION_NUMBER <= 0x1010104fL
+    #error "Version of OpenSSL does not support RSA-PSS"
 #endif
+
+#if defined(LIBRESSL_VERSION_NUMBER)
+#error "LibSSL does not support RSA-PSS"
+#endif
+
+#if defined(OPENSSL_IS_BORINGSSL)
+#error "BoringSSL does not support RSA-PSS"
 #endif
 
 int main()
