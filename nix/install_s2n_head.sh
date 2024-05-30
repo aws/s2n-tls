d899f1eddf9f60e72fdd401e04c98dd613d04403
Doug Chapman <54039637+dougch@users.noreply.github.com>
2024-05-30 11:47:26 -0700

    Remove verbose output

--- nix/install_s2n_head.sh
+++ nix/install_s2n_head.sh
@@ -12,7 +12,7 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-set -eux
+set -eu
 
 usage() {
     echo "install_s2n_head.sh build_dir"
