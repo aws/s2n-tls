f5ee8f8f932bf20022f697e29c149744e375607e
Jou Ho <jouh@amazon.com>
2024-07-16 17:57:32 +0000

    cleanup

--- tests/unit/s2n_connection_protocol_versions_test.c
+++ tests/unit/s2n_connection_protocol_versions_test.c
@@ -251,7 +251,8 @@ int main(int argc, char **argv)
         EXPECT_SUCCESS(s2n_connections_set_io_pair(client, server, &io_pair));
 
         if (s2n_is_in_fips_mode() && client->client_protocol_version == S2N_SSLv3 && !s2n_libcrypto_is_awslc()) {
-            EXPECT_ERROR_WITH_ERRNO(s2n_negotiate_test_server_and_client_until_message(server, client, SERVER_CERT), S2N_ERR_SSLV3_HANDSHAKE_WITH_OSSL_FIPS_NOT_SUPPORTED);
+            EXPECT_ERROR_WITH_ERRNO(s2n_negotiate_test_server_and_client_until_message(server, client, SERVER_CERT),
+                    S2N_ERR_SSLV3_HANDSHAKE_WITH_OSSL_FIPS_NOT_SUPPORTED);
         }
         EXPECT_OK(s2n_negotiate_test_server_and_client_until_message(server, client, SERVER_CERT));
 
