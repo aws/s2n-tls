5d7796dd55ea8641186c21e1f466b529facb0e2a
Appelmans <madelea@amazon.com>
2024-07-26 17:47:29 -0700

    fmt

--- bindings/rust/s2n-tls/src/config.rs
+++ bindings/rust/s2n-tls/src/config.rs
@@ -755,8 +755,7 @@ impl Builder {
     /// Requires that session tickets are only used when forward secrecy is possible
     pub fn require_ticket_forward_secrecy(&mut self, enable: bool) -> Result<&mut Self, Error> {
         unsafe {
-            s2n_config_require_ticket_forward_secrecy(self.as_mut_ptr(), enable)
-                .into_result()
+            s2n_config_require_ticket_forward_secrecy(self.as_mut_ptr(), enable).into_result()
         }?;
         Ok(self)
     }
