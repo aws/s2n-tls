7fc3c4d4e1e0ffd4b644a72a637cf9293f8196a3
Jou Ho <jouh@amazon.com>
2024-08-02 19:12:47 +0000

    fix syntax

--- tests/fuzz/runFuzzTest.sh
+++ tests/fuzz/runFuzzTest.sh
@@ -86,10 +86,11 @@ if [ "$CORPUS_UPLOAD_LOC" != "none" ]; then
         unset LD_LIBRARY_PATH
         if aws s3 ls "${CORPUS_UPLOAD_LOC}/${TEST_NAME}/corpus.zip" > /dev/null 2>&1; then
             echo "corpus.zip found, downloading from S3 bucket and unzipping..."
-            # Download corpus.zip
             aws s3 cp "${CORPUS_UPLOAD_LOC}/${TEST_NAME}/corpus.zip" "${TEMP_CORPUS_DIR}/corpus.zip"
-            # Unzip the file
             unzip -o "${TEMP_CORPUS_DIR}/corpus.zip" -d "${TEMP_CORPUS_DIR}"
+        else 
+            cp -r ./corpus/${TEST_NAME}/. "${TEMP_CORPUS_DIR}"
+        fi
     )
 else
     cp -r ./corpus/${TEST_NAME}/. "${TEMP_CORPUS_DIR}"
