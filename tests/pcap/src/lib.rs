18ae630aa58dda40b0e500d4794d077b6cec5589
Lindsay Stewart <slindsay@amazon.com>
2024-08-22 15:33:24 -0700

    tests(pcaps): download additional pcaps

--- tests/pcap/src/lib.rs
+++ tests/pcap/src/lib.rs
@@ -5,13 +5,25 @@ pub mod client_hello;
 pub mod handshake_message;
 pub mod packet;
 
-/// Creates an iterator over all test pcap file paths
-pub fn all_pcaps() -> impl Iterator<Item = String> {
-    std::fs::read_dir("data")
-        .expect("Missing test pcap file")
+use std::path::Path;
+
+fn pcaps_from<P: AsRef<Path>>(path: P) -> impl Iterator<Item = String> {
+    std::fs::read_dir(path.as_ref())
+        .unwrap_or_else(|_| panic!("Unable to read pcap folder: {}", path.as_ref().display()))
         .filter_map(Result::ok)
         .filter_map(|entry| {
             let path = entry.path();
             path.to_str().map(std::string::ToString::to_string)
         })
+}
+
+/// Creates an iterator over all test pcap file paths
+pub fn all_pcaps() -> impl Iterator<Item = String> {
+    let local_pcaps = pcaps_from("data");
+
+    let downloaded_pcaps_path =
+        std::env::var("DOWNLOADED_PCAPS_PATH").expect("DOWNLOADED_PCAPS_PATH not set by build.rs");
+    let downloaded_pcaps = pcaps_from(downloaded_pcaps_path);
+
+    local_pcaps.chain(downloaded_pcaps)
 }
