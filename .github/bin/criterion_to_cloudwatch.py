b16a9fe954a878342618a16d9174441a03180411
Sam Clark <3758302+goatgoose@users.noreply.github.com>
2024-09-09 15:33:02 -0400

    pr feedback

--- .github/bin/criterion_to_cloudwatch.py
+++ .github/bin/criterion_to_cloudwatch.py
@@ -14,15 +14,17 @@
 
 import argparse
 import json
+from typing import Tuple
+from collections.abc import Generator
 import boto3
 
 
 class CriterionResult:
-    def __init__(self, json_obj):
-        self._json_obj = json_obj
+    def __init__(self, json_obj: dict):
+        self._json_obj: dict = json_obj
 
     @property
-    def _group_and_category(self):
+    def _group_and_category(self) -> Tuple[str, str]:
         id_ = self._json_obj["id"]
 
         # The id is made up of the bench group and the category, separated by a /.
@@ -36,17 +38,17 @@ class CriterionResult:
         return group, category
 
     @property
-    def group(self):
+    def group(self) -> str:
         group, category = self._group_and_category
         return group
 
     @property
-    def category(self):
+    def category(self) -> str:
         group, category = self._group_and_category
         return category
 
     @property
-    def mean_us(self):
+    def mean_us(self) -> float:
         mean = self._json_obj["mean"]
 
         unit = mean["unit"]
@@ -60,10 +62,10 @@ class CriterionResult:
 
 
 class CriterionReader:
-    def __init__(self, criterion_output_path):
-        self.criterion_output_path = criterion_output_path
+    def __init__(self, criterion_output_path: str):
+        self.criterion_output_path: str = criterion_output_path
 
-    def read_bench_results(self):
+    def read_bench_results(self) -> Generator[CriterionResult, None, None]:
         with open(self.criterion_output_path, 'r') as output:
             for line in output.readlines():
                 obj = json.loads(line)
@@ -89,12 +91,12 @@ if __name__ == '__main__':
     parser.add_argument(
         "--namespace",
         required=True,
-        help="CloudWatch namespace to emit metrics to.",
+        help="CloudWatch namespace to emit metrics to (e.g. s2n-tls-bench).",
     )
     parser.add_argument(
         "--platform",
         required=True,
-        help="Specifies the platform dimension of the CloudWatch metrics.",
+        help="Specifies the platform dimension of the CloudWatch metrics (e.g. Ubuntu24-x86).",
     )
     args = parser.parse_args()
 
