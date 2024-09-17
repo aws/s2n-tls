a1ff94719027a5b231a309a3ebf5916e8f4fe84f
Lindsay Stewart <slindsay@amazon.com>
2024-09-16 22:36:37 -0700

    add a lot more comments

--- tests/testlib/s2n_seccomp.c
+++ tests/testlib/s2n_seccomp.c
@@ -29,16 +29,25 @@ bool s2n_is_seccomp_supported()
     return true;
 }
 
+/* "seccomp" allows the kernel to control what system calls an application
+ * is allowed to make based on a provided filter.
+ *
+ * seccomp is commonly used for "sandboxing" programs for security reasons.
+ */
 S2N_RESULT s2n_seccomp_init()
 {
-    /* Using SCMP_ACT_TRAP instead of SCMP_ACT_KILL makes this test easier
-     * to debug. GDB will report exactly which syscalls triggered the signal.
+    /* Using SCMP_ACT_TRAP instead of SCMP_ACT_KILL as the default action
+     * makes this test easier to debug. GDB can be used to debug failures caused
+     * by SCMP_ACT_TRAP, but not caused by SCMP_ACT_KILL.
      */
     DEFER_CLEANUP(scmp_filter_ctx ctx = seccomp_init(SCMP_ACT_TRAP),
             seccomp_release_pointer);
     RESULT_ENSURE_REF(ctx);
 
-    /* Basic requirements */
+    /* Basic requirements: s2n-tls is known to need these system calls in order
+     * to operate. Adding a new system call to this list means that any application
+     * using s2n-tls with seccomp will potentially also need to update its filter rules.
+     */
     RESULT_GUARD_POSIX(seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(access), 0));
     RESULT_GUARD_POSIX(seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(brk), 0));
     RESULT_GUARD_POSIX(seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(clock_gettime), 0));
@@ -50,14 +59,19 @@ S2N_RESULT s2n_seccomp_init()
     RESULT_GUARD_POSIX(seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(read), 0));
     RESULT_GUARD_POSIX(seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(write), 0));
 
-    /* Requirements for Ubuntu22 */
+    /* Ubuntu22 uses "newfstatat" instead of "fstat" */
     RESULT_GUARD_POSIX(seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(newfstatat), 0));
 
-    /* See https://github.com/aws/aws-lc/blob/main/SANDBOXING.md#fork-protection */
+    /* See https://github.com/aws/aws-lc/blob/main/SANDBOXING.md#fork-protection:
+     * We can just cause the madavise call to fail rather than blocking it entirely. */
     RESULT_GUARD_POSIX(seccomp_rule_add(ctx, SCMP_ACT_ERRNO(EINVAL), SCMP_SYS(madvise), 0));
 
-    RESULT_GUARD_POSIX(seccomp_load(ctx));
+    /* Checking whether the terminal supports color requires an additional
+     * system call. Preemptively disable color.
+     */
     s2n_use_color_in_output = false;
+
+    RESULT_GUARD_POSIX(seccomp_load(ctx));
     return S2N_RESULT_OK;
 }
 
--- tests/unit/s2n_seccomp_handshake_test.c
+++ tests/unit/s2n_seccomp_handshake_test.c
@@ -19,10 +19,13 @@
 
 int main(int argc, char **argv)
 {
-    BEGIN_TEST();
+    BEGIN_TEST_NO_INIT();
 
-    /* We don't use s2n_test_cert_chain_and_key_new because we want to explicitly
-     * only do the PEM file read before enabling seccomp.
+    /* One of the primary purposes of seccomp is to block opening new files.
+     * So before we enable seccomp, we need to open any files that the test would
+     * need. In this case, we need to load certificate pems from files.
+     *
+     * An application using s2n-tls with seccomp would need to do the same.
      */
     char cert_chain_pem[S2N_MAX_TEST_PEM_SIZE] = { 0 };
     char private_key_pem[S2N_MAX_TEST_PEM_SIZE] = { 0 };
@@ -30,6 +33,15 @@ int main(int argc, char **argv)
             cert_chain_pem, S2N_MAX_TEST_PEM_SIZE));
     EXPECT_SUCCESS(s2n_read_test_pem(S2N_DEFAULT_ECDSA_TEST_PRIVATE_KEY,
             private_key_pem, S2N_MAX_TEST_PEM_SIZE));
+
+    /* We need to execute s2n_init before the seccomp filter is applied.
+     * Some one-time initialization involves opening files, like "dev/urandom".
+     * If built with aws-lc, s2n-tls also needs to call CRYPTO_pre_sandbox_init()
+     * before seccomp starts sandboxing.
+     *
+     * An application using s2n-tls with seccomp would need to do the same.
+     */
+    EXPECT_SUCCESS(s2n_init());
 
     /* No unexpected syscalls allowed beyond this point */
     EXPECT_OK(s2n_seccomp_init());
@@ -42,7 +54,7 @@ int main(int argc, char **argv)
     EXPECT_SUCCESS(s2n_config_add_cert_chain_and_key_to_store(config, chain_and_key));
     EXPECT_SUCCESS(s2n_config_add_pem_to_trust_store(config, cert_chain_pem));
 
-    const char *security_policies[] = { "default", "default_tls13" };
+    const char *security_policies[] = { "test_all_tls12", "default_tls13" };
 
     for (size_t i = 0; i < s2n_array_len(security_policies); i++) {
         DEFER_CLEANUP(struct s2n_connection *client = s2n_connection_new(S2N_CLIENT),
