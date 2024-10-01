a29068a46ca12a3cb75f559bc2130ac8fa5daa7d
toidiu <apoorv@toidiu.com>
2024-10-01 11:38:46 -0700

    Revert "test: disallow explict use of "default" policy in tests (#4750)" (#4812)

--- tests/unit/s2n_renegotiate_io_test.c
+++ tests/unit/s2n_renegotiate_io_test.c
@@ -61,7 +61,7 @@ int main(int argc, char *argv[])
     EXPECT_NOT_NULL(config);
     EXPECT_SUCCESS(s2n_config_set_unsafe_for_testing(config));
     EXPECT_SUCCESS(s2n_config_add_cert_chain_and_key_to_store(config, chain_and_key));
-    EXPECT_SUCCESS(s2n_config_set_cipher_preferences(config, "20240501"));
+    EXPECT_SUCCESS(s2n_config_set_cipher_preferences(config, "default"));
 
     uint8_t app_data[] = "test application data";
 
