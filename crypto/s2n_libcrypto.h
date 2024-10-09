ce0234e4116d05936c0847a1d3874f3851e44b3d
Alex Weibel <aweibel@amazon.com>
2024-10-08 18:49:25 -0700

    Add ML-KEM Feature Probe and Test (#4823)

--- crypto/s2n_libcrypto.h
+++ crypto/s2n_libcrypto.h
@@ -17,6 +17,7 @@
 
 #include "utils/s2n_result.h"
 
+uint64_t s2n_libcrypto_awslc_api_version(void);
 S2N_RESULT s2n_libcrypto_validate_runtime(void);
 const char *s2n_libcrypto_get_version_name(void);
 bool s2n_libcrypto_supports_flag_no_check_time();
