c525bfc20d4ff15eb65c475375039299cf7b6e39
James Mayclin <maycj@amazon.com>
2024-10-16 20:24:01 +0000

    don't load system certs

--- bindings/rust/bench/src/s2n_tls.rs
+++ bindings/rust/bench/src/s2n_tls.rs
@@ -80,7 +80,7 @@ impl crate::harness::TlsBenchConfig for S2NConfig {
         let mut builder = Builder::new();
         builder
             .set_security_policy(&Policy::from_version(security_policy)?)?
-            .wipe_trust_store()?
+            .with_system_certs(false)?
             .set_client_auth_type(match handshake_type {
                 HandshakeType::MutualAuth => ClientAuthType::Required,
                 _ => ClientAuthType::None, // ServerAuth or resumption handshake
