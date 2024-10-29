63d617a05c305593eea1a9cea8d991365fa94486
James Mayclin <maycj@amazon.com>
2024-10-29 17:40:37 +0000

    address pr feedback
    * add copyright header

--- bindings/rust/bench/src/harness/io.rs
+++ bindings/rust/bench/src/harness/io.rs
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 use std::{cell::RefCell, collections::VecDeque, io::ErrorKind, pin::Pin, rc::Rc};
 
 pub type LocalDataBuffer = RefCell<VecDeque<u8>>;
