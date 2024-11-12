21919aef74f9c48b8d5884999a1da40204a0a7be
James Mayclin <maycj@amazon.com>
2024-11-12 19:41:57 +0000

    address ci failures
    * pin test-log for msrv issues
    * pep8 python code

--- tests/integrationv2/test_well_known_endpoints.py
+++ tests/integrationv2/test_well_known_endpoints.py
@@ -1,13 +1,13 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-'''
-This is a stub test, which allows the existing CI to continue passing while 
-https://github.com/aws/s2n-tls/pull/4884 is merged in.
-
-Once the PR is merged, the Codebuild spec for NixIntegV2Batch will be updated
-to remove the "well_known_endpoints" argument (manual process) and then this test
-can be fully removed (PR).
-'''
 def test_well_known_endpoints():
+    '''
+    This is a stub test, which allows the existing CI to continue passing while 
+    https://github.com/aws/s2n-tls/pull/4884 is merged in.
+
+    Once the PR is merged, the Codebuild spec for NixIntegV2Batch will be updated
+    to remove the "well_known_endpoints" argument (manual process) and then this test
+    can be fully removed (PR).
+    '''
     assert 1 == 1
