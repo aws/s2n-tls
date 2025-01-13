74ed7647e4d356ba3560c8afeaa9d80b75627570
James Mayclin <maycj@amazon.com>
2025-01-13 21:22:22 +0000

    address ci failures
    * pep8 code
    * remove .python-version

--- tests/integrationv2/fixtures.py
+++ tests/integrationv2/fixtures.py
@@ -10,6 +10,7 @@ from providers import Provider, S2N, JavaSSL
 
 from common import ProviderOptions
 
+
 @pytest.fixture
 def path_configuration(request: pytest.FixtureRequest):
     """
@@ -19,10 +20,10 @@ def path_configuration(request: pytest.FixtureRequest):
     """
     if not request.config.getoption("--best-effort"):
         return None
-    
+
     providers = {}
 
-    # s2n-tls MUST be available, and we expect it to be in 
+    # s2n-tls MUST be available, and we expect it to be in
     # <git_root>/build/bin
     expected_location = "../../build/bin"
     for binary in ["s2nd", "s2nc"]:
@@ -32,12 +33,13 @@ def path_configuration(request: pytest.FixtureRequest):
     providers[S2N] = expected_location
 
     # The configured location of the SSLSocketClient is in
-    # integrationv2/bin, which is expected for local runs. 
+    # integrationv2/bin, which is expected for local runs.
     # We just make sure that it is compiled
     if os.path.exists("./bin/SSLSocketClient.class"):
         providers[JavaSSL] = ""
-    
+
     return providers
+
 
 @pytest.fixture
 def managed_process(path_configuration):
@@ -63,14 +65,14 @@ def managed_process(path_configuration):
             if provider_class not in path_configuration:
                 aborted = True
                 pytest.skip(f"{provider_class} is not available")
-        
+
         provider = provider_class(options)
         cmd_line = provider.get_cmd_line()
 
         if best_effort_mode and provider_class is S2N:
             if cmd_line[0] == "s2nc" or cmd_line[0] == "s2nd":
                 cmd_line[0] = f"{path_configuration[S2N]}/{cmd_line[0]}"
-            else: # "s2nc_head" or "s2nd_head"
+            else:  # "s2nc_head" or "s2nd_head"
                 aborted = True
                 pytest.skip("s2nc_head or s2nd_head not found")
 
