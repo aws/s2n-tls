089aa843955fdfcb8643f4bb7f31c12f64ebd5e2
James Mayclin <maycj@amazon.com>
2025-02-11 23:25:56 +0000

    address pr feedback
    * cite docs on each quote
    * use stuffer_wipe_n to clear the null terminator
    * use base64_group convention

--- stuffer/s2n_stuffer_base64.c
+++ stuffer/s2n_stuffer_base64.c
@@ -58,7 +58,8 @@ int s2n_stuffer_read_base64(struct s2n_stuffer *stuffer, struct s2n_stuffer *out
     POSIX_GUARD(s2n_stuffer_skip_write(out, binary_output_size));
     uint8_t *start_of_binary_output = out->blob.data + binary_output_offset;
 
-    /* > This function will return the length of the data decoded or -1 on error.*/
+    /* https://docs.openssl.org/master/man3/EVP_EncodeInit/
+     * > This function will return the length of the data decoded or -1 on error. */
     int res = EVP_DecodeBlock(start_of_binary_output, start_of_base64_data, base64_data_size);
     POSIX_ENSURE(res == binary_output_size, S2N_ERR_INVALID_BASE64);
 
@@ -90,11 +91,14 @@ int s2n_stuffer_write_base64(struct s2n_stuffer *stuffer, struct s2n_stuffer *in
     if (binary_data_size == 0) {
         return S2N_SUCCESS;
     }
-    int base64_output_size = (binary_data_size / 3) * 4;
+    
+    int base64_groups = binary_data_size / 3;
     /* we will need to add a final padded block */
     if (binary_data_size % 3 != 0) {
-        base64_output_size += 4;
+        base64_groups++;
     }
+    
+    int base64_output_size = base64_groups * 4;
     /* Null terminator is added */
     base64_output_size += 1;
 
@@ -106,10 +110,11 @@ int s2n_stuffer_write_base64(struct s2n_stuffer *stuffer, struct s2n_stuffer *in
     POSIX_GUARD(s2n_stuffer_skip_write(stuffer, base64_output_size));
     uint8_t *start_of_base64_output = stuffer->blob.data + base64_output_offset;
 
-    /* > The length of the data generated without the NUL terminator is returned from the function. */
+    /* https://docs.openssl.org/master/man3/EVP_EncodeInit/
+     * > The length of the data generated without the NUL terminator is returned from the function. */
     int res = EVP_EncodeBlock(start_of_base64_output, start_of_binary_data, binary_data_size);
     POSIX_ENSURE(res == base64_output_size - 1, S2N_ERR_INVALID_BASE64);
-    stuffer->write_cursor -= 1;
+    POSIX_GUARD(s2n_stuffer_wipe_n(stuffer, 1));
 
     return S2N_SUCCESS;
 }
