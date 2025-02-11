9bcb1751130fc1bf2e8c6315f21404b8851ccaa3
Lindsay Stewart <slindsay@amazon.com>
2025-02-11 10:32:13 -0800

    update cbmc proofs

---
+++ tests/cbmc/stubs/s2n_evp_signing_supported.c
@@ -0,0 +1,28 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use
+ * this file except in compliance with the License. A copy of the License is
+ * located at
+ *
+ *     http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <cbmc_proof/nondet.h>
+
+static bool is_set = false;
+static bool is_evp_signing_supported = false;
+
+bool s2n_evp_signing_supported()
+{
+    if (!is_set) {
+        is_evp_signing_supported = nondet_bool();
+        is_set = true;
+    }
+    return is_evp_signing_supported;
+}
