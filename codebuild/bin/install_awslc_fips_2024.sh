7a4e7b68d63709c39d124058c64db6aaf523a76a
Doug Chapman <54039637+dougch@users.noreply.github.com>
2025-02-18 13:30:14 -0800

    Update codebuild/bin/install_awslc_fips_2024.sh
    Co-authored-by: Lindsay Stewart <stewart.r.lindsay@gmail.com>

--- codebuild/bin/install_awslc_fips_2024.sh
+++ codebuild/bin/install_awslc_fips_2024.sh
@@ -13,7 +13,5 @@ if [ "$#" -ne "2" ]; then
 fi
 
 CBPATH=$(dirname $0)
-BUILD_DIR=$1
-INSTALL_DIR=$2
 
-$CBPATH/install_awslc_fips.sh $BUILD_DIR $INSTALL_DIR 2024
+$CBPATH/install_awslc_fips.sh $@ 2024
