fda1cd255c9b54598235e525055d924cea50a14d
Lindsay Stewart <slindsay@amazon.com>
2025-02-20 15:35:42 -0800

    also test other policies for RSA+MD5_SHA1 and NULL

--- tests/unit/s2n_signature_scheme_test.c
+++ tests/unit/s2n_signature_scheme_test.c
@@ -28,6 +28,9 @@ int main(int argc, char **argv)
     while (security_policy_selection[policy_i].version != NULL) {
         const struct s2n_signature_preferences *sig_prefs =
                 security_policy_selection[policy_i].security_policy->signature_preferences;
+
+        bool s2n_rsa_pkcs1_md5_sha1_found = false;
+
         for (size_t sig_i = 0; sig_i < sig_prefs->count; sig_i++) {
             const struct s2n_signature_scheme *const sig_scheme = sig_prefs->signature_schemes[sig_i];
 
@@ -53,6 +56,15 @@ int main(int argc, char **argv)
                 EXPECT_NOT_EQUAL(sig_scheme->iana_value, potential_duplicate->iana_value);
             }
 
+            if (sig_scheme == &s2n_rsa_pkcs1_md5_sha1) {
+                s2n_rsa_pkcs1_md5_sha1_found = true;
+            }
+
+            /* s2n_null_sig_scheme is not a real signature scheme and is just a placeholder.
+             * It should not appear in any policy.
+             */
+            EXPECT_NOT_EQUAL(sig_scheme, &s2n_null_sig_scheme);
+
             /* Must be included in s2n_signature_preferences_all */
             bool in_all = false;
             for (size_t all_i = 0; all_i < all_prefs->count; all_i++) {
@@ -62,38 +74,18 @@ int main(int argc, char **argv)
             }
             EXPECT_TRUE(in_all);
         }
+
+        /* Only s2n_signature_preferences_all should include s2n_rsa_pkcs1_md5_sha1
+         *
+         * s2n_rsa_pkcs1_md5_sha1 is the implicit default for pre-TLS1.2 when no signature
+         * schemes are provided. Any code that needs to handle "all signature schemes"
+         * also needs to handle s2n_rsa_pkcs1_md5_sha1. It is not explicitly included
+         * in any real signature preferences, but should still be tracked by
+         * s2n_signature_preferences_all.
+         */
+        EXPECT_EQUAL(s2n_rsa_pkcs1_md5_sha1_found, sig_prefs == all_prefs);
+
         policy_i++;
-    }
-
-    /* Test: s2n_signature_preferences_all should also include s2n_rsa_pkcs1_md5_sha1
-     *
-     * s2n_rsa_pkcs1_md5_sha1 is the implicit default for pre-TLS1.2 when no signature
-     * schemes are provided. Any code that needs to handle "all signature schemes"
-     * also needs to handle s2n_rsa_pkcs1_md5_sha1. It is not explicitly included
-     * in any security policy, but should still be tracked by s2n_signature_preferences_all.
-     */
-    {
-        bool includes_md5_sha1 = false;
-        for (size_t i = 0; i < all_prefs->count; i++) {
-            if (all_prefs->signature_schemes[i] == &s2n_rsa_pkcs1_md5_sha1) {
-                includes_md5_sha1 = true;
-            }
-        }
-        EXPECT_TRUE(includes_md5_sha1);
-    }
-
-    /* Test: s2n_signature_preferences_all should not include s2n_null_sig_scheme.
-     *
-     * s2n_null_sig_scheme is not a real signature scheme and is just a placeholder.
-     */
-    {
-        bool includes_null = false;
-        for (size_t i = 0; i < all_prefs->count; i++) {
-            if (all_prefs->signature_schemes[i] == &s2n_null_sig_scheme) {
-                includes_null = true;
-            }
-        }
-        EXPECT_FALSE(includes_null);
     }
 
     END_TEST();
