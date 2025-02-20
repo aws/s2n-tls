8cc964d9f3f35c3ccef461943359216ebf80ecff
Jacob Jo <johubertj@gmail.com>
2025-02-20 00:46:58 +0000

    removed an import not needed

--- tests/integrationv2/utils.py
+++ tests/integrationv2/utils.py
@@ -1,6 +1,5 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
-import subprocess
 from common import Certificates, Protocols
 from providers import OpenSSL, S2N
 from global_flags import get_flag, S2N_FIPS_MODE
