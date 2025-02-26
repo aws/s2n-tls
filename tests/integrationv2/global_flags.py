ac1d09851b4d625f83d1fa68b467abbca8fdd408
Jacob Jo <47648260+johubertj@users.noreply.github.com>
2025-02-26 10:59:26 -0800

    Ruff Formatting and add to CI (#5138)
    Co-authored-by: James Mayclin <maycj@amazon.com>

--- tests/integrationv2/global_flags.py
+++ tests/integrationv2/global_flags.py
@@ -5,11 +5,11 @@
 # based on the environment.
 
 # If S2N is operating in FIPS mode
-S2N_FIPS_MODE = 's2n_fips_mode'
+S2N_FIPS_MODE = "s2n_fips_mode"
 
 # The version of provider being used
 # (set from the S2N_LIBCRYPTO env var, which is how the original integration test works)
-S2N_PROVIDER_VERSION = 's2n_provider_version'
+S2N_PROVIDER_VERSION = "s2n_provider_version"
 
 _flags = {}
 
