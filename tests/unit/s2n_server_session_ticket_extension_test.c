a6159eb4b94fd026bd91918e8705707829794758
James Mayclin <maycj@amazon.com>
2025-02-27 18:45:32 +0000

    style: fix redundant return

--- tests/unit/s2n_server_session_ticket_extension_test.c
+++ tests/unit/s2n_server_session_ticket_extension_test.c
@@ -92,5 +92,4 @@ int main(int argc, char **argv)
     };
 
     END_TEST();
-    return 0;
 }
--- tests/unit/s2n_session_ticket_test.c
+++ tests/unit/s2n_session_ticket_test.c
@@ -1749,5 +1749,4 @@ int main(int argc, char **argv)
     free(cert_chain);
     free(private_key);
     END_TEST();
-    return 0;
 }
