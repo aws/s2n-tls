470a02caaa075e2efe3c196759305806cced69d5
Jacob Jo <johubertj@gmail.com>
2025-03-10 19:07:27 +0000

    removed an additional # lgtm [py/unused-import]

--- tests/integrationv2/test_version_negotiation.py
+++ tests/integrationv2/test_version_negotiation.py
@@ -10,7 +10,7 @@ from configuration import (
     ALL_TEST_CERTS,
 )
 from common import ProviderOptions, Protocols, data_bytes
-from fixtures import managed_process  # lgtm [py/unused-import]  # noqa: F401
+from fixtures import managed_process  # noqa: F401
 from providers import Provider, S2N, OpenSSL, GnuTLS
 from utils import (
     invalid_test_parameters,
