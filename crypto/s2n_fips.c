6f1df2b31819f6a4479e310348bc53bb443855af
Lindsay Stewart <slindsay@amazon.com>
2025-03-13 15:57:46 -0700

    feature: openssl-3.0-fips support

--- crypto/s2n_fips.c
+++ crypto/s2n_fips.c
@@ -59,11 +59,11 @@ int s2n_fips_init(void)
 
     /* When using Openssl, ONLY 3.0 currently supports FIPS.
      * openssl-1.0.2-fips is no longer supported.
-     * openssl > 3.0 will likely have a FIPS 140-3 certificate instead of a
+     * openssl >= 3.5 will likely have a FIPS 140-3 certificate instead of a
      * FIPS 140-2 certificate, which will require additional review in order
      * to properly integrate.
      */
-#if defined(OPENSSL_FIPS) || S2N_OPENSSL_VERSION_AT_LEAST(3, 1, 0)
+#if defined(OPENSSL_FIPS) || S2N_OPENSSL_VERSION_AT_LEAST(3, 5, 0)
     POSIX_ENSURE(!s2n_fips_mode_enabled, S2N_ERR_FIPS_MODE_UNSUPPORTED);
 #endif
 
