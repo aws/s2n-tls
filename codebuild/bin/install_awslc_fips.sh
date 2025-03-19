ce4c610b625e62029f0409a52b5c33d6f97fa016
Doug Chapman <dougch@amazon.com>
2025-03-18 17:15:20 -0700

    refactor of unit nix buildspec

--- codebuild/bin/install_awslc_fips.sh
+++ codebuild/bin/install_awslc_fips.sh
@@ -4,7 +4,8 @@
 set -eu
 
 usage() {
-    echo "install_awslc_fips.sh build_dir install_dir version"
+    echo -e "\ninstall_awslc_fips.sh build_dir install_dir version"
+    echo -e "\tversion: 2022|2024|next\n"
     exit 1
 }
 
@@ -48,19 +49,23 @@ check_dep go
 BUILD_DIR=$1
 INSTALL_DIR=$2
 VERSION=$3
+GH_RELEASE_URL="https://api.github.com/repos/aws/aws-lc/releases"
 
-# Map version to a specific feature branch/tag.
+# Map version to the latest release of the certificate year.
 # Note: since the next FIPS validation will be split off from main
-# building main with FIPS enabled is essentially the next FIPS release.
+# building main with FIPS enabled is the next candidate FIPS branch.
 case $VERSION in
   "2022")
-    AWSLC_BRANCH=fips-2024-09-27
+    AWSLC_BRANCH=$(curl --silent $GH_RELEASE_URL \
+      |grep -Po '"tag_name": "\KAWS-LC-FIPS-2.*?(?=")' |head -1)
     ;;
   "2024")
-    AWSLC_BRANCH=fips-2022-11-02
+    AWSLC_BRANCH=$(curl --silent $GH_RELEASE_URL \
+      |grep -Po '"tag_name": "\KAWS-LC-FIPS-3.*?(?=")' |head -1)
     ;;
   "next")
     AWSLC_BRANCH=main
+    ;;
   *)
     echo "Unknown version: $VERSION"
     usage
