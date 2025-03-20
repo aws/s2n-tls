58d874253b3b0b1d50eef27cedb4e22e6c81e255
Wesley Rosenblum <wesleyr@amazon.com>
2025-03-20 01:09:41 +0000

    Update https

--- bin/https.c
+++ bin/https.c
@@ -113,6 +113,10 @@ int https(struct s2n_connection *conn, uint32_t bench)
     BUFFER("Curve: %s\n", s2n_connection_get_curve(conn));
     BUFFER("KEM: %s\n", s2n_connection_get_kem_name(conn));
     BUFFER("KEM Group: %s\n", s2n_connection_get_kem_group_name(conn));
+    const char* group_name = NULL;
+    if (s2n_connection_get_key_exchange_group_name(conn, &group_name)) {
+        BUFFER("Key Exchange Group: %s\n", group_name);
+    }
     BUFFER("Cipher negotiated: %s\n", s2n_connection_get_cipher(conn));
     BUFFER("Session resumption: %s\n", s2n_connection_is_session_resumed(conn) ? "true" : "false");
 
