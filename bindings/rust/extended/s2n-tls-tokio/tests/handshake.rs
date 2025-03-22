0687d454951703cb97edcc325fafcb532dcd24f1
Wesley Rosenblum <wesleyr@amazon.com>
2025-03-22 04:04:51 +0000

    fixes

--- bindings/rust/extended/s2n-tls-tokio/tests/handshake.rs
+++ bindings/rust/extended/s2n-tls-tokio/tests/handshake.rs
@@ -16,6 +16,7 @@ use tokio::time;
 
 pub mod common;
 
+#[allow(deprecated)]
 #[tokio::test]
 async fn handshake_basic() -> Result<(), Box<dyn std::error::Error>> {
     let (server_stream, client_stream) = common::get_streams().await?;
@@ -33,13 +34,11 @@ async fn handshake_basic() -> Result<(), Box<dyn std::error::Error>> {
         assert!(tls.as_ref().handshake_type()?.contains("NEGOTIATED"));
         // Cipher suite may change, so just makes sure we can retrieve it.
         assert!(tls.as_ref().cipher_suite().is_ok());
-        #[allow(deprecated)]
         assert!(tls.as_ref().selected_curve().is_ok());
         assert!(tls.as_ref().selected_key_exchange_group_name().is_ok());
-        #[allow(deprecated)]
         assert_eq!(
-            tls.as_ref().selected_curve(),
-            tls.as_ref().selected_key_exchange_group_name(),
+            tls.as_ref().selected_curve().unwrap(),
+            tls.as_ref().selected_key_exchange_group_name().unwrap(),
         );
     }
 
