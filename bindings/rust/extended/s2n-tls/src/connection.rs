fd09952ee201ccf578d18a9acb952e3ce0d49578
Jacob Jo <johubertj@gmail.com>
2025-03-26 17:21:32 +0000

    removed comments

--- bindings/rust/extended/s2n-tls/src/connection.rs
+++ bindings/rust/extended/s2n-tls/src/connection.rs
@@ -1143,16 +1143,9 @@ impl Connection {
         hash_alg.try_into()
     }
 
-    /// This method safely wraps the C function and returns the certificate match result
-    /// (e.g., Exact, Wildcard, or No Match) as a Rust enum.
     /// Corresponds to [s2n_connection_get_certificate_match].
     pub fn certificate_match(&self) -> Result<CertSNIMatch, Error> {
-        // Create a mutable variable to store the output value from the C function.
-        // We initialize it to a default (NO_MATCH), but the C function will overwrite it.
         let mut version = s2n_cert_sni_match::SNI_NO_MATCH;
-
-        // Call the raw C function via FFI.
-        // It takes a raw pointer to the connection, and a pointer to where the match result should go.
         unsafe {
             s2n_connection_get_certificate_match(self.connection.as_ptr(), &mut version)
                 .into_result()?;
