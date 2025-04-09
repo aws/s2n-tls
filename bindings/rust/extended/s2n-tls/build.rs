d9a4a9087ffd38031c33518cd5fabee7a6d34731
Sam Clark <3758302+goatgoose@users.noreply.github.com>
2025-04-09 15:17:47 -0400

    pr feedback

--- bindings/rust/extended/s2n-tls/build.rs
+++ bindings/rust/extended/s2n-tls/build.rs
@@ -7,18 +7,15 @@ const EXTERNAL_BUILD_CFG_NAME: &str = "s2n_tls_external_build";
 fn main() {
     println!("cargo:rustc-check-cfg=cfg({EXTERNAL_BUILD_CFG_NAME})");
 
-    for (name, value) in std::env::vars() {
-        /* s2n-tls-sys exports the external build environment variable when libs2n is externally
-         * linked. Set a cfg attribute in this case to allow s2n-tls to be aware of the external
-         * build.
-         */
-        if name == EXTERNAL_BUILD_ENV_NAME {
-            println!("cargo:rerun-if-env-changed={EXTERNAL_BUILD_ENV_NAME}");
+    /* s2n-tls-sys exports the external build environment variable when libs2n is externally
+     * linked. Set a cfg attribute in this case to allow s2n-tls to be aware of the external build.
+     */
+    if let Ok(external_build_var) = std::env::var(EXTERNAL_BUILD_ENV_NAME) {
+        println!("cargo:rerun-if-env-changed={EXTERNAL_BUILD_ENV_NAME}");
 
-            let external_build: bool = value.parse().unwrap();
-            if external_build {
-                println!("cargo:rustc-cfg={EXTERNAL_BUILD_CFG_NAME}");
-            }
+        let external_build: bool = external_build_var.parse().unwrap();
+        if external_build {
+            println!("cargo:rustc-cfg={EXTERNAL_BUILD_CFG_NAME}");
         }
     }
 }
