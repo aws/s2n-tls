fb7c1c8024b67700964479961418ba49d3f7a90e
Jou Ho <jouh@amazon.com>
2025-04-15 23:45:04 +0000

    use EXPECT_EQUAL instead of strstr

--- tests/unit/s2n_build_test.c
+++ tests/unit/s2n_build_test.c
@@ -164,14 +164,7 @@ int main()
     {
         if (version != NULL) {
             const char *ssleay_version_text = SSLeay_version(SSLEAY_VERSION);
-            if (strstr(ssleay_version_text, version) == NULL) {
-                char fail_msg[256];
-                snprintf(
-                        fail_msg, sizeof(fail_msg),
-                        "Libcrypto version mismatch - expected version '%s' not found in '%s'",
-                        version, ssleay_version_text);
-                FAIL_MSG(fail_msg);
-            }
+            EXPECT_EQUAL(ssleay_version_text, version);
         }
     };
 
