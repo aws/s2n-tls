8d75631053c0041ca89fbaf2dcd8dfe13eaf03c9
Jacob Jo <johubertj@gmail.com>
2025-04-23 23:02:14 +0000

    : to ;

--- bindings/rust/extended/s2n-tls-sys/build.rs
+++ bindings/rust/extended/s2n-tls-sys/build.rs
@@ -186,7 +186,7 @@ fn builder(libcrypto: &Libcrypto) -> cc::Build {
         .flag_if_supported("-z noexecstack")
         // we use some deprecated libcrypto features so don't warn here
         .flag_if_supported("-Wno-deprecated-declarations")
-        .flag_if_supported("-Wa,-mbranches-within-32B-boundaries"):
+        .flag_if_supported("-Wa,-mbranches-within-32B-boundaries");
 
     build
 }
