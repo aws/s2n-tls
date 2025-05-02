990985e3d4d77d831e14031f68cf9ff72d7de794
Doug Chapman <dougch@amazon.com>
2025-05-02 11:51:11 -0700

    Make valgrind.nix platform-aware
    This commit updates the valgrind.nix file to handle different platforms:
    - On Linux, it uses a custom build of valgrind 3.21.0
    - On macOS, it uses the nixpkgs valgrind with the broken attribute overridden
    This fixes the issue with glibc not being available on macOS.

--- nix/valgrind.nix
+++ nix/valgrind.nix
@@ -1,30 +1,38 @@
 { pkgs }:
-pkgs.stdenv.mkDerivation rec {
-  pname = "valgrind";
-  version = "3.21.0"; # This is a stable version that should work
+# Check if we're on Linux or macOS
+if pkgs.stdenv.isLinux then
+  # Linux version
+  pkgs.stdenv.mkDerivation rec {
+    pname = "valgrind";
+    version = "3.21.0"; # This is a stable version that should work
 
-  src = pkgs.fetchurl {
-    url = "https://sourceware.org/pub/valgrind/valgrind-${version}.tar.bz2";
-    sha256 = "sha256-Yk9qPJfiGGd2LwJjgH9VLlDI8MhRvqx0aYOzUZSQJFU="; # SHA for 3.21.0
-  };
+    src = pkgs.fetchurl {
+      url = "https://sourceware.org/pub/valgrind/valgrind-${version}.tar.bz2";
+      sha256 = "sha256-Yk9qPJfiGGd2LwJjgH9VLlDI8MhRvqx0aYOzUZSQJFU="; # SHA for 3.21.0
+    };
 
-  # Dependencies needed for valgrind
-  nativeBuildInputs = [ pkgs.perl pkgs.gdb ];
-  buildInputs = [ pkgs.glibc ];
+    # Dependencies needed for valgrind
+    nativeBuildInputs = [ pkgs.perl pkgs.gdb ];
+    buildInputs = [ pkgs.glibc ];
 
-  # Standard configure/make/install process
-  configureFlags = [
-    "--enable-only64bit"
-    "--enable-lto=yes"
-  ];
+    # Standard configure/make/install process
+    configureFlags = [
+      "--enable-only64bit"
+      "--enable-lto=yes"
+    ];
 
-  # Ensure valgrind can find the debug info
-  postInstall = ''
-    for i in $out/lib/valgrind/*-*-linux; do
-      ln -s $i $out/lib/valgrind/$(basename $i | sed 's/-linux$//')
-    done
-  '';
+    # Ensure valgrind can find the debug info
+    postInstall = ''
+      for i in $out/lib/valgrind/*-*-linux; do
+        ln -s $i $out/lib/valgrind/$(basename $i | sed 's/-linux$//')
+      done
+    '';
 
-  # Disable broken meta attribute
-  meta.broken = false;
-}
+    # Disable broken meta attribute
+    meta.broken = false;
+  }
+else
+  # For macOS, just use the nixpkgs valgrind but override the broken attribute
+  pkgs.valgrind.overrideAttrs (oldAttrs: {
+    meta = oldAttrs.meta // { broken = false; };
+  })
