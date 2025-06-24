c9c9acd84e8cc77c6b09b370af0bd142708cfac0
DougCh <dougch@amazon.com>
2025-06-24 23:07:33 +0000

    rework uvinteg snapshot check command

--- nix/shell.sh
+++ nix/shell.sh
@@ -81,24 +81,17 @@ function unit {(set -e
     fi
 )}
 
-function uvinteg_snapshot {(
-    # Use pytest --collect-only to gather the top level tests
-    set -e
-    echo "Gathering the top level tests pytest would run..."
-    echo "PYTEST_ARGS: $PYTEST_ARGS"
-    PYTHONPATH="" uv run pytest --collect-only $PYTEST_ARGS | grep Module > /tmp/uvinteg_tests.txt
-    echo "Comparing the current list of integ tests against what is checked-in..."
-    diff -q /tmp/uvinteg_tests.txt uvinteg_tests.txt
-)}
-
 # Function to launch pytest with uv.
 function uvinteg {(
-    set -e
-    apache2_start
-    cd ./tests/integrationv2
-    # Dynamic Record Sizes will be re-written; skip for now.
+    set -eu
+    # TODO: Dynamic Record Sizes needs a rewrite; skip for now.
     export PYTEST_ARGS="--provider-version $S2N_LIBCRYPTO -x -n auto --reruns=2 --durations=10 -rpfs --cache-clear --ignore-glob=*test_dynamic_record_sizes*"
-    uvinteg_snapshot
+    cd ./tests/integrationv2
+    echo -n "Comparing the current list of integ tests against what is checked-in..."
+    PYTHONPATH="" uv run pytest --collect-only $PYTEST_ARGS | grep Module > /tmp/uvinteg_tests.txt
+    diff -q /tmp/uvinteg_tests.txt uvinteg_tests.txt
+    echo "tests to run match uvinteg_tests.txt."
+    apache2_start
     if [[ -z "$1" ]]; then
         echo "Running all integ tests with uv"
         PYTHONPATH="" uv run pytest $PYTEST_ARGS --junitxml=../../build/junit/uv_integ.xml
