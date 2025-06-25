9e87feaad41a27b4e9ce8cf131e91cf1dbb6ef16
Jacob Jo <johubertj@gmail.com>
2025-06-25 21:37:04 +0000

    fixed clang formatting

--- tls/s2n_kem.c
+++ tls/s2n_kem.c
@@ -484,8 +484,7 @@ bool s2n_kem_is_available(const struct s2n_kem *kem)
         return s2n_libcrypto_supports_mlkem();
     }
 
-    if (kem->kem_nid == S2N_NID_KYBER512 ||
-        kem->kem_nid == S2N_NID_KYBER768 ||
+    if (kem->kem_nid == S2N_NID_KYBER512 || kem->kem_nid == S2N_NID_KYBER768 ||
         kem->kem_nid == S2N_NID_KYBER1024) {
         return s2n_libcrypto_supports_evp_kem();
     }
