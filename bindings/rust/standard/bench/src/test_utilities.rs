f633da0e67c844944999658b9b619690c7df3eac
James Mayclin <maycj@amazon.com>
2025-07-23 00:33:58 +0000

    address ci failure
    * nightly fmt

--- bindings/rust/standard/bench/src/test_utilities.rs
+++ bindings/rust/standard/bench/src/test_utilities.rs
@@ -1,8 +1,8 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::harness::{TlsBenchConfig, TlsInfo};
 use crate::{
+    harness::{TlsBenchConfig, TlsInfo},
     CipherSuite, CryptoConfig, HandshakeType, KXGroup, SigType, TlsConnPair, TlsConnection,
 };
 use std::io::ErrorKind;
