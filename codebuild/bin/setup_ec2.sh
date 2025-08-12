805c18816979a8885f78ca07fe26e463187be417
Doug Chapman <dougch@amazon.com>
2025-08-12 17:39:12 +0000

    add gnutls over-ride fixes; shellcheck improvements

--- codebuild/bin/setup_ec2.sh
+++ codebuild/bin/setup_ec2.sh
@@ -2,23 +2,45 @@
 
 set -euo pipefail
 
-echo "Is $HOME set? Is $USER set?"
-echo "=== Setting up sudo for the nix user ==="
-sudo bash -c "echo 'nix ALL=NOPASSWD: ALL' > /etc/sudoers.d/nix"
-sudo groupadd nixbld
-sudo useradd -m -g nixbld -G nixbld nix
-sudo chmod 755 /home/nix
+if [[ -f /etc/os-release ]]; then
+    source "/etc/os-release"
+    if [[ "$UBUNTU_CODENAME" == "noble" ]]; then
+        echo "=== Turning off gnuTLS overrides"
+        rm /etc/gnutls/config
+    fi
+fi
 
-echo "=== Installing Nix ==="
-sudo -u nix bash -c "sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --no-daemon"
-echo "=== Enabling Nix flakes ==="
-sudo -u nix bash -c 'mkdir -p ~/.config/nix; echo "experimental-features = nix-command flakes" >> ~/.config/nix/nix.conf'
-# This sidesteps the need to update PATH
-sudo ln -s /home/nix/.nix-profile/bin/nix /usr/local/bin
+setupnix() {
+    echo "=== Setting up nix user, groups and permissions ==="
+    sudo groupadd nixbld
+    sudo useradd -m -g nixbld -G nixbld nix
+    # Give everyone on the system read access to the nix user homedir.
+    sudo chmod 755 /home/nix
+    echo "Both $HOME and $USER need to be set for the nix installer to work."
+    echo "=== Installing Nix ==="
+    sudo -u nix bash -c "sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --no-daemon"
+    echo "=== Enabling Nix flakes ==="
+    sudo -u nix bash -c 'mkdir -p ~/.config/nix; echo "experimental-features = nix-command flakes" >> ~/.config/nix/nix.conf'
+    # This sidesteps the need to update PATH for every user.
+    sudo ln -s /home/nix/.nix-profile/bin/nix /usr/local/bin
+    echo "=== Setting up Nix configs for the root user ==="
+    sudo -u root bash -c "ln -s /home/nix/.nix-profile ~/"
+    sudo -u root bash -c "ln -s /home/nix/.config ~/"
+}
 
-echo "=== Setting up Nix for root ==="
-sudo -u root bash -c "ln -s /home/nix/.nix-profile ~/"
-sudo -u root bash -c "ln -s /home/nix/.config ~/"
+setupsudo() {
+    echo "=== Setting up sudo for the nix user, needed for installation ==="
+    # The nix installer refuses to install as root, so we need to set up a user, with sudo.
+    sudo bash -c "echo 'nix ALL=NOPASSWD: ALL' > /etc/sudoers.d/nix"
+}
+# main
+if [[ "$UBUNTU_CODENAME" == "noble" ]]; then
+    # system defaults for gnuTLS disable older algorithms we need for testing.
+    echo "=== Turning off gnuTLS overrides"
+    rm /etc/gnutls/config
+fi
 
 sudo apt update
 sudo apt upgrade -y
+setupsudo
+setupnix
