779b6e1528c225c75b4b712a5b4d76d475da077e
WillChilds-Klein <willck93@gmail.com>
2025-08-21 03:09:13 +0000

    Create separate non-PQ signature pref preferring ECDSA

--- tls/s2n_security_policies.c
+++ tls/s2n_security_policies.c
@@ -360,7 +360,7 @@ const struct s2n_security_policy security_policy_cloudfront_upstream_2025_08_08 
     .minimum_protocol_version = S2N_SSLv3,
     .cipher_preferences = &cipher_preferences_cloudfront_upstream_2025_08_08,
     .kem_preferences = &kem_preferences_null,
-    .signature_preferences = &s2n_signature_preferences_20201021,
+    .signature_preferences = &s2n_signature_preferences_20250820,
     .ecc_preferences = &s2n_ecc_preferences_20230623,
 };
 
@@ -368,7 +368,7 @@ const struct s2n_security_policy security_policy_cloudfront_upstream_2025_08_08_
     .minimum_protocol_version = S2N_TLS10,
     .cipher_preferences = &cipher_preferences_cloudfront_upstream_2025_08_08,
     .kem_preferences = &kem_preferences_null,
-    .signature_preferences = &s2n_signature_preferences_20201021,
+    .signature_preferences = &s2n_signature_preferences_20250820,
     .ecc_preferences = &s2n_ecc_preferences_20230623,
 };
 
@@ -376,7 +376,7 @@ const struct s2n_security_policy security_policy_cloudfront_upstream_2025_08_08_
     .minimum_protocol_version = S2N_TLS11,
     .cipher_preferences = &cipher_preferences_cloudfront_upstream_2025_08_08,
     .kem_preferences = &kem_preferences_null,
-    .signature_preferences = &s2n_signature_preferences_20201021,
+    .signature_preferences = &s2n_signature_preferences_20250820,
     .ecc_preferences = &s2n_ecc_preferences_20230623,
 };
 
@@ -384,7 +384,7 @@ const struct s2n_security_policy security_policy_cloudfront_upstream_2025_08_08_
     .minimum_protocol_version = S2N_TLS12,
     .cipher_preferences = &cipher_preferences_cloudfront_upstream_2025_08_08,
     .kem_preferences = &kem_preferences_null,
-    .signature_preferences = &s2n_signature_preferences_20201021,
+    .signature_preferences = &s2n_signature_preferences_20250820,
     .ecc_preferences = &s2n_ecc_preferences_20230623,
 };
 
@@ -392,7 +392,7 @@ const struct s2n_security_policy security_policy_cloudfront_upstream_2025_08_08_
     .minimum_protocol_version = S2N_TLS13,
     .cipher_preferences = &cipher_preferences_cloudfront_upstream_2025_08_08,
     .kem_preferences = &kem_preferences_null,
-    .signature_preferences = &s2n_signature_preferences_20201021,
+    .signature_preferences = &s2n_signature_preferences_20250820,
     .ecc_preferences = &s2n_ecc_preferences_20230623,
 };
 
@@ -400,7 +400,7 @@ const struct s2n_security_policy security_policy_cloudfront_upstream_2025_08_08_
     .minimum_protocol_version = S2N_SSLv3,
     .cipher_preferences = &cipher_preferences_cloudfront_upstream_2025_08_08,
     .kem_preferences = &kem_preferences_pq_tls_1_3_ietf_2025_07,
-    .signature_preferences = &s2n_signature_preferences_20250820,
+    .signature_preferences = &s2n_signature_preferences_20250821,
     .ecc_preferences = &s2n_ecc_preferences_20230623,
 };
 
@@ -408,7 +408,7 @@ const struct s2n_security_policy security_policy_cloudfront_upstream_2025_08_08_
     .minimum_protocol_version = S2N_TLS10,
     .cipher_preferences = &cipher_preferences_cloudfront_upstream_2025_08_08,
     .kem_preferences = &kem_preferences_pq_tls_1_3_ietf_2025_07,
-    .signature_preferences = &s2n_signature_preferences_20250820,
+    .signature_preferences = &s2n_signature_preferences_20250821,
     .ecc_preferences = &s2n_ecc_preferences_20230623,
 };
 
@@ -416,7 +416,7 @@ const struct s2n_security_policy security_policy_cloudfront_upstream_2025_08_08_
     .minimum_protocol_version = S2N_TLS11,
     .cipher_preferences = &cipher_preferences_cloudfront_upstream_2025_08_08,
     .kem_preferences = &kem_preferences_pq_tls_1_3_ietf_2025_07,
-    .signature_preferences = &s2n_signature_preferences_20250820,
+    .signature_preferences = &s2n_signature_preferences_20250821,
     .ecc_preferences = &s2n_ecc_preferences_20230623,
 };
 
@@ -424,7 +424,7 @@ const struct s2n_security_policy security_policy_cloudfront_upstream_2025_08_08_
     .minimum_protocol_version = S2N_TLS12,
     .cipher_preferences = &cipher_preferences_cloudfront_upstream_2025_08_08,
     .kem_preferences = &kem_preferences_pq_tls_1_3_ietf_2025_07,
-    .signature_preferences = &s2n_signature_preferences_20250820,
+    .signature_preferences = &s2n_signature_preferences_20250821,
     .ecc_preferences = &s2n_ecc_preferences_20230623,
 };
 
@@ -432,7 +432,7 @@ const struct s2n_security_policy security_policy_cloudfront_upstream_2025_08_08_
     .minimum_protocol_version = S2N_TLS13,
     .cipher_preferences = &cipher_preferences_cloudfront_upstream_2025_08_08_tls13,
     .kem_preferences = &kem_preferences_pq_tls_1_3_ietf_2025_07,
-    .signature_preferences = &s2n_signature_preferences_20250820,
+    .signature_preferences = &s2n_signature_preferences_20250821,
     .ecc_preferences = &s2n_ecc_preferences_20230623,
 };
 
