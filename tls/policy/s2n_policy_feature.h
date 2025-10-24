a1ed3585548167932337bddbd2c13627c066348d
Jou Ho <43765840+jouho@users.noreply.github.com>
2025-10-24 13:28:48 -0700

    feat: output utility for security policy (#5502)

--- tls/policy/s2n_policy_feature.h
+++ tls/policy/s2n_policy_feature.h
@@ -153,3 +153,75 @@ struct s2n_security_policy *s2n_security_policy_build(struct s2n_security_policy
  * @returns S2N_SUCCESS on success. S2N_FAILURE on failure
  */
 int s2n_security_policy_free(struct s2n_security_policy **policy);
+
+/**
+ * Output format types for verbose policy output.
+ */
+typedef enum {
+    /**
+     * Produces structured output with the following sections:
+     * - min version: <minimum_protocol_version>
+     * - rules:
+     *   - <rule_name>: <yes|no>
+     * - cipher suites:
+     *   - <cipher_suite_name>
+     * - signature schemes:
+     *   - <signature_scheme_name>
+     * - curves:
+     *   - <curve_name>
+     * - certificate signature schemes: (if present)
+     *   - <cert_signature_scheme_name>
+     * - certificate keys: (if present)
+     *   - <certificate_key_name>
+     * - pq: (if present)
+     *   - revision: <pq_hybrid_draft_revision>
+     *   - kems: (if present)
+     *     -- <kem_name>
+     *   - kem groups:
+     *     -- <kem_group_name>
+     */
+    S2N_POLICY_FORMAT_DEBUG_V1 = 1,
+} s2n_policy_format;
+
+/**
+ * Retrieves the length of the buffer needed for s2n_security_policy_write_bytes().
+ * This function should be used to allocate enough memory for the policy output buffer before calling
+ * s2n_security_policy_write_bytes().
+ *
+ * @note The size of the policy output depends on the specific policy configuration.
+ * Do not expect the size to always remain the same across different policies.
+ *
+ * @param policy The security policy to get the buffer size for
+ * @param format The output format to use
+ * @param length Output parameter where the required buffer length will be written
+ * @returns S2N_SUCCESS on success, S2N_FAILURE on failure
+ */
+int s2n_security_policy_write_length(const struct s2n_security_policy *policy,
+        s2n_policy_format format, uint32_t *length);
+
+/**
+ * Writes output of a security policy to a user-provided buffer in the specified format.
+ * 
+ * @param policy The security policy to output
+ * @param format The output format to use
+ * @param buffer The buffer to write to
+ * @param buffer_length The size of the buffer
+ * @param output_size Output variable to be set to the actual number of bytes written to `buffer`
+ *                    This value is only meaningful when the function returns S2N_SUCCESS
+ * @returns S2N_SUCCESS on success, S2N_FAILURE on failure (e.g., if buffer is too small)
+ */
+int s2n_security_policy_write_bytes(const struct s2n_security_policy *policy,
+        s2n_policy_format format, uint8_t *buffer, uint32_t buffer_length, uint32_t *output_size);
+
+/**
+ * Writes output of a security policy to a file descriptor in the specified format.
+ * 
+ * @param policy The security policy to output
+ * @param format The output format to use  
+ * @param fd The file descriptor to write to (e.g., STDOUT_FILENO or an open file)
+ * @param output_size Output variable to be set to the actual number of bytes written to the file descriptor
+ *                    This value is only meaningful when the function returns S2N_SUCCESS
+ * @returns S2N_SUCCESS on success, S2N_FAILURE on failure
+ */
+int s2n_security_policy_write_fd(const struct s2n_security_policy *policy,
+        s2n_policy_format format, int fd, uint32_t *output_size);
