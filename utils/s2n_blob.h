92e05eb9aeab7629287dc614d6f7878554c40ad4
Akshat Sinha <akshatsinhasramhardy@gmail.com>
2025-10-24 11:33:09 +0530

    fix: use union to cast away const

--- utils/s2n_blob.h
+++ utils/s2n_blob.h
@@ -69,6 +69,10 @@ int S2N_RESULT_MUST_USE s2n_blob_slice(const struct s2n_blob *b, struct s2n_blob
     struct s2n_blob name = { 0 };                        \
     RESULT_GUARD_POSIX(s2n_blob_init(&name, name##_buf, name##_requested_size))
 
-#define S2N_BLOB_LABEL(name, str)             \
-    static const uint8_t name##_data[] = str; \
-    const struct s2n_blob name = { .data = (uint8_t *) name##_data, .size = sizeof(name##_data) - 1 };
+#define S2N_BLOB_LABEL(name, str)                                    \
+    static const uint8_t name##_data[] = str;                        \
+    static union{                                                   \
+        const uint8_t *const_ptr;                                    \
+        uint8_t *ptr;                                                \
+    } name##_union = { .const_ptr = name##_data };                   \
+    const struct s2n_blob name = { .data = name##_union.ptr, .size = sizeof(name##_data) - 1 };
