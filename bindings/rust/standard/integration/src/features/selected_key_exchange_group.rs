4ce7ea32dd28458e6a1d8fdd2098a50c1ac59a60
James Mayclin <maycj@amazon.com>
2025-12-15 22:58:06 +0000

    address ci issue
    * add header

--- bindings/rust/standard/integration/src/features/selected_key_exchange_group.rs
+++ bindings/rust/standard/integration/src/features/selected_key_exchange_group.rs
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 use std::time::SystemTime;
 
 use openssl::ssl::{SslContextBuilder, SslVersion};
