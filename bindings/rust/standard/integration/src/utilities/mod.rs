e1f7ac3056a951153ae963a2880115179ba9d8e6
James Mayclin <maycj@amazon.com>
2026-02-20 13:23:12 -0800

    Update bindings/rust/standard/integration/src/utilities/mod.rs
    Co-authored-by: kaukabrizvi <100529019+kaukabrizvi@users.noreply.github.com>

--- bindings/rust/standard/integration/src/utilities/mod.rs
+++ bindings/rust/standard/integration/src/utilities/mod.rs
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-//! This module holds items that make it simplier to write our tests.
+//! This module holds items that make it simpler to write our tests.
 //!
 //! This module should never hold any tests of TLS implementations.
 
