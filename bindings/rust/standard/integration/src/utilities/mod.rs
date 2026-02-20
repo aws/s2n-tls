6dfa87b79546d26b129d45cdf8d82dce4c7606f0
James Mayclin <maycj@amazon.com>
2026-02-20 00:07:32 +0000

    refactor(integration): utilities module with cert materials

---
+++ bindings/rust/standard/integration/src/utilities/mod.rs
@@ -0,0 +1,18 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+//! This module holds items that make it simplier to write our tests.
+//!
+//! This module should never hold any tests of TLS implementations.
+
+use s2n_tls::callbacks::VerifyHostNameCallback;
+
+pub mod capability_check;
+pub mod certs;
+
+pub struct IgnoreHostName;
+impl VerifyHostNameCallback for IgnoreHostName {
+    fn verify_host_name(&self, _host_name: &str) -> bool {
+        true
+    }
+}
