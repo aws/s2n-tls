c673fb0855ffa1e74668f09b907b9874b3dc5c9e
Jou Ho <jouh@amazon.com>
2026-05-20 23:43:15 +0000

    feat: add PQ-compatible variants of security policies

--- tls/s2n_cipher_preferences.h
+++ tls/s2n_cipher_preferences.h
@@ -71,6 +71,14 @@ extern const struct s2n_cipher_preferences cipher_preferences_20251114;
 extern const struct s2n_cipher_preferences cipher_preferences_20251115;
 extern const struct s2n_cipher_preferences cipher_preferences_20251116;
 extern const struct s2n_cipher_preferences cipher_preferences_20251117;
+extern const struct s2n_cipher_preferences cipher_preferences_20260520;
+extern const struct s2n_cipher_preferences cipher_preferences_20260520_gcm;
+extern const struct s2n_cipher_preferences cipher_preferences_20260521;
+extern const struct s2n_cipher_preferences cipher_preferences_20260521_gcm;
+extern const struct s2n_cipher_preferences cipher_preferences_20260522;
+extern const struct s2n_cipher_preferences cipher_preferences_20260522_gcm;
+extern const struct s2n_cipher_preferences cipher_preferences_20260523;
+extern const struct s2n_cipher_preferences cipher_preferences_20260523_gcm;
 extern const struct s2n_cipher_preferences cipher_preferences_20260220;
 
 extern const struct s2n_cipher_preferences cipher_preferences_default_fips;
