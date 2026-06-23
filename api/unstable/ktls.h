6b481b46917f9ecae302f5f9809d2e7c7ba2f265
Carol Yeh <69044490+CarolYeh910@users.noreply.github.com>
2026-06-23 10:55:37 -0700

    fix clang format

--- api/unstable/ktls.h
+++ api/unstable/ktls.h
@@ -20,9 +20,9 @@
 
     #include <s2n.h>
 
-#ifdef __cplusplus
+    #ifdef __cplusplus
 extern "C" {
-#endif
+    #endif
 
 /**
  * @file ktls.h
@@ -165,8 +165,8 @@ S2N_API int s2n_connection_get_key_update_counts(struct s2n_connection *conn,
 S2N_API int s2n_sendfile(struct s2n_connection *conn, int fd, off_t offset, size_t count,
         size_t *bytes_written, s2n_blocked_status *blocked);
 
-#ifdef __cplusplus
+    #ifdef __cplusplus
 }
-#endif
+    #endif
 
 #endif
