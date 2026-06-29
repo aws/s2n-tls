7f0308ad7c124aa183c2590d94267c21fb36085d
boquan-fang <boquanfang3@gmail.com>
2026-06-29 19:43:10 +0000

    test: enable rust bindings tests on Windows

--- bindings/rust/extended/generate.sh
+++ bindings/rust/extended/generate.sh
@@ -7,6 +7,17 @@ set -xe
 # cd into the script directory so it can be executed from anywhere
 pushd "$(dirname "${BASH_SOURCE[0]}")"
 
+# Helper function to check if the rust bindings is running on Windows
+is_windows() {
+    local os
+    os="$(uname -s)"
+    if [[ "$os" == MINGW* || "$os" == MSYS* || "$os" == CYGWIN* ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
 # delete the existing copy in case we have extra files
 rm -rf s2n-tls-sys/lib
 mkdir -p s2n-tls-sys/lib
@@ -49,15 +60,31 @@ fi;
 # make sure everything builds and passes sanity checks
 pushd s2n-tls-sys
 cargo test
-cargo test --all-features
 cargo test --release
 cargo publish --dry-run --allow-dirty
-cargo publish --dry-run --allow-dirty --all-features
+if is_windows; then
+    # `fips` is the one feature that can't build on Windows/MinGW, since aws-lc-fips-sys can't be built on MSYS2 MinGW.
+    # Test every other feature instead of `--all-features`.
+    # The list is read from the manifest, so newly added features are covered automatically.
+    windows_features=$(cargo metadata --no-deps --format-version 1 \
+        | jq -r '.packages[] | select(.name == "s2n-tls-sys").features | keys - ["default", "fips"] | join(",")')
+    cargo test --features "$windows_features"
+    cargo publish --dry-run --allow-dirty --features "$windows_features"
+else
+    cargo test --all-features
+    cargo publish --dry-run --allow-dirty --all-features
+fi
 popd
 
 pushd ../standard/integration
 rustc --version || rustup toolchain install
-cargo run
+# The integration tests depend on boringssl, which can't be built on
+# MSYS2/MinGW, so skip them on Windows.
+if is_windows; then
+    echo "Windows detected: skipping integration tests (boringssl is unavailable)"
+else
+    cargo run
+fi
 popd
 
 popd
