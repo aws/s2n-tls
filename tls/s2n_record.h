e82d364f661c75083ba8f0141dfe275893434a8d
James Mayclin <maycj@amazon.com>
2026-07-28 14:45:06 -0700

    Apply suggestion from @jmayclin

--- tls/s2n_record.h
+++ tls/s2n_record.h
@@ -87,5 +87,4 @@ int s2n_sslv2_record_header_parse(struct s2n_connection *conn, uint8_t *record_t
 int s2n_verify_cbc(struct s2n_connection *conn, struct s2n_hmac_state *hmac, struct s2n_blob *decrypted);
 S2N_RESULT s2n_aead_aad_init(const struct s2n_connection *conn, uint8_t *sequence_number, uint8_t content_type, uint16_t record_length, struct s2n_blob *ad);
 S2N_RESULT s2n_tls13_aead_aad_init(struct s2n_record_header *header, struct s2n_blob *ad);
-S2N_RESULT s2n_record_header_version(const uint8_t (*record_header)[5], uint16_t *out);
 S2N_RESULT s2n_record_wipe(struct s2n_connection *conn);
